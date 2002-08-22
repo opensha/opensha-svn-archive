a1080f03ba4ca33acb4c21d67ec9493ec87072e5
Ned Field <field@usgs.gov>
2002-08-22 20:31:03 +0000

    <No Comment Entered>

---
+++ org/scec/calc/GaussExceedProbCalc.java
@@ -0,0 +1,35 @@
+package org.scec.calc;
+
+import edu.uah.math.psol.distributions.*;
+
+/**
+ * <p>Title: GaussExceedProbCalc.java </p>
+ * <p>Description: This is a utility to calculate probability of exceedance assuming a gaussian distribution</p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: </p>
+ * @author Edward Field  date: aug 22, 2002
+ * @version 1.0
+ */
+
+public final class GaussExceedProbCalc {
+
+
+ /**
+  * This function calculates the Gaussian exceedance probability for the standardized
+  * random variable assuming no truncation of the distribution.
+  */
+ public static double getExceedProb(double standRandVariable) {
+
+    return (0.0);
+ }
+
+ /**
+  * This function calculates the exceedance probability for a truncated Gaussian
+  * distribution (truncType=0 for none; truncType=1 for upper truncation, and
+  * truncType=2 for two-sided truncation).
+  */
+ public static double getExceedProb(double standRandVariable, int truncType, double truncLevel) {
+   return (0.0);
+ }
+
+}
