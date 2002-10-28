1aa287015f361e1750eb1c35d03df0ff7d0b0371
Ned Field <field@usgs.gov>
2002-10-28 18:48:15 +0000

    <No Comment Entered>

--- org/scec/calc/MomentMagCalc.java
+++ org/scec/calc/MomentMagCalc.java
@@ -2,7 +2,7 @@ package org.scec.calc;
 
 /**
  * <p>Title: MomentMagCalc </p>
- * <p>Description: This is a utility to calculate moment for a given magnitude or vice versa</p>
+ * <p>Description: This is a utility to calculate moment (in SI units: Newton-Meters) for a given magnitude or vice versa</p>
  *
  * @author Vipin Gupta
  * @created    August 7, 2002
@@ -11,13 +11,18 @@ package org.scec.calc;
 
 public final class MomentMagCalc {
 
+ /**
+  * This function calculates the moment for the given magnitude
+  * @param mag: Magnitude
+  * @returns Moment in Newton-Meters
+  */
  public static double getMoment(double mag) {
     return (Math.pow(10,1.5*mag+9.05));
  }
 
  /**
   * This function calculates the magnitude for the given moment
-  * @param moment : Moment for which magnitude needs to be calculated
+  * @param moment : Moment in Newton-Meters
   * @returns magnitude for the given moment
   */
  public static double getMag(double moment) {
