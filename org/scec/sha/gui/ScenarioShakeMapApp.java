193b3bcf20ccdbca8b27b78ded7159595bf31382
niting <niting>
2003-03-24 20:09:36 +0000

    <No Comment Entered>

---
+++ org/scec/sha/gui/ScenarioShakeMapApp.java
@@ -0,0 +1,270 @@
+package org.scec.sha.gui;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.*;
+import javax.swing.*;
+import javax.swing.border.*;
+import java.util.*;
+
+import org.scec.sha.gui.beans.*;
+import org.scec.sha.imr.*;
+
+/**
+ * <p>Title: ScenarioShakeMapApp</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: </p>
+ * @author: Ned Field & Nitin Gupta & Vipin Gupta
+ * @created : March 21,2003
+ * @version 1.0
+ */
+
+public class ScenarioShakeMapApp extends Applet {
+
+
+  //variables that determine the width and height of the frame
+  private static final int W=900;
+  private static final int H=740;
+  // default insets
+  private Insets defaultInsets = new Insets( 4, 4, 4, 4 );
+
+  /**
+   *  The object class names for all the supported attenuation ralations (IMRs)
+   *  Temp until figure out way to dynamically load classes during runtime
+   */
+  public final static String BJF_CLASS_NAME = "org.scec.sha.imr.attenRelImpl.BJF_1997_AttenRel";
+  public final static String AS_CLASS_NAME = "org.scec.sha.imr.attenRelImpl.AS_1997_AttenRel";
+  public final static String C_CLASS_NAME = "org.scec.sha.imr.attenRelImpl.Campbell_1997_AttenRel";
+  public final static String SCEMY_CLASS_NAME = "org.scec.sha.imr.attenRelImpl.SCEMY_1997_AttenRel";
+  public final static String F_CLASS_NAME = "org.scec.sha.imr.attenRelImpl.Field_2000_AttenRel";
+  // public final static String A_CLASS_NAME = "org.scec.sha.imr.attenRelImpl.Abrahamson_2000_AttenRel";
+  public final static String CB_CLASS_NAME = "org.scec.sha.imr.attenRelImpl.CB_2003_AttenRel";
+
+  /**
+   *  The object class names for all the supported Eqk Rup Forecasts
+   */
+  public final static String PEER_FAULT_FORECAST_CLASS_NAME = "org.scec.sha.earthquake.PEER_TestCases.PEER_FaultForecast";
+  public final static String PEER_AREA_FORECAST_CLASS_NAME = "org.scec.sha.earthquake.PEER_TestCases.PEER_AreaForecast";
+  public final static String PEER_NON_PLANAR_FAULT_FORECAST_CLASS_NAME = "org.scec.sha.earthquake.PEER_TestCases.PEER_NonPlanarFaultForecast";
+  public final static String PEER_LISTRIC_FAULT_FORECAST_CLASS_NAME = "org.scec.sha.earthquake.PEER_TestCases.PEER_ListricFaultForecast";
+  public final static String PEER_MULTI_SOURCE_FORECAST_CLASS_NAME = "org.scec.sha.earthquake.PEER_TestCases.PEER_MultiSourceForecast";
+  public final static String PEER_LOGIC_TREE_FORECAST_CLASS_NAME = "org.scec.sha.earthquake.PEER_TestCases.PEER_LogicTreeERF_List";
+  public final static String FRANKEL_FORECAST_CLASS_NAME = "org.scec.sha.earthquake.rupForecastImpl.Frankel96.Frankel96_EqkRupForecast";
+
+
+  // instances of the GUI Beans which will be shown in this applet
+  private EqkRupSelectorGuiBean erfGuiBean;
+  private IMR_GuiBean imrGuiBean;
+  private IMT_GuiBean imtGuiBean;
+  private SitesInGriddedRegionGuiBean sitesGuiBean;
+  private IMLorProbSelectorGuiBean imlProbGuiBean;
+  private MapGuiBean mapGuiBean;
+
+  private boolean isStandalone = false;
+  private JPanel mainPanel = new JPanel();
+  private Border border1;
+  private JSplitPane mainSplitPane = new JSplitPane();
+  private JSplitPane chartSplitPane = new JSplitPane();
+  private JTabbedPane parameterTabbedPanel = new JTabbedPane();
+  private JPanel imrPanel = new JPanel();
+  private JPanel eqkRupPanel = new JPanel();
+  private JPanel timespanPanel = new JPanel();
+  private JPanel prob_IMLPanel = new JPanel();
+  private JPanel buttonPanel = new JPanel();
+  private JComboBox controlPanelCombo = new JComboBox();
+  private JButton addButton = new JButton();
+  private GridBagLayout gridBagLayout2 = new GridBagLayout();
+  private GridBagLayout gridBagLayout3 = new GridBagLayout();
+  private GridBagLayout gridBagLayout4 = new GridBagLayout();
+  private GridBagLayout gridBagLayout6 = new GridBagLayout();
+  private BorderLayout borderLayout1 = new BorderLayout();
+  private GridBagLayout gridBagLayout1 = new GridBagLayout();
+  private BorderLayout borderLayout2 = new BorderLayout();
+  private JSplitPane imr_IMTSplit = new JSplitPane();
+  private JSplitPane imr_RegionSplit = new JSplitPane();
+  private JPanel imrSelectionPanel = new JPanel();
+  private GridBagLayout gridBagLayout5 = new GridBagLayout();
+  private JPanel siteRegionPanel = new JPanel();
+  private JPanel imtPanel = new JPanel();
+  //Get a parameter value
+  public String getParameter(String key, String def) {
+    return isStandalone ? System.getProperty(key, def) :
+      (getParameter(key) != null ? getParameter(key) : def);
+  }
+
+  //Construct the applet
+  public ScenarioShakeMapApp() {
+  }
+  //Initialize the applet
+  public void init() {
+    try {
+      jbInit();
+    }
+    catch(Exception e) {
+      e.printStackTrace();
+    }
+  }
+  //Component initialization
+  private void jbInit() throws Exception {
+    border1 = new EtchedBorder(EtchedBorder.RAISED,new Color(248, 254, 255),new Color(121, 124, 136));
+    this.setSize(new Dimension(1074, 714));
+    this.setLayout(borderLayout1);
+    mainPanel.setBorder(border1);
+    mainPanel.setLayout(gridBagLayout6);
+    mainSplitPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
+    buttonPanel.setLayout(gridBagLayout1);
+    addButton.setText("Add Map");
+    prob_IMLPanel.setLayout(gridBagLayout2);
+    timespanPanel.setLayout(gridBagLayout3);
+    eqkRupPanel.setLayout(gridBagLayout4);
+    imrPanel.setLayout(borderLayout2);
+    imr_IMTSplit.setOrientation(JSplitPane.VERTICAL_SPLIT);
+    imr_RegionSplit.setOrientation(JSplitPane.VERTICAL_SPLIT);
+    imrSelectionPanel.setLayout(gridBagLayout5);
+    this.add(mainPanel, BorderLayout.CENTER);
+    mainPanel.add(mainSplitPane,  new GridBagConstraints(0, 0, 1, 1, 1.0, 1.0
+            ,GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 5, 2), 0, 492));
+    mainSplitPane.add(chartSplitPane, JSplitPane.LEFT);
+    mainSplitPane.add(buttonPanel, JSplitPane.RIGHT);
+    buttonPanel.add(controlPanelCombo,     new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0
+            ,GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(5, 3, 10, 0), 2, 3));
+    buttonPanel.add(addButton,  new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0
+            ,GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(19, 44, 24, 435), 60, 18));
+    chartSplitPane.add(parameterTabbedPanel, JSplitPane.RIGHT);
+    parameterTabbedPanel.add(imrPanel,   "IMR Model");
+    imrPanel.add(imr_IMTSplit, BorderLayout.CENTER);
+    parameterTabbedPanel.add(eqkRupPanel,  "Forecast Model");
+    parameterTabbedPanel.add(timespanPanel,  "Time Span");
+    parameterTabbedPanel.add(prob_IMLPanel,  "MapType");
+    imr_IMTSplit.add(imr_RegionSplit, JSplitPane.TOP);
+    imr_RegionSplit.add(imrSelectionPanel, JSplitPane.TOP);
+    imr_RegionSplit.add(siteRegionPanel, JSplitPane.BOTTOM);
+    imr_IMTSplit.add(imtPanel, JSplitPane.BOTTOM);
+    mainSplitPane.setDividerLocation(585);
+    chartSplitPane.setDividerLocation(650);
+    imr_IMTSplit.setDividerLocation(400);
+    imr_RegionSplit.setDividerLocation(100);
+  }
+  //Start the applet
+  public void start() {
+  }
+  //Stop the applet
+  public void stop() {
+  }
+  //Destroy the applet
+  public void destroy() {
+  }
+  //Get Applet information
+  public String getAppletInfo() {
+    return "Applet Information";
+  }
+  //Get parameter info
+  public String[][] getParameterInfo() {
+    return null;
+  }
+  //Main method
+  public static void main(String[] args) {
+    ScenarioShakeMapApp applet = new ScenarioShakeMapApp();
+    applet.isStandalone = true;
+    JFrame frame = new JFrame();
+    //EXIT_ON_CLOSE == 3
+    frame.setDefaultCloseOperation(3);
+    frame.setTitle("ScenarioShakeMap App");
+    frame.getContentPane().add(applet, BorderLayout.CENTER);
+    applet.init();
+    applet.start();
+    frame.setSize(W,H);
+    Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+    frame.setLocation((d.width - frame.getSize().width) / 2, (d.height - frame.getSize().height) / 2);
+    frame.setVisible(true);
+  }
+
+  //static initializer for setting look & feel
+  static {
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+      //UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+    }
+    catch(Exception e) {
+    }
+  }
+
+  /**
+   * Initialize the IMR Gui Bean
+   */
+  private void initIMRPanel() {
+    // create the IMR Gui Bean object
+     // It accepts the vector of IMR class names
+     Vector imrClasses = new Vector();
+     //imrClasses.add(this.A_CLASS_NAME);
+     imrClasses.add(this.AS_CLASS_NAME);
+     imrClasses.add(this.BJF_CLASS_NAME);
+     imrClasses.add(this.C_CLASS_NAME);
+     imrClasses.add(this.SCEMY_CLASS_NAME);
+     imrClasses.add(this.CB_CLASS_NAME);
+     imrClasses.add(this.F_CLASS_NAME);
+     imrGuiBean = new IMR_GuiBean(imrClasses);
+     //imrGuiBean.getParameterEditor(imrGuiBean.IMR_PARAM_NAME).getParameter().addParameterChangeListener(this);
+
+     // show this gui bean the JPanel
+    imrPanel.add(this.imrGuiBean,new GridBagConstraints( 0, 0, 1, 1, 1.0, 1.0,
+         GridBagConstraints.CENTER, GridBagConstraints.BOTH, defaultInsets, 0, 0 ));
+
+     // get the selected IMR
+     AttenuationRelationshipAPI imr = imrGuiBean.getSelectedIMR_Instance();
+     // create the Site Gui Bean object
+     sitesGuiBean = new SitesInGriddedRegionGuiBean();
+     sitesGuiBean.replaceSiteParams(imr.getSiteParamsIterator());
+     // show the sitebean in JPanel
+     imrPanel.add(this.sitesGuiBean, new GridBagConstraints( 1, 0, 1, 1, 1.0, 1.0,
+         GridBagConstraints.CENTER, GridBagConstraints.BOTH, defaultInsets, 0, 0 ));
+
+
+     /**
+      * Initialize the IMT Gui Bean
+      */
+
+     // create the IMT Gui Bean object
+     imtGuiBean = new IMT_GuiBean(imr);
+
+     imrPanel.add(imtGuiBean, new GridBagConstraints( 2, 0, 1, 1, 1.0, 1.0,
+               GridBagConstraints.CENTER, GridBagConstraints.BOTH, defaultInsets, 0, 0 ));
+
+
+
+
+  }
+
+  /**
+   * Initialize the ERF Gui Bean
+   */
+  private void initERFSelector_GuiBean() {
+     // create the ERF Gui Bean object
+   Vector erf_Classes = new Vector();
+   erf_Classes.add(PEER_FAULT_FORECAST_CLASS_NAME);
+   erf_Classes.add(PEER_AREA_FORECAST_CLASS_NAME);
+   erf_Classes.add(PEER_NON_PLANAR_FAULT_FORECAST_CLASS_NAME);
+   erf_Classes.add(PEER_LISTRIC_FAULT_FORECAST_CLASS_NAME);
+   erf_Classes.add(PEER_MULTI_SOURCE_FORECAST_CLASS_NAME);
+   erf_Classes.add(PEER_LOGIC_TREE_FORECAST_CLASS_NAME);
+   erf_Classes.add(FRANKEL_FORECAST_CLASS_NAME);
+   erfGuiBean = new EqkRupSelectorGuiBean(erf_Classes);
+
+   eqkRupPanel.add(erfGuiBean, new GridBagConstraints( 0, 0, 1, 1, 1.0, 1.0,
+                GridBagConstraints.CENTER,GridBagConstraints.BOTH, defaultInsets, 0, 0 ));
+   //erfGuiBean.getParameterEditor(erfGuiBean.ERF_PARAM_NAME).getParameter().addParameterChangeListener(this);
+
+  }
+
+  /**
+   * Initialise the IMT_Prob Selector Gui Bean
+   */
+  private void initImlProb_GuiBean(){
+    imlProbGuiBean = new IMLorProbSelectorGuiBean();
+
+  }
+
+
+
+}
