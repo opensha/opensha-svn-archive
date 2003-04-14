e55cbf8a123a84190244a4fe11f568b38f574221
niting <niting>
2003-04-14 21:06:16 +0000

    <No Comment Entered>

--- org/scec/sha/gui/HazardMapServerModeApplet.java
+++ org/scec/sha/gui/HazardMapServerModeApplet.java
@@ -72,6 +72,7 @@ public class HazardMapServerModeApplet extends JApplet implements
   public final static String PEER_LISTRIC_FAULT_FORECAST_CLASS_NAME = "org.scec.sha.earthquake.PEER_TestCases.PEER_ListricFaultForecast";
   public final static String PEER_MULTI_SOURCE_FORECAST_CLASS_NAME = "org.scec.sha.earthquake.PEER_TestCases.PEER_MultiSourceForecast";
   public final static String FRANKEL_FORECAST_CLASS_NAME = "org.scec.sha.earthquake.rupForecastImpl.Frankel96.Frankel96_EqkRupForecast";
+  public final static String FRANKEL_FORECAST_ADJUSTABLE_CLASS_NAME = "org.scec.sha.earthquake.rupForecastImpl.Frankel96.AdjustableEqkRupForecast";
   public final static String STEP_FORECAST_CLASS_NAME = "org.scec.sha.earthquake.rupForecastImpl.step.STEP_EqkRupForecastWebEnabled";
 
 
@@ -545,6 +546,7 @@ public class HazardMapServerModeApplet extends JApplet implements
     // create the ERF Gui Bean object
   Vector erf_Classes = new Vector();
   erf_Classes.add(FRANKEL_FORECAST_CLASS_NAME);
+  erf_Classes.add(FRANKEL_FORECAST_ADJUSTABLE_CLASS_NAME);
   erf_Classes.add(this.STEP_FORECAST_CLASS_NAME);
   erf_Classes.add(PEER_FAULT_FORECAST_CLASS_NAME);
   erf_Classes.add(PEER_AREA_FORECAST_CLASS_NAME);
