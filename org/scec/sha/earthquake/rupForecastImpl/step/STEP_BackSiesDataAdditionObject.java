328db5fbeee752b0015569a06c14b8d645468aa2
niting <niting>
2003-09-12 17:34:36 +0000

    <No Comment Entered>

--- org/scec/sha/earthquake/rupForecastImpl/step/STEP_BackSiesDataAdditionObject.java
+++ org/scec/sha/earthquake/rupForecastImpl/step/STEP_BackSiesDataAdditionObject.java
@@ -36,200 +36,23 @@ public class STEP_BackSiesDataAdditionObject {
   public STEP_BackSiesDataAdditionObject() {
   }
 
-  void addDataSet(String backSiesDataSetDir,String stepDataSetDir,String finalDataSetDir){
-    //gets the info for the step dataset
-    loadStepDataSet(stepDataSetDir);
-    //gets the info for the back sies dataset
-    loadBackSiesDataSet(backSiesDataSetDir);
-    //adds the 2 datasets and generates a final result
-    generateFinalDataSet(backSiesDataSetDir,stepDataSetDir,finalDataSetDir);
-  }
-
-  /**
-   * Reads the step dataset metadata and site info file for the given region
-   */
-  private void loadStepDataSet(String stepDir) {
-    try {
-      File dirs =new File(stepDir);
-      File[] dirList=dirs.listFiles(); // get the list of all the data in the parent directory
-      if(dirList==null)
-        throw new Exception(this.NO_DATA_EXISTS);
-
-      // for each data set, read the meta data and sites info
-      for(int i=0;i<dirList.length;++i){
-          // read the meta data file
-          String dataSetDescription= new String();
-          try {
-            FileReader dataReader = new FileReader(stepDir+"/metadata.dat");
-            BufferedReader in = new BufferedReader(dataReader);
-            dataSetDescription = "";
-            String str=in.readLine();
-            while(str!=null) {
-              dataSetDescription += str+"\n";
-              str=in.readLine();
-            }
-            stepMetaData = dataSetDescription;
-            in.close();
-          }catch(Exception ee) {
-            ee.printStackTrace();
-          }
-
-          try {
-            // read the sites file
-            FileReader dataReader =
-                new FileReader(stepDir+"/sites.dat");
-            BufferedReader in = new BufferedReader(dataReader);
-            // first line in the file contains the min lat, max lat, discretization interval
-            stepLatitude = in.readLine();
-
-            // Second line in the file contains the min lon, max lon, discretization interval
-            stepLongitude = in.readLine();
-
-          } catch(Exception e) {
-            e.printStackTrace();
-        }
-      }
-    }catch(Exception e) {
-      e.printStackTrace();
-    }
-  }
-
-
-  /**
-   * Reads the BackSies MetaData and Site info files for the given region
-   */
-  private void loadBackSiesDataSet(String backSiesDir) {
-    try {
-      File dirs =new File(backSiesDir);
-      File[] dirList=dirs.listFiles(); // get the list of all the data in the parent directory
-      if(dirList==null)
-        throw new Exception(this.NO_DATA_EXISTS);
-
-      // for each data set, read the meta data and sites info
-      for(int i=0;i<dirList.length;++i){
-          // read the meta data file
-          String dataSetDescription= new String();
-          try {
-            FileReader dataReader = new FileReader(backSiesDir+"/metadata.dat");
-            BufferedReader in = new BufferedReader(dataReader);
-            dataSetDescription = "";
-            String str=in.readLine();
-            while(str!=null) {
-              dataSetDescription += str+"\n";
-              str=in.readLine();
-            }
-            stepMetaData = dataSetDescription;
-            in.close();
-          }catch(Exception ee) {
-            ee.printStackTrace();
-          }
-
-          try {
-            // read the sites file
-            FileReader dataReader =
-                new FileReader(backSiesDir+"/sites.dat");
-            BufferedReader in = new BufferedReader(dataReader);
-            // first line in the file contains the min lat, max lat, discretization interval
-            stepLatitude = in.readLine();
-
-            // Second line in the file contains the min lon, max lon, discretization interval
-            stepLongitude = in.readLine();
-
-          } catch(Exception e) {
-            e.printStackTrace();
-        }
-      }
-    }catch(Exception e) {
-      e.printStackTrace();
-    }
-  }
 
   /**
    * Adds the 2 dataset for the backSies and Step and generates a final dataset
-   * @param backSiesDir : Directory name for the Step BackGround Prob
-   * @param stepDir :Directory name for the Step Addon Prob
-   * @param resultedDir : Directory that contains the combined result for the
+   * @param backSies : Vector for the Step BackGround Prob
+   * @param step : Vector for the Step Addon Prob
    * backGround and Addon
    */
-  private void generateFinalDataSet(String backSiesDir, String stepDir, String resultedDir){
-    //reading latitude info line
-    StringTokenizer st = new StringTokenizer(this.stepLatitude);
-    String minLat = st.nextToken();
-    String maxLat = st.nextToken();
-    String gridSpacingLat = st.nextToken();
-
-    //reading the longitude info line
-    st = new StringTokenizer(this.stepLongitude);
-    String minLon= st.nextToken();
-    String maxLon = st.nextToken();
-    String gridSpacingLon = st.nextToken();
-
-    File resultFile = new File(resultedDir);
-    if(!resultFile.isDirectory()){
-    //creating a new directory that stores all the HazardCurves for that region
-      boolean success = (new File(resultedDir)).mkdir();
+  public Vector addDataSet(Vector backSiesDataSet,Vector stepDataSet){
+    Vector resultSet = new Vector();
+    int size = backSiesDataSet.size();
+    for(int i=0;i<size;++i){
+      double backSiesProb = ((Double)backSiesDataSet.get(i)).doubleValue();
+      double stepAddonProb = ((Double)stepDataSet.get(i)).doubleValue();
+      double finalProb = backSiesProb + stepAddonProb - (backSiesProb)*(stepAddonProb);
+      resultSet.add(new Double(finalProb));
     }
-
-    //getting the instance for the step addon and back ground directories
-    File stepFileDir = new File(stepDir);
-    File backSiesFileDir = new File(backSiesDir);
-
-    //checking if the backGround and Addon directories exist for the step
-    if(stepFileDir.isDirectory() && backSiesFileDir.isDirectory()){
-      //getting the listing of all the step files
-      File[] stepFiles = stepFileDir.listFiles();
-      //looping over all the files of the step addon directory
-      for(int i=0;i<stepFiles.length;++i){
-        //getting one file at time
-        String stepFileName = stepFiles[i].getName();
-        if(!stepFileName.endsWith(".txt"))
-          continue;
-        try{
-          //reading the step file
-          FileReader frStep = new FileReader(stepDir+"/"+stepFileName);
-          BufferedReader brStep = new BufferedReader(frStep);
-
-          //Here we are assuming that name of the files in the step addon and step backGround are
-          //the same and also assuming that region for the step addon is either smaller or
-          //equal to the step backGround.
-          //reading the step background file
-          FileReader frBack = new FileReader(backSiesDir+"/"+stepFileName);
-          BufferedReader brBack = new BufferedReader(frBack);
-          //creating the file combined result in a new directory craeted earler
-          // the file name is same as the step file names
-          FileWriter fw = new FileWriter(resultedDir+"/"+stepFileName);
-          BufferedWriter bw = new BufferedWriter(fw);
-
-          //reading the first line form the step addon file
-          String stepLine = brStep.readLine();
-          //reading the first line from the step backGround file
-          String backLine = brBack.readLine();
-          //looping over all the lines in the step file
-          while(stepLine !=null){
-            StringTokenizer stStep = new StringTokenizer(stepLine);
-            StringTokenizer stBack = new StringTokenizer(backLine);
-            //Also assuming that values for the same lat and lons will same in both
-            //step files
-            String iml = stStep.nextToken();
-            stBack.nextToken();
-            //taking the prob values for the backGround and Addon
-            String stepProb = stStep.nextToken();
-            String backProb = stBack.nextToken();
-            //summing up the Probability for the backGround and Addon to create a final
-            //prob. value
-            double prob = new Double(stepProb).doubleValue() +
-                          new Double(backProb).doubleValue() -
-                          (new Double(stepProb).doubleValue() * new Double(backProb).doubleValue());
-            //writing the combined values to the file
-            bw.write(iml+"  "+prob+"\n");
-            stepLine = brStep.readLine();
-            backLine = brBack.readLine();
-          }
-          bw.close();
-        }catch(Exception e){
-          e.printStackTrace();
-        }
-      }
-    }
+    return resultSet;
   }
+
 }
--- org/scec/sha/earthquake/rupForecastImpl/step/STEP_DataSetGenerator.java
+++ org/scec/sha/earthquake/rupForecastImpl/step/STEP_DataSetGenerator.java
@@ -11,6 +11,8 @@ import org.scec.param.event.ParameterChangeWarningEvent;
 import org.scec.param.WarningParameterAPI;
 import org.scec.data.region.SitesInGriddedRegion;
 import org.scec.sha.calc.HazardMapCalculator;
+import org.scec.sha.earthquake.*;
+import org.scec.util.*;
 
 
 /**
@@ -26,20 +28,22 @@ public class STEP_DataSetGenerator implements ParameterChangeWarningListener{
 
 
   private final static String DELTA_RATES_FILE_NAME = "http://www.relm.org/models/step/SoCalDeltaRates.txt";
-  // make a array for saving the X values
-  private  double [] xValues = { .001, .01, .05, .1, .15, .2, .25, .3, .4, .5,
-    .6, .7, .8, .9, 1, 1.1, 1.2, 1.3, 1.4, 1.5}  ;
 
   // VS 30 value to be set in the IMR
   private final Double VS_30= new Double(760);
 
-  private final double MIN_LAT= 34.45;
+  private final double MIN_LAT= 32.45;
   private final double MAX_LAT= 36.60;
   private final double MIN_LON = -121.45 ;
-  private final double MAX_LON= -118.50;
-  private final double GRID_SPACING= .5;
-  private static final String BACKGROUND_STEP_DIR ="backGround/";
+  private final double MAX_LON= -114.50;
+  private final double GRID_SPACING= .1;
   private static final String STEP_DIR = "step/";
+  private static final String STEP_BACKGROUND_FILE = "backGround.txt";
+  private static final String STEP_ADDON_FILE_SUFFIX = "_addon.txt";
+  private static final String STEP_COMBINED_FILE_SUFFIX = "_both.txt";
+  private static final double IML_VALUE = 0.126;
+  private Vector latVals = new Vector();
+  private Vector lonVals = new Vector();
 
 
   public STEP_DataSetGenerator() {
@@ -62,14 +66,17 @@ public class STEP_DataSetGenerator implements ParameterChangeWarningListener{
         MAX_LON, GRID_SPACING);
     region.addSiteParams(imr.getSiteParamsIterator());
 
-    // make the calculator
-    HazardMapCalculator calc = new HazardMapCalculator();
-    calc.showProgressBar(false);
-    File file = new File(this.STEP_DIR);
-    if(!file.isDirectory()){
-      boolean success = (new File(STEP_DIR)).mkdir();
+    int numSites = region.getNumGridLocs();
+
+    //adding the numSites to the lat and Lon Vector
+    for(int i=0;i<numSites;++i){
+      latVals.add(new Double(region.getSite(i).getLocation().getLatitude()));
+      lonVals.add(new Double(region.getSite(i).getLocation().getLongitude()));
     }
-    File backFile = new File(STEP_DIR+BACKGROUND_STEP_DIR);
+
+
+
+    //MetaData String
     String metadata = "IMR Info: \n"+
                       "\t"+"Name: "+imr.getName()+"\n"+
                       "\t"+"Intensity Measure Type: "+ imr.getIntensityMeasure().getName()+"\n"+
@@ -84,50 +91,191 @@ public class STEP_DataSetGenerator implements ParameterChangeWarningListener{
                       "\n\n"+
                       "Forecast Info: \n"+
                       "\t"+"Name: "+forecast.getName()+"\n";
+
     //generating the background dataSet
-    if(!backFile.isDirectory()){
-      String dataInfo = "Step Back Ground DataSet\n\n"+metadata;
-      forecast.getParameter(forecast.SEIS_TYPE_NAME).setValue(forecast.SEIS_TYPE_BACKGROUND);
-      forecast.updateForecast();
-      calc.getHazardMapCurves(STEP_DIR+BACKGROUND_STEP_DIR,
-                              true, xValues, region, imr, forecast, dataInfo);
+    String dataInfo = "Step Back Ground DataSet\n\n"+metadata;
+    //updating the forecast for the background Siesmicity
+    forecast.getParameter(forecast.SEIS_TYPE_NAME).setValue(forecast.SEIS_TYPE_BACKGROUND);
+    forecast.updateForecast();
+    //generating the file for the BackGround
+    File backSiesFile = new File(this.STEP_DIR+this.STEP_BACKGROUND_FILE);
+    Vector backSiesProbVals = new Vector();
+    //if the file for the backGround already exists then just pick up the values for the Prob from the file
+    if(backSiesFile.exists()){
+      try{
+        ArrayList fileLines=FileUtils.loadFile(this.STEP_DIR+this.STEP_BACKGROUND_FILE);
+        ListIterator it =fileLines.listIterator();
+        while(it.hasNext()){
+          StringTokenizer st = new StringTokenizer((String)it.next());
+          st.nextToken();
+          st.nextToken();
+          backSiesProbVals.add(new Double(st.nextToken().trim()));
+        }
+      }catch(Exception  e){
+        e.printStackTrace();
+      }
+    }
+    //if the backGround file does not already exist then create it
+    else{
+      backSiesProbVals = getBackSeisProbVals(imr,region,(EqkRupForecast)forecast);
+      createFile(backSiesProbVals,this.STEP_DIR+this.STEP_BACKGROUND_FILE);
     }
 
-    String dataInfo = "Step Addon Data Set for :\n"+
+    //metadata for the Addon Prob
+    dataInfo = "Step Addon Data Set for :\n"+
                "\t"+this.getSTEP_DateTimeInfo()+"\n\n"+
                metadata;
-    String stepDirName = this.getStepDirName();
-    String stepAddonDirName = STEP_DIR+stepDirName+"_Addon";
-    File addonFile = new File(stepAddonDirName);
-    //generating the Addon DataSet
-    if(!addonFile.isDirectory()){
 
-      forecast.getParameter(forecast.SEIS_TYPE_NAME).setValue(forecast.SEIS_TYPE_ADD_ON);
-      forecast.updateForecast();
+    //updating the STEP forecast for the STEP Addon Probabilities
+    forecast.getParameter(forecast.SEIS_TYPE_NAME).setValue(forecast.SEIS_TYPE_ADD_ON);
+    forecast.updateForecast();
+    //creating the dataFile for the STEP Addon Probabilities
+    Vector stepAddonProbVals = getBackSeisProbVals(imr,region,(EqkRupForecast)forecast);
+    createFile(backSiesProbVals,this.STEP_DIR+this.getStepDirName()+this.STEP_ADDON_FILE_SUFFIX);
 
-      calc.getHazardMapCurves(stepAddonDirName,
-                              true, xValues, region, imr, forecast, dataInfo );
-    }
-    //combining the backgound and Addon dataSet
+    //combining the backgound and Addon dataSet and wrinting the result to the file
     STEP_BackSiesDataAdditionObject addStepData = new STEP_BackSiesDataAdditionObject();
-    String stepBothDirName = STEP_DIR+stepDirName+"_Both";
-    addStepData.addDataSet(STEP_DIR+this.BACKGROUND_STEP_DIR,stepAddonDirName,stepBothDirName);
-    try{
-      FileWriter fr = new FileWriter(stepBothDirName+"/metadata.dat");
-      dataInfo = "Step Combined(Added) Data Set for :\n"+
+    Vector stepBothProbVals = addStepData.addDataSet(backSiesProbVals,stepAddonProbVals);
+    createFile(backSiesProbVals,this.STEP_DIR+this.getStepDirName()+this.STEP_COMBINED_FILE_SUFFIX);
+
+    dataInfo = "Step Combined(Added) Data Set for :\n"+
                "\t"+this.getSTEP_DateTimeInfo()+"\n\n"+
                metadata;
-      fr.write(dataInfo);
-      fr.close();
-      fr=new FileWriter(stepBothDirName+"/sites.dat");
-      fr.write(region.getMinLat()+" "+region.getMaxLat()+" "+
-               region.getGridSpacing()+"\n"+region.getMinLon()+" "+
-               region.getMaxLon()+" "+ region.getGridSpacing()+"\n");
-      fr.close();
+
+
+    /*fr.write(region.getMinLat()+" "+region.getMaxLat()+" "+
+    region.getGridSpacing()+"\n"+region.getMinLon()+" "+
+    region.getMaxLon()+" "+ region.getGridSpacing()+"\n");*/
+
+  }
+
+
+  /**
+   * craetes the output xyz files
+   * @param probVals : Probablity values Vector for each Lat and Lon
+   * @param fileName : File to create
+   */
+  private void createFile(Vector probVals, String fileName){
+    int size = probVals.size();
+    try{
+      FileWriter fr = new FileWriter(this.STEP_DIR+this.getStepDirName());
+      for(int i=0;i<size;++i)
+        fr.write(latVals.get(i)+"  "+lonVals.get(i)+"  "+probVals.get(i)+"\n");
     }catch(IOException ee){
       ee.printStackTrace();
     }
+  }
 
+
+
+  /**
+   * HazardCurve Calculator for the STEP
+   * @param imr : ShakeMap_2003_AttenRel for the STEP Calculation
+   * @param region
+   * @param eqkRupForecast : STEP Forecast
+   * @returns the Vector of Probability values for the given region
+   */
+  private Vector getBackSeisProbVals(ShakeMap_2003_AttenRel imr,SitesInGriddedRegion region,
+                                     EqkRupForecast eqkRupForecast){
+
+    Vector probVals = new Vector();
+    double MAX_DISTANCE = 300;
+
+
+    /* this determines how the calucations are done (doing it the way it's outlined
+    in the paper SRL gives probs greater than 1 if the total rate of events for the
+    source exceeds 1.0, even if the rates of individual ruptures are << 1).
+    */
+    boolean poissonSource = false;
+
+
+    // declare some varibles used in the calculation
+    double qkProb, distance;
+    int k,i;
+
+    // get total number of sources
+    int numSources = eqkRupForecast.getNumSources();
+
+    // compute the total number of ruptures for updating the progress bar
+    int totRuptures = 0;
+    for(i=0;i<numSources;++i)
+      totRuptures+=eqkRupForecast.getSource(i).getNumRuptures();
+
+
+
+    // this boolean will tell us whether a source was actually used
+    // (e.g., all could be outside MAX_DISTANCE)
+    boolean sourceUsed = false;
+    double sourceHazVal =0;
+    double hazVal =0;
+    double condProb =0;
+    int numSites = region.getNumGridLocs();
+    for(int j=0;j<numSites;++j){
+      imr.setSite(region.getSite(j));
+      // loop over sources
+      for(i=0;i < numSources ;i++) {
+
+        // get the ith source
+        ProbEqkSource source = eqkRupForecast.getSource(i);
+
+        // compute it's distance from the site and skip if it's too far away
+        distance = source.getMinDistance(region.getSite(j));
+        if(distance > MAX_DISTANCE)
+          //update progress bar for skipped ruptures
+          continue;
+
+        // indicate that a source has been used
+        sourceUsed = true;
+
+        // determine whether it's poissonian
+        poissonSource = source.isSourcePoissonian();
+
+        if(!poissonSource)
+          sourceHazVal=0;
+
+
+        // get the number of ruptures for the current source
+        int numRuptures = source.getNumRuptures();
+
+        // loop over these ruptures
+        for(int n=0; n < numRuptures ; n++) {
+
+          // get the rupture probability
+          qkProb = ((ProbEqkRupture)source.getRupture(n)).getProbability();
+
+          // set the PQkRup in the IMR
+          try {
+            imr.setProbEqkRupture((ProbEqkRupture)source.getRupture(n));
+          } catch (Exception ex) {
+            System.out.println("Parameter change warning caught");
+          }
+
+          // get the conditional probability of exceedance from the IMR
+          condProb = 0.4343*Math.log(imr.getExceedProbability(Math.log(this.IML_VALUE)));
+
+
+          // For poisson source
+          if(poissonSource)
+              hazVal = hazVal*Math.pow(1-qkProb,condProb);
+          // For non-Poissin source
+          else
+              sourceHazVal =sourceHazVal + qkProb*condProb;
+        }
+        // for non-poisson source:
+        if(!poissonSource)
+            hazVal = hazVal*(1-condProb);
+      }
+
+      // finalize the hazard function
+      if(sourceUsed)
+        hazVal = 1-hazVal;
+      else
+        hazVal = 0.0;
+
+      probVals.add(new Double(hazVal));
+    }
+
+    return probVals;
   }
 
   /**
