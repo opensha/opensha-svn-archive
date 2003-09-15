512ded63cdd6a25bbb4fe835b1f81fb89d27abd8
niting <niting>
2003-09-15 17:51:28 +0000

    <No Comment Entered>

--- org/scec/sha/earthquake/rupForecastImpl/step/STEP_DataSetGenerator.java
+++ org/scec/sha/earthquake/rupForecastImpl/step/STEP_DataSetGenerator.java
@@ -17,10 +17,10 @@ import org.scec.util.*;
 
 /**
  * <p>Title: STEP_DataSetGenerator</p>
- * <p>Description: </p>
- * <p>Copyright: Copyright (c) 2002</p>
- * <p>Company: </p>
- * @author unascribed
+ * <p>Description: This class generates the Dataset for the STEP Map which includes the
+ * BackGround, STEP addon and combined result for both</p>
+ * @author : Edward (Ned) Field, Nitin Gupta and Vipin Gupta
+ * @created :Sept 03, 2003
  * @version 1.0
  */
 
@@ -41,6 +41,7 @@ public class STEP_DataSetGenerator implements ParameterChangeWarningListener{
   private static final String STEP_BACKGROUND_FILE = "backGround.txt";
   private static final String STEP_ADDON_FILE_SUFFIX = "_addon.txt";
   private static final String STEP_COMBINED_FILE_SUFFIX = "_both.txt";
+  private static final String METADATA_FILE_SUFFIX = "_metadata.dat";
   private static final double IML_VALUE = 0.126;
   private Vector latVals = new Vector();
   private Vector lonVals = new Vector();
@@ -124,6 +125,9 @@ public class STEP_DataSetGenerator implements ParameterChangeWarningListener{
     else{
       backSiesProbVals = getBackSeisProbVals(imr,region,(EqkRupForecast)forecast);
       createFile(backSiesProbVals,this.STEP_DIR+this.STEP_BACKGROUND_FILE);
+      //creting the metadata file for the backGround
+      String backFile = this.STEP_BACKGROUND_FILE.substring(0,STEP_BACKGROUND_FILE.indexOf("."));
+      createMetaDataFile(dataInfo,this.STEP_DIR+backFile+this.METADATA_FILE_SUFFIX);
     }
 
     //metadata for the Addon Prob
@@ -136,27 +140,31 @@ public class STEP_DataSetGenerator implements ParameterChangeWarningListener{
     forecast.updateForecast();
     //creating the dataFile for the STEP Addon Probabilities
     Vector stepAddonProbVals = getBackSeisProbVals(imr,region,(EqkRupForecast)forecast);
-    File addonFile = new File(this.STEP_DIR+this.getStepDirName()+this.STEP_ADDON_FILE_SUFFIX);
-    if(!addonFile.exists())
-      createFile(backSiesProbVals,this.STEP_DIR+this.getStepDirName()+this.STEP_ADDON_FILE_SUFFIX);
+    String stepDirName = this.getStepDirName();
+    File addonFile = new File(this.STEP_DIR+stepDirName+this.STEP_ADDON_FILE_SUFFIX);
+    if(!addonFile.exists()){
+      createFile(stepAddonProbVals,this.STEP_DIR+stepDirName+this.STEP_ADDON_FILE_SUFFIX);
+      //creating the metadata file for the STEP addon probabilities
+      String stepFile = this.STEP_ADDON_FILE_SUFFIX.substring(0,STEP_ADDON_FILE_SUFFIX.indexOf("."));
+      createMetaDataFile(dataInfo,this.STEP_DIR+stepDirName+stepFile+this.METADATA_FILE_SUFFIX);
+    }
 
-    //combining the backgound and Addon dataSet and wrinting the result to the file
-    STEP_BackSiesDataAdditionObject addStepData = new STEP_BackSiesDataAdditionObject();
-    Vector stepBothProbVals = addStepData.addDataSet(backSiesProbVals,stepAddonProbVals);
-    File bothFile = new File(this.STEP_DIR+this.getStepDirName()+this.STEP_COMBINED_FILE_SUFFIX);
-    if(!bothFile.exists())
-      createFile(backSiesProbVals,this.STEP_DIR+this.getStepDirName()+this.STEP_COMBINED_FILE_SUFFIX);
-
+    //metadata for the Combined Prob. (Addon +BackGround)
     dataInfo = "Step Combined(Added) Data Set for :\n"+
                "\t"+this.getSTEP_DateTimeInfo()+"\n\n"+
                metadata;
+    //combining the backgound and Addon dataSet and wrinting the result to the file
+    STEP_BackSiesDataAdditionObject addStepData = new STEP_BackSiesDataAdditionObject();
+    Vector stepBothProbVals = addStepData.addDataSet(backSiesProbVals,stepAddonProbVals);
+    File bothFile = new File(this.STEP_DIR+stepDirName+this.STEP_COMBINED_FILE_SUFFIX);
+    if(!bothFile.exists()){
+      createFile(stepBothProbVals,this.STEP_DIR+stepDirName+this.STEP_COMBINED_FILE_SUFFIX);
+      //creating the metadata file for the STEP addon and backGround probabilities combined
+      String stepBothFile = this.STEP_COMBINED_FILE_SUFFIX.substring(0,STEP_COMBINED_FILE_SUFFIX.indexOf("."));
+      createMetaDataFile(dataInfo,this.STEP_DIR+stepDirName+stepBothFile+this.METADATA_FILE_SUFFIX);
+    }
 
-
-    /*fr.write(region.getMinLat()+" "+region.getMaxLat()+" "+
-    region.getGridSpacing()+"\n"+region.getMinLon()+" "+
-    region.getMaxLon()+" "+ region.getGridSpacing()+"\n");*/
-
-  }
+ }
 
 
   /**
@@ -174,6 +182,22 @@ public class STEP_DataSetGenerator implements ParameterChangeWarningListener{
       fr.close();
     }catch(IOException ee){
       ee.printStackTrace();
+    }
+  }
+
+
+  /**
+   * Creates the metadata file for the dataSet
+   * @param metadata : String that contains the metadata info
+   * @param fileName : Name of the metadataFile
+   */
+  private void createMetaDataFile(String metadata, String fileName){
+    try{
+      FileWriter file = new FileWriter(fileName);
+      file.write(metadata);
+      file.close();
+    }catch(Exception e){
+      e.printStackTrace();
     }
   }
 
