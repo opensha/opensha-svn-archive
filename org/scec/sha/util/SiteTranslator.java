62c4bd95a86248d782956d30ed8d3360efdb2fd7
Ned Field <field@usgs.gov>
2003-12-26 18:21:23 +0000

    <No Comment Entered>

--- org/scec/sha/util/SiteTranslator.java
+++ org/scec/sha/util/SiteTranslator.java
@@ -11,67 +11,76 @@ import org.scec.sha.imr.AttenuationRelationship;
 
 /**
  * <p>Title: SiteTranslator</p>
- * <p>Description: Translation is performed on the following scale:
- * Converting from Vs30 & Basin-Depth-2.5 to the above:
-
- Abrahamson & Silva (1997) & Abrahamson (2000):
- ----------------------------------------------
-
- NA 				if Vs30²180
- Deep-Soil			if Vs30 ² 400 m/s & Basin-Depth-2.5 ³ 100 m
- Rock/Shallow-Soil		otherwise
-
- Sadigh et al. (1997):
- ---------------------
-
- NA 				if Vs30²180
- Deep-Soil			if Vs30 ² 400 m/s & Basin-Depth-2.5 ³ 100 m
- Rock				otherwise
-
- Boore et al. (1997)
- -------------------
-
- Vs30 = Vs30			(if Vs30 > 180; NA otherwise)
-
- Campbell (1997)
- ---------------
-
- NA 				if Vs30²180
- Firm-Soil			if 180<Vs30²400
- Soft-Rock			if 400<Vs30²500
- Hard-Rock			if 500>Vs30
-
- Campbell-Basin-Depth = 0                    		if Vs30 ³ 400
- Campbell-Basin-Depth = Basin-Depth-2.5      	if Vs30 < 400
-
- Field (2000)
- ------------
-
- Vs30 = Vs30			(if Vs30 > 180; NA otherwise)
- Basin-Depth-2.5 = Basin-Depth-2.5
-
- Campbell & Bozorgnia (2003)
- ---------------------------
-
- NA 			if Vs30²180
- Firm-Soil		if 180<Vs30²300
- Very-Firm-Soil	        if 300<Vs30²400
- Soft-Rock		if 400<Vs30²500
- Firm-Rock		if 500>Vs30
-
- ShakeMap (2003)
- ---------------
-
- E                      if Vs30 = 163
- DE                     if Vs30 = 298
- D                      if Vs30 = 301
- CD                     if Vs30 = 372
- C                      if Vs30 = 464
- BC                     if Vs30 = 724
- B                      if Vs30 = 686
- NA                     if anything else
-
-
+ * <p>Description: This object sets the value of a site parameter from the
+ * Wills Site Type (Wills et al., 2000, BSSA, v. 90, S187-S208) and (optionally) from
+ * Basin-Depth-2.5 (the depth in m where the shear-wave velocity equals 2.5 km/sec).
+ * The conversions from the Wills Site Types (E, DE, D, CD, C, BC, B) and basin-depth
+ * are as follows (NA means nothing is set):<p>
+ *
+ *
+ * AS_1997_AttenRel.SITE_TYPE_NAME (Abrahamson & Silva (1997) & Abrahamson (2000))<p>
+ * <UL>
+ * <LI> NA 				if E
+ * <LI> Deep-Soil			if DE, D, or CD
+ * <LI> Rock/Shallow-Soil		if C, BC, or B
+ * </UL>
+ *
+ * SCEMY_1997_AttenRel.SITE_TYPE_NAME (Sadigh et al. (1997)):<p>
+ * <UL>
+ * <LI> NA 				if E
+ * <LI> Deep-Soil			if DE, D, or CD
+ * <LI> Rock		                if C, BC, or B
+ * </UL>
+ *
+ * AttenuationRelationship.VS30_NAME (Boore et al. (1997) & Field (2000))<p>
+ * <LI> <UL>
+ * <LI> Vs30 = NA			if E
+ * <LI> Vs30 = 180			if DE
+ * <LI> Vs30 = 270			if D
+ * <LI> Vs30 = 360			if CD
+ * <LI> Vs30 = 560			if C
+ * <LI> Vs30 = 760			if BC
+ * <LI> Vs30 = 1000			if B
+ * <LI> </UL>
+ *
+ * Campbell_1997_AttenRel.SITE_TYPE_NAME (Campbell (1997))<p>
+ * <UL>
+ * <LI> NA 				if E
+ * <LI> Firm-Soil			if DE, D, or CD
+ * <LI> Soft-Rock			if C
+ * <LI> Hard-Rock			if BC or B
+ * </UL>
+ *
+ * Campbell_1997_AttenRel.BASIN_DEPTH_NAME (Campbell (1997))<p>
+ * <UL>
+ * <LI> Campbell-Basin-Depth = Basin-Depth-2.5
+ * </UL>
+ *
+ * Field_2000_AttenRel.BASIN_DEPTH_NAME (Field (2000))<p>
+ * <UL>
+ * <LI> Basin-Depth-2.5 = Basin-Depth-2.5
+ * </UL>
+ *
+ * CB_2003_AttenRel.SITE_TYPE_NAME (Campbell & Bozorgnia (2003))<p>
+ * <UL>
+ * <LI> NA 			if E
+ * <LI> Firm-Soil		if DE or D
+ * <LI> Very-Firm-Soil	        if CD
+ * <LI> Soft-Rock		if C
+ * <LI> Firm-Rock		if BC or B
+ * </UL>
+ *
+ * ShakeMap_2003_AttenRel.WILLS_SITE_NAME (ShakeMap (2003))<p>
+ * <LI> <UL>
+ * <LI> E                      if E
+ * <LI> DE                     if DE
+ * <LI> D                      if D
+ * <LI> CD                     if CD
+ * <LI> C                      if C
+ * <LI> BC                     if BC
+ * <LI> B                      if B
+ * </UL>
+ *
  * <p>Copyright: Copyright (c) 2002</p>
  * <p>Company: </p>
  * @author: Ned Field & Nitin Gupta & Vipin Gupta
@@ -83,158 +92,172 @@ public class SiteTranslator implements java.io.Serializable{
   private final static String C = "SiteTranslator";
   private final static boolean D = false;
 
-  private double default_VS30;
+  public final static String WILLS_B = "B";
+  public final static String WILLS_BC = "BC";
+  public final static String WILLS_C = "C";
+  public final static String WILLS_CD = "CD";
+  public final static String WILLS_D = "D";
+  public final static String WILLS_DE = "DE";
+  public final static String WILLS_E = "E";
+
 
   public SiteTranslator() {
   }
 
   /**
-   * This function sets the default value for the vs30 at that site for the selected IMR
-   * if value is less than 180
-   * @param vs30
-   */
-
-  public void setDefault_VS30(double vs30){
-    this.default_VS30 = vs30;
-  }
-
-  /**
-   * @param s : site Object
-   * @param vs30
-   * @param basinDepth
+   * @param parameter: the parameter object to be set
+   * @param willsClass - a String with one of the folowing ("E", "DE", "D", "CD", "C", "BC", or "B")
+   * @param basinDepth - Depth (in meters) to where Vs = 2.5-km/sec
    *
-   * @returns the boolean which is required in the case of the HazardCurveApp
-   * to tell the user that site is in the water.
+   * @returns a boolean to tell if setting the value was successful (if false
+   * it means the parameter value was not changed).  A basinDepth value of NaN is allowed
+   * (it will not cause the returned value to be false).
    */
-  public boolean setSiteParams(Site s, double vs30,double basinDepth ){
-    boolean isDefaultVs30Set = false;
+  public boolean setParameterValue(ParameterAPI param, String willsClass,double basinDepth ){
 
-    if(D) System.out.println("Site: "+s.getLocation().toString()+"; vs30: "+vs30+"; basinDepth: "+basinDepth);
+      // shorten name for convenience
+      String wc = willsClass;
 
-    // set Vs30 to default if it's less than 180 m/sec or NaN
-    if(vs30 <=180 || Double.isNaN(vs30)){
-      isDefaultVs30Set= true;
-      vs30=this.default_VS30;
-    }
+      // AS_1997_AttenRel.SITE_TYPE_NAME
+      // (e.g., used by Abrahamson & Silva (1997) & Abrahamson (2000))
+      if(param.getName().equals(AS_1997_AttenRel.SITE_TYPE_NAME)){
 
-    ListIterator  it = s.getParametersIterator();
-    while(it.hasNext()){
+          if(wc.equals(WILLS_DE) || wc.equals(WILLS_D) || wc.equals(WILLS_CD)) {
+            param.setValue(AS_1997_AttenRel.SITE_TYPE_SOIL);
+            return true;
+          }
+          else if(wc.equals(WILLS_C) || wc.equals(WILLS_BC) || wc.equals(WILLS_B)) {
+            param.setValue(AS_1997_AttenRel.SITE_TYPE_ROCK);
+            return true;
+          }
+          else {
+            return false;
+          }
+      }
 
-      ParameterAPI tempParam = (ParameterAPI)it.next();
+      // SCEMY_1997_AttenRel.SITE_TYPE_NAME
+      else if(param.getName().equals(SCEMY_1997_AttenRel.SITE_TYPE_NAME)){
 
-      //Abrahamson & Silva (1997) site type
-      if(tempParam.getName().equalsIgnoreCase(AS_1997_AttenRel.SITE_TYPE_NAME)){
-        if(Double.isNaN(basinDepth)){
-          if(vs30 <=400)
-            tempParam.setValue(AS_1997_AttenRel.SITE_TYPE_SOIL);
-          else
-            tempParam.setValue(AS_1997_AttenRel.SITE_TYPE_ROCK);
+        if(wc.equals(WILLS_DE) || wc.equals(WILLS_D) || wc.equals(WILLS_CD)) {
+          param.setValue(SCEMY_1997_AttenRel.SITE_TYPE_SOIL);
+          return true;
+        }
+        else if(wc.equals(WILLS_C) || wc.equals(WILLS_BC) || wc.equals(WILLS_B)) {
+          param.setValue(SCEMY_1997_AttenRel.SITE_TYPE_ROCK);
+          return true;
         }
         else {
-          if(vs30 <=400 && basinDepth > 100)
-            tempParam.setValue(AS_1997_AttenRel.SITE_TYPE_SOIL);
-          else
-            tempParam.setValue(AS_1997_AttenRel.SITE_TYPE_ROCK);
+          return false;
         }
       }
 
-      // Vs30 site type (e.g., BJF-1997 and Field-2000) site type
-      else if(tempParam.getName().equalsIgnoreCase(AttenuationRelationship.VS30_NAME)){
-        if(vs30>180)
-          tempParam.setValue(new Double(vs30));
-      }
-
-      //Cambell 1997 basin depth
-      else if(tempParam.getName().equalsIgnoreCase(Campbell_1997_AttenRel.BASIN_DEPTH_NAME)){
-        if(vs30>=400)
-          tempParam.setValue(new Double(0));
-        else {
-          // set basin depth in kms
-          if (D) System.out.println("BasinDEpth:"+basinDepth);
-          if(Double.isNaN(basinDepth)) tempParam.setValue(null);
-          else  tempParam.setValue(new Double(basinDepth/1000));
+      // AttenuationRelationship.VS30_NAME
+      // (e.g., used by BJF-1997 and Field-2000) site type
+      else if(param.getName().equals(AttenuationRelationship.VS30_NAME)){
+        if (wc.equals(WILLS_DE)) {
+          param.setValue(new Double(180));
+          return true;
         }
-      }
-
-      //Cambell (1997) site type
-      else if(tempParam.getName().equalsIgnoreCase(Campbell_1997_AttenRel.SITE_TYPE_NAME)){
-        if(vs30>180 && vs30<=400)
-          tempParam.setValue(Campbell_1997_AttenRel.SITE_TYPE_FIRM_SOIL);
-        else if(vs30>400 && vs30<=500)
-          tempParam.setValue(Campbell_1997_AttenRel.SITE_TYPE_SOFT_ROCK);
-        else if(vs30>500)
-          tempParam.setValue(Campbell_1997_AttenRel.SITE_TYPE_HARD_ROCK);
-      }
-
-      //Campbell & Bozorgnia (2003) site type
-      else if(tempParam.getName().equalsIgnoreCase(CB_2003_AttenRel.SITE_TYPE_NAME)){
-        if(vs30>180 && vs30<=300)
-          tempParam.setValue(CB_2003_AttenRel.SITE_TYPE_FIRM_SOIL);
-        if(vs30>300 && vs30<=400)
-          tempParam.setValue(CB_2003_AttenRel.SITE_TYPE_VERY_FIRM_SOIL);
-        if(vs30 >400 && vs30 <=500)
-          tempParam.setValue(CB_2003_AttenRel.SITE_TYPE_SOFT_ROCK);
-        if(vs30 >500)
-          tempParam.setValue(CB_2003_AttenRel.SITE_TYPE_FIRM_ROCK);
-      }
-
-      //Abrahamson (2000) site type - not needed because same as Abrahamson & Silva (1997)
-      else if(tempParam.getName().equalsIgnoreCase(Abrahamson_2000_AttenRel.SITE_TYPE_NAME)){
-        if(Double.isNaN(basinDepth)){
-          if(vs30 <=400)
-            tempParam.setValue(Abrahamson_2000_AttenRel.SITE_TYPE_SOIL);
-          else
-            tempParam.setValue(Abrahamson_2000_AttenRel.SITE_TYPE_ROCK);
+        else if (wc.equals(WILLS_D)) {
+          param.setValue(new Double(270));
+          return true;
+        }
+        else if (wc.equals(WILLS_CD)) {
+          param.setValue(new Double(360));
+          return true;
+        }
+        else if (wc.equals(WILLS_C)) {
+          param.setValue(new Double(560));
+          return true;
+        }
+        else if (wc.equals(WILLS_BC)) {
+          param.setValue(new Double(760));
+          return true;
+        }
+        else if (wc.equals(WILLS_B)) {
+          param.setValue(new Double(1000));
+          return true;
         }
         else {
-          if(vs30 <=400 && basinDepth > 100)
-            tempParam.setValue(Abrahamson_2000_AttenRel.SITE_TYPE_SOIL);
-          else
-            tempParam.setValue(Abrahamson_2000_AttenRel.SITE_TYPE_ROCK);
+          return false;
         }
       }
 
-      //SCEMY Site type
-      else if(tempParam.getName().equalsIgnoreCase(SCEMY_1997_AttenRel.SITE_TYPE_NAME)){
-        if(Double.isNaN(basinDepth)){
-          if(vs30 <=400)
-            tempParam.setValue(SCEMY_1997_AttenRel.SITE_TYPE_SOIL);
-          else
-            tempParam.setValue(SCEMY_1997_AttenRel.SITE_TYPE_ROCK);
+      // Campbell_1997_AttenRel.BASIN_DEPTH_NAME
+      else if(param.getName().equals(Campbell_1997_AttenRel.BASIN_DEPTH_NAME)){
+          if(Double.isNaN(basinDepth)) param.setValue(null);
+          else  param.setValue(new Double(basinDepth/1000));  // converted to km
+          return true;
+      }
+
+      // Campbell_1997_AttenRel.SITE_TYPE_NAME
+      else if(param.getName().equals(Campbell_1997_AttenRel.SITE_TYPE_NAME)){
+
+        if(wc.equals(WILLS_DE) || wc.equals(WILLS_D) || wc.equals(WILLS_CD)) {
+          param.setValue(Campbell_1997_AttenRel.SITE_TYPE_FIRM_SOIL);
+          return true;
+        }
+        else if(wc.equals(WILLS_C)) {
+          param.setValue(Campbell_1997_AttenRel.SITE_TYPE_SOFT_ROCK);
+          return true;
+        }
+        else if(wc.equals(WILLS_BC) || wc.equals(WILLS_B)) {
+          param.setValue(Campbell_1997_AttenRel.SITE_TYPE_HARD_ROCK);
+          return true;
         }
         else {
-          if(vs30 <=400 && basinDepth > 100)
-            tempParam.setValue(SCEMY_1997_AttenRel.SITE_TYPE_SOIL);
-          else
-            tempParam.setValue(SCEMY_1997_AttenRel.SITE_TYPE_ROCK);
+          return false;
         }
       }
 
-      //Field basin-depth site type
-      else if(tempParam.getName().equalsIgnoreCase(Field_2000_AttenRel.BASIN_DEPTH_NAME)){
+      // CB_2003_AttenRel.SITE_TYPE_NAME
+      else if(param.getName().equals(CB_2003_AttenRel.SITE_TYPE_NAME)){
+        if(wc.equals(WILLS_DE) || wc.equals(WILLS_D)) {
+          param.setValue(CB_2003_AttenRel.SITE_TYPE_FIRM_SOIL);
+          return true;
+        }
+        else if(wc.equals(WILLS_CD)) {
+          param.setValue(CB_2003_AttenRel.SITE_TYPE_VERY_FIRM_SOIL);
+          return true;
+        }
+        else if(wc.equals(WILLS_C)) {
+          param.setValue(CB_2003_AttenRel.SITE_TYPE_SOFT_ROCK);
+          return true;
+        }
+        else if(wc.equals(WILLS_BC) || wc.equals(WILLS_B)) {
+          param.setValue(CB_2003_AttenRel.SITE_TYPE_FIRM_ROCK);
+         return true;
+        }
+        else {
+          return false;
+        }
+      }
+
+
+
+      // Field_2000_AttenRel.BASIN_DEPTH_NAME
+      else if(param.getName().equals(Field_2000_AttenRel.BASIN_DEPTH_NAME)){
         // set basin depth in kms
-          if(Double.isNaN(basinDepth)) tempParam.setValue(null);
-          else  tempParam.setValue(new Double(basinDepth/1000));
+          if(Double.isNaN(basinDepth)) param.setValue(null);
+          else  param.setValue(new Double(basinDepth/1000));
+          return true;
       }
 
-      //The Wills site classification used by the ShakeMap (2003) relationship
-      else if(tempParam.getName().equalsIgnoreCase(ShakeMap_2003_AttenRel.WILLS_SITE_NAME)){
-        if      (vs30 == 163)  tempParam.setValue(ShakeMap_2003_AttenRel.WILLS_SITE_E);
-        else if (vs30 == 298)  tempParam.setValue(ShakeMap_2003_AttenRel.WILLS_SITE_DE);
-        else if (vs30 == 301)  tempParam.setValue(ShakeMap_2003_AttenRel.WILLS_SITE_D);
-        else if (vs30 == 372)  tempParam.setValue(ShakeMap_2003_AttenRel.WILLS_SITE_CD);
-        else if (vs30 == 464)  tempParam.setValue(ShakeMap_2003_AttenRel.WILLS_SITE_C);
-        else if (vs30 == 724)  tempParam.setValue(ShakeMap_2003_AttenRel.WILLS_SITE_BC);
-        else if (vs30 == 686)  tempParam.setValue(ShakeMap_2003_AttenRel.WILLS_SITE_B);
-        else {
-          throw new RuntimeException(" That Vs30 is not allowed for "+ShakeMap_2003_AttenRel.WILLS_SITE_NAME);
-        }
+      // ShakeMap_2003_AttenRel.WILLS_SITE_NAME
+      else if(param.getName().equals(ShakeMap_2003_AttenRel.WILLS_SITE_NAME)){
+
+          if(param.isAllowed(wc)) {
+            param.setValue(wc);
+            return true;
+          }
+          else {
+            return false;
+          }
       }
 
+      // site type not found
       else {
-        throw new RuntimeException(C+" does not support the site type: "+tempParam.getName());
+        throw new RuntimeException(C+" does not support the site type: "+param.getName());
       }
-    }
-    return isDefaultVs30Set;
   }
 }
