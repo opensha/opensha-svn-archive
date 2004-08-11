e49d664dfd88ffda1f27fafa29ed39f4bf13d8ed
Ned Field <field@usgs.gov>
2004-08-11 05:07:52 +0000

    <No Comment Entered>

--- org/scec/sha/earthquake/rupForecastImpl/GriddedRegionPoissonEqkSource.java
+++ org/scec/sha/earthquake/rupForecastImpl/GriddedRegionPoissonEqkSource.java
@@ -14,7 +14,8 @@ import java.util.Iterator;
  * <p>Title: GriddedRegionPoissonEqkSource </p>
  * <p>Description: This takes a EvenlyGriddedGeographicRegionAPI, an IncrementalMagFreqDist (of Poissonian
  * rates), a duration, an aveRake, an aveDip, and creates a ProbEqkRupture for each
- * magnitude (with a non-zero rate) and location.  It is assumed that the duration units are the same
+ * magnitude (with a non-zero rate) and location.The MagFreqDist applied to each point is just the
+ * original divided by the number of points.  It is assumed that the duration units are the same
  * as those for the rates in the IncrementalMagFreqDist.</p>
  *
  * @author Edward Field
@@ -26,12 +27,11 @@ public class GriddedRegionPoissonEqkSource extends ProbEqkSource implements java
 
 
   //for Debug purposes
-  private static String  C = new String("PointPoissonEqkSource");
-  private boolean D = true;
+  private static String  C = new String("GriddedRegionPoissonEqkSource");
+  private boolean D = false;
 
   private IncrementalMagFreqDist magFreqDist;
   private EvenlyGriddedGeographicRegionAPI region;
-  private Location location;
   private double aveDip=Double.NaN;
   private double aveRake=Double.NaN;
   private double duration;
@@ -49,8 +49,9 @@ public class GriddedRegionPoissonEqkSource extends ProbEqkSource implements java
    * to consider from the magFreqDist in making the source (those below are ingored).
    *
    */
-  public GriddedRegionPoissonEqkSource(EvenlyGriddedGeographicRegionAPI region, IncrementalMagFreqDist magFreqDist,double duration,
-                        double aveRake, double aveDip, double minMag){
+  public GriddedRegionPoissonEqkSource(EvenlyGriddedGeographicRegionAPI region, IncrementalMagFreqDist
+                                       magFreqDist,double duration,double aveRake, double aveDip,
+                                       double minMag){
     this.region =region;
     this.numLocs = region.getNumGridLocs();
     this.duration=duration;
@@ -59,7 +60,7 @@ public class GriddedRegionPoissonEqkSource extends ProbEqkSource implements java
     this.minMag=minMag;
 
     // set the magFreqDist
-    this.setMagFreqDist(magFreqDist);
+    setMagFreqDist(magFreqDist);
 
     // make the prob qk rupture
     probEqkRupture = new ProbEqkRupture();
@@ -76,8 +77,9 @@ public class GriddedRegionPoissonEqkSource extends ProbEqkSource implements java
    * zero (magnitudes from magFreqDist below are ignored in making the source)
    *
    */
-  public GriddedRegionPoissonEqkSource(EvenlyGriddedGeographicRegionAPI region, IncrementalMagFreqDist magFreqDist,double duration,
-                        double aveRake, double aveDip){
+  public GriddedRegionPoissonEqkSource(EvenlyGriddedGeographicRegionAPI region, IncrementalMagFreqDist
+                                       magFreqDist, double duration, double aveRake, double aveDip){
+
     this( region,  magFreqDist, duration, aveRake,  aveDip, 0.0);
 
   }
@@ -92,20 +94,16 @@ public class GriddedRegionPoissonEqkSource extends ProbEqkSource implements java
     this.magFreqDist=magFreqDist;
 
     // make list of non-zero rates and mags (if mag >= minMag)
-    //magsAndRates = new ArbitrarilyDiscretizedFunc();
     mags = new ArrayList();
     rates = new ArrayList();
-    this.numLocs = region.getNumGridLocs();
+
     for (int i=0; i<magFreqDist.getNum(); ++i){
         if(magFreqDist.getY(i) > 0 && magFreqDist.getX(i) >= minMag){
-            //magsAndRates.set(magFreqDist.getX(i),magFreqDist.getY(i));
           mags.add(new Double(magFreqDist.getX(i)));
           rates.add(new Double(magFreqDist.getY(i)/numLocs));   // normalized by numLocs!
         }
     }
     this.numMags = mags.size();
-
-   // if (D) System.out.println(C+" numNonZeroMagDistPoints="+magsAndRates.getNum());
   }
 
 
@@ -113,7 +111,6 @@ public class GriddedRegionPoissonEqkSource extends ProbEqkSource implements java
    * @return the number of rutures (equals number of mags with non-zero rates time number of locations in region)
    */
   public int getNumRuptures() {
-    //return magsAndRates.getNum();
     return numMags*numLocs;
   }
 
@@ -133,7 +130,7 @@ public class GriddedRegionPoissonEqkSource extends ProbEqkSource implements java
     probEqkRupture.setPointSurface(region.getGridLocation(ithLoc), aveDip);
 
     // compute and set the probability
-    double prob = 1 - Math.exp(-duration*((Double)rates.get(ithMag)).doubleValue());
+    double prob = 1.0 - Math.exp(-duration*((Double)rates.get(ithMag)).doubleValue());
     probEqkRupture.setProbability(prob);
 
     // return the ProbEqkRupture
@@ -166,7 +163,7 @@ public class GriddedRegionPoissonEqkSource extends ProbEqkSource implements java
 
   /**
   * This gets the duration used in computing Poisson probabilities
-  * @param duration
+  * @param duration (in same units as in the MagFreqDist)
   */
   public double getDuration() {
     return duration;
@@ -174,8 +171,8 @@ public class GriddedRegionPoissonEqkSource extends ProbEqkSource implements java
 
 
   /**
-   * This gets the location
-   * @return Location
+   * This gets the region
+   * @return region
    */
   public EvenlyGriddedGeographicRegionAPI getRegion(){
     return region;
