a2e48f2afa251d5094ff4ebe7f547928fd75af39
vgupta <vgupta>
2005-03-13 17:22:32 +0000

    <No Comment Entered>

--- gov/usgs/sha/io/NEHRP_FileReader.java
+++ gov/usgs/sha/io/NEHRP_FileReader.java
@@ -483,6 +483,78 @@ public class NEHRP_FileReader {
   }
 
 
+  private  ArbitrarilyDiscretizedFunc calculateSMSsS1(ArbitrarilyDiscretizedFunc saVals, float fa, float fv) {
+    ArbitrarilyDiscretizedFunc function = new ArbitrarilyDiscretizedFunc();
+    function.set(saVals.getX(0), fa*saVals.getY(0));
+    function.set(saVals.getX(1), fv*saVals.getY(1));
+    String title = "Spectral Response Accelerations SMs and SM1";
+    String subTitle = "SMs = FaSs and SM1 = FvS1";
+    String text1 = "SMs";
+    String text2 = "SM1";
+    return function;
+  }
+
+
+  private  ArbitrarilyDiscretizedFunc calculateSDSsS1(ArbitrarilyDiscretizedFunc saVals, float fa, float fv) {
+    ArbitrarilyDiscretizedFunc function = new ArbitrarilyDiscretizedFunc();
+    function.set(saVals.getX(0), fa*saVals.getY(0)*2.0/3.0);
+    function.set(saVals.getX(1), fv*saVals.getY(1)*2.0/3.0);
+    String title = "Spectral Response Accelerations SDs and SD1";
+    String subTitle = "SDs = 2/3 x SMs and SD1 = 2/3 x SM1";
+    String text1 = "SDs";
+    String text2 = "SD1";
+    return function;
+  }
+
+
+  private DiscretizedFuncList approxSaSd(ArbitrarilyDiscretizedFunc saVals,
+                                         double fa, double fv) {
+    DiscretizedFuncList funcList = new DiscretizedFuncList();
+    ArbitrarilyDiscretizedFunc saSdfunction = new ArbitrarilyDiscretizedFunc();
+    ArbitrarilyDiscretizedFunc saTfunction = new ArbitrarilyDiscretizedFunc();
+    funcList.add(saSdfunction);
+    funcList.add(saTfunction);
+
+    double tAcc = saVals.getX(0);
+    double sAcc = fa*saVals.getY(0);
+    double tVel = saVals.getX(0);
+    double sVel = fv*saVals.getY(1);
+    double spga = 0.4*fa*saVals.getX(0);
+    double tPga = 0;
+    double tMaxVel = 2;
+    double tInc = 0.1;
+    double tVelTransition = sVel/sAcc;
+    double tPgaTransition = 0.2*tVelTransition;
+
+    saTfunction.set(spga,tPga);
+    saTfunction.set(sAcc,tPgaTransition);
+
+    if(tPgaTransition <= tAcc) {
+      saTfunction.set(sAcc, tAcc);
+    }
+    saTfunction.set(sAcc, tVelTransition);
+    double lastT = ((int)(tVelTransition * 10.0)) / 10.0;
+    double nextT = lastT + tInc;
+    while(nextT<tMaxVel) {
+        saTfunction.set(sVel/nextT,nextT);
+        nextT+=tInc;
+    }
+
+    for(int i=0; i < saTfunction.getNum(); ++i) {
+      saSdfunction.set(saTfunction.getX(0), 9.77*saTfunction.getX(0)*Math.pow(saTfunction.getY(0),2));
+    }
+
+    String title = "MCE Response Spectra for Site Class B";
+    String subTitle = "Ss and S1 = Mapped Spectral Acceleration Values";
+
+
+    return funcList;
+  }
+
+
+
+
+
   /**
    *
    * @param lat float
