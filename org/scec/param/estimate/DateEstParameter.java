11de7ec07fd73af0a42081f54a6c60a2a567e079
vgupta <vgupta>
2005-03-30 23:00:15 +0000

    <No Comment Entered>

--- org/scec/param/estimate/DateEstParameter.java
+++ org/scec/param/estimate/DateEstParameter.java
@@ -1,5 +1,7 @@
 package org.scec.param.estimate;
 
+import org.scec.data.estimate.Estimate;
+
 /**
  * <p>Title: DateEstParameter.java </p>
  * <p>Description: Date Estimate Parameter </p>
@@ -9,7 +11,7 @@ package org.scec.param.estimate;
  * @version 1.0
  */
 
-public class DateEstParameter extends EstimateParameter {
+public class DateEstParameter extends DoubleEstimateParameter {
   public final static int BC = 1;
   public final static int AD = 2;
 
@@ -17,7 +19,29 @@ public class DateEstParameter extends EstimateParameter {
   private int era = AD;
 
   public DateEstParameter(String name) {
-    super(name);
+    this(name,null, AD);
   }
+
+  public DateEstParameter(String name, Estimate value, int era) {
+    super(name, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+    setUnits("years");
+    setEra(era);
+  }
+
+  /**
+   * Set the era. Era can have value DateEstParameter.AD or it can have
+   * value DateEstParameter.BC
+   *
+   * @param era
+   */
+  public void setEra(int era) {
+    if(era!=AD && era!=BC) {
+      throw new RuntimeException("Invalid era value");
+    }
+    this.era  =era;
+  }
+
+  // get the era.
+  public int getEra() { return era; }
 
  }
