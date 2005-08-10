3e3b31649ed77cc375ab943ccad311d9107b975b
vgupta <vgupta>
2005-08-10 20:21:43 +0000

    <No Comment Entered>

---
+++ org/opensha/data/estimate/date/LogNormalDateEstimate.java
@@ -0,0 +1,34 @@
+package org.opensha.data.estimate.date;
+
+import org.opensha.data.estimate.LogNormalEstimate;
+
+/**
+ * <p>Title: LogNormalDateEstimate.java </p>
+ * <p>Description: This class saves the date estimate </p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: </p>
+ * @author not attributable
+ * @version 1.0
+ */
+
+public class LogNormalDateEstimate extends LogNormalEstimate implements DateEstimateAPI {
+
+  private boolean isDateCorrected=false;
+
+  public LogNormalDateEstimate(double linearMedian, double stdDev) {
+    super(linearMedian, stdDev);
+  }
+
+  /**
+   * This method allows to set whether date has been corrected
+   * @param isDateCorrected
+   */
+  public void setIsDateCorrected(boolean isDateCorrected) {
+    this.isDateCorrected = isDateCorrected;
+  }
+
+  public boolean isDateCorrected() {
+    return isDateCorrected;
+  }
+
+}
