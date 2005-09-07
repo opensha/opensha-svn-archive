22b163b50c28d3b33c2f2309c107e79e0a51d412
mattgers <mattgers>
2005-09-07 22:33:23 +0000

    <No Comment Entered>

--- javaDevelopers/matt/calc/STEP_main.java
+++ javaDevelopers/matt/calc/STEP_main.java
@@ -14,9 +14,22 @@ package javaDevelopers.matt.calc;
  */
 public class STEP_main {
   public STEP_main() {
-  }
 
-  public static void main(String[] args) {
-    STEP_main step_main = new STEP_main();
+    /**
+     * First load the active aftershock sequence objects from the last run
+     * load: ActiveSTEP_AIC_AftershockForecastList
+     * each object is a STEP_AftershockHypoMagFreqDistForecast
+     */
+
+    /**
+     * Now obtain all events that have occurred since the last time the code
+     * was run:
+     * NewObsEqkRuptureList
+     */
+
+    /**
+     * load background rates/grid list
+     * BackgroundRatesList
+     */
   }
 }
