c823ca28309d6b587f7c2c08f939e6becf59c26e
niting <niting>
2005-09-21 19:29:14 +0000

    <No Comment Entered>

--- javaDevelopers/interns/ReadFrankel02_FaultSurface.java
+++ javaDevelopers/interns/ReadFrankel02_FaultSurface.java
@@ -9,6 +9,8 @@ import org.opensha.sha.surface.GriddedSurfaceAPI;
 import org.opensha.data.Location;
 import org.opensha.param.ParameterConstraintAPI;
 import org.opensha.param.StringConstraint;
+import org.opensha.sha.earthquake.ProbEqkRupture;
+import org.opensha.sha.earthquake.ProbEqkSource;
 
 /**
  * <p>Title: ReadFrankel02_FaultSurface</p>
@@ -35,10 +37,14 @@ public class ReadFrankel02_FaultSurface {
     frankelForecast.getAdjustableParameterList().getParameter(
         Frankel02_AdjustableEqkRupForecast.
         BACK_SEIS_NAME).setValue(Frankel02_AdjustableEqkRupForecast.
-                                 BACK_SEIS_EXCLUDE);
+                                 BACK_SEIS_INCLUDE);
+      frankelForecast.getAdjustableParameterList().getParameter(
+        Frankel02_AdjustableEqkRupForecast.BACK_SEIS_RUP_NAME).setValue(Frankel02_AdjustableEqkRupForecast.BACK_SEIS_RUP_FINITE
+                                 );
+
     frankelForecast.getAdjustableParameterList().getParameter(
         Frankel02_AdjustableEqkRupForecast.
-        RUP_OFFSET_PARAM_NAME).setValue(new Double(10.0));
+        RUP_OFFSET_PARAM_NAME).setValue(new Double(5.0));
     frankelForecast.getTimeSpan().setDuration(50.0);
     setFaultModelInFrankelForecast(frankelForecast.FAULT_MODEL_STIRLING);
   }
@@ -131,15 +137,40 @@ public class ReadFrankel02_FaultSurface {
   }*/
 
 
-  /*public static void main(String[] args) {
+  public static void main(String[] args) {
     ReadFrankel02_FaultSurface nshmpSources = new ReadFrankel02_FaultSurface();
     nshmpSources.createFrankel02Forecast();
+    nshmpSources.frankelForecast.updateForecast();
+
     try {
-      nshmpSources.createNSHMP_CharSourcesFile();
+      FileWriter fw = new FileWriter("NSHMP_SOURCE_RUPTURE_INFO.txt");
+      fw.write("#This file contains the Source-Rupture Info as defined in next line\n");
+      fw.write("#SourceIndex , RuptureIndex , SourceName , Prob , Mag , RuptureSurfaceTop2EndPoints\n");
+      int numSources = nshmpSources.frankelForecast.getNumSources();
+
+      //Going over each and every source in the forecast
+      for (int sourceIndex = 0; sourceIndex < numSources; ++sourceIndex) {
+        // get the ith source
+        ProbEqkSource source = nshmpSources.frankelForecast.getSource(sourceIndex);
+        int numRuptures = source.getNumRuptures();
+
+        //going over all the ruptures in the source
+        for (int rupIndex = 0; rupIndex < numRuptures; ++rupIndex) {
+          ProbEqkRupture rupture = source.getRupture(rupIndex);
+          GriddedSurfaceAPI surface = rupture.getRuptureSurface();
+          String loc1 = surface.getLocation(0,0).toString().replaceAll(",", "  ");
+          String loc2 = surface.getLocation(0,surface.getNumCols()-1).toString().replaceAll(",", "  ");
+          fw.write(sourceIndex+","+rupIndex+","+source.getName()+","+
+                   (float)rupture.getProbability()+","+(float)rupture.getMag()+","+
+              loc1+"   -   "+loc2+"\n");
+        }
+      }
+      fw.close();
+      //nshmpSources.createNSHMP_CharSourcesFile();
     }
     catch (IOException ex) {
       ex.printStackTrace();
     }
-  }*/
+  }
 
 }
