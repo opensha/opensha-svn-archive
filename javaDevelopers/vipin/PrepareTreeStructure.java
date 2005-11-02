5a9f445b595a21b1471a3acb9aa81660dc462843
vgupta <vgupta>
2005-11-02 17:42:03 +0000

    <No Comment Entered>

--- javaDevelopers/vipin/PrepareTreeStructure.java
+++ javaDevelopers/vipin/PrepareTreeStructure.java
@@ -6,6 +6,8 @@ import org.opensha.data.LocationList;
 import java.util.ArrayList;
 import org.opensha.calc.RelativeLocation;
 import java.io.FileWriter;
+import java.io.*;
+import org.opensha.exceptions.*;
 
 /**
  * <p>Title: PrepareTreeStructure.java </p>
@@ -23,6 +25,41 @@ public class PrepareTreeStructure {
   public PrepareTreeStructure() {
     FaultSections faultSections = new FaultSections();
     HashMap faultTraceMapping = faultSections.getAllFaultSections();
+    createTreesForFaultSections(faultTraceMapping);
+    try {
+      // now attach fault sections with other sections which are within cutoff distance
+      Iterator it = faultTree.keySet().iterator();
+      FileWriter fw = new FileWriter(OUT_FILENAME);
+      // do for all fault sections
+      while (it.hasNext()) {
+        HashMap sectionNearestNodeMap = new HashMap();
+        String faultSectionName = (String) it.next();
+        System.out.println(faultSectionName);
+        fw.write("#" + faultSectionName + "\n");
+        Node node = (Node) faultTree.get(faultSectionName);
+        while (node != null) { // loop over all locations on this fault section
+          /* get a list of section names and their distance near this location.
+          Only include sections which are within FAULT_JUMP_CUTOFF_DIST of the location*/
+          HashMap sectionNameAndDist = getAdjacentFaultSectionNode(node);
+          // If same section is near to more than 1 node, then find the neaest node
+          compareSectionsWithPreviousNodes(sectionNearestNodeMap, sectionNameAndDist);
+          // next location on this fault section
+          ArrayList nextNodes = (ArrayList) node.getNext();
+          if (nextNodes == null || nextNodes.size() == 0)
+            break;
+          node = (Node) nextNodes.get(0);
+        }
+        // write nearby sections to file
+        writeNearbySectionsToFile(fw, sectionNearestNodeMap);
+      }
+      fw.close();
+    }catch(Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void createTreesForFaultSections(HashMap faultTraceMapping) throws
+      InvalidRangeException {
     Iterator it = faultTraceMapping.keySet().iterator();
 
     // create individual trees for each section
@@ -38,28 +75,45 @@ public class PrepareTreeStructure {
         node = newNode;
       }
     }
-    try {
-      // now attach fault sections with other sections which are within cutoff distance
-      it = faultTree.keySet().iterator();
-      FileWriter fw = new FileWriter(OUT_FILENAME);
-      while (it.hasNext()) {
-        HashMap sectionNearestNodeMap = new HashMap();
-        String faultSectionName = (String) it.next();
-        System.out.println(faultSectionName);
-        fw.write("#" + faultSectionName + "\n");
-        Node node = (Node) faultTree.get(faultSectionName);
-        while (node != null) {
-          String sectionName = getAdjacentFaultSectionNode(node);
-          if (sectionName != null) fw.write("\t" + sectionName + " at " + node.getLoc()+"\n");
-          ArrayList nextNodes = (ArrayList) node.getNext();
-          if (nextNodes == null || nextNodes.size() == 0)
-            break;
-          node = (Node) nextNodes.get(0);
-        }
+  }
+
+  /**
+   * If same section is near to more than 1 node, then find the neaest node
+   *
+   * @param sectionNearestNodeMap
+   * @param sectionNameAndDist
+   */
+  private void compareSectionsWithPreviousNodes(HashMap sectionNearestNodeMap,
+                                                HashMap sectionNameAndDist) {
+    Iterator sectionNameDistIt = sectionNameAndDist.keySet().iterator();
+    while(sectionNameDistIt.hasNext()) {
+      String sectionName = (String)sectionNameDistIt.next();
+      SectionNodeDist currentNodeSectionDist = (SectionNodeDist)sectionNameAndDist.get(sectionName);
+      if(!sectionNearestNodeMap.containsKey(sectionName))
+        sectionNearestNodeMap.put(sectionName, currentNodeSectionDist);
+      else {
+        SectionNodeDist previousNodeSectionDist = (SectionNodeDist)sectionNearestNodeMap.get(sectionName);
+        if(previousNodeSectionDist.getDistance()>currentNodeSectionDist.getDistance())
+          sectionNearestNodeMap.put(sectionName, currentNodeSectionDist);
       }
-      fw.close();
-    }catch(Exception e) {
-      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Write nearby sections to file
+   *
+   * @param fw
+   * @param sectionNearestNodeMap
+   * @throws IOException
+   */
+  private void writeNearbySectionsToFile(FileWriter fw,
+                                         HashMap sectionNearestNodeMap) throws IOException {
+    // write the nearby sections and the nearest locations into a file
+    Iterator sectionNearestNodeMapIt= sectionNearestNodeMap.keySet().iterator();
+    while(sectionNearestNodeMapIt.hasNext()) {
+      String sectionName = (String)sectionNearestNodeMapIt.next();
+      SectionNodeDist sectionNodeDist = (SectionNodeDist)sectionNearestNodeMap.get(sectionName);
+      fw.write("\t"+sectionName+" at "+sectionNodeDist.getNode().getLoc()+"\n");
     }
   }
 
@@ -70,26 +124,63 @@ public class PrepareTreeStructure {
    * @param interFaultCutOffDistance
    * @param adjacentFaultNames
    */
-  private String getAdjacentFaultSectionNode(Node node) {
+  private HashMap getAdjacentFaultSectionNode(Node node) {
     Iterator it = faultTree.keySet().iterator();
+    HashMap adjacentSectionsAndDist = new HashMap();
     while(it.hasNext()) {
       String sectionName = (String)it.next();
       if(sectionName.equalsIgnoreCase(node.getFaultSectionName())) continue;
       Node locationNode = (Node)this.faultTree.get(sectionName);
+      double minDist= Double.MAX_VALUE;
+      // loop over all locations of this section to find nearest location
       while(locationNode!=null) {
-        if(RelativeLocation.getHorzDistance(node.getLoc(), locationNode.getLoc())<=FAULT_JUMP_CUTOFF_DIST)
-          return sectionName;
+        double currDistance = RelativeLocation.getHorzDistance(node.getLoc(), locationNode.getLoc());
+        if(currDistance<=FAULT_JUMP_CUTOFF_DIST && currDistance<minDist)
+          minDist = currDistance;
         ArrayList nextNodes = (ArrayList)locationNode.getNext();
         if(nextNodes==null || nextNodes.size()==0) break;
         locationNode = (Node)nextNodes.get(0);
       }
+      // if there is atleast on location on this section which is near, then put it in hashmap
+      if(minDist<Double.MAX_VALUE) {
+        adjacentSectionsAndDist.put(sectionName, new SectionNodeDist(sectionName, node, minDist));
+      }
     }
-    return null;
+    return adjacentSectionsAndDist;
   }
-
-
 
   public static void main(String args[]) {
     new PrepareTreeStructure();
   }
+}
+
+
+/**
+ * <p>Title: SectionNodeDist.java </p>
+ * <p>Description: This class saves the section near to a location and the distance
+ * between location and nearest location on other section</p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: </p>
+ * @author not attributable
+ * @version 1.0
+ */
+class SectionNodeDist {
+  private String sectionName;
+  private Node node;
+  private double distance;
+
+  public SectionNodeDist(String sectionName, Node node, double dist) {
+    setSectionName(sectionName);
+    setNode(node);
+    setDistance(dist);
+  }
+
+  public String getSectionName() { return this.sectionName; }
+  public Node getNode() { return this.node; }
+  public double getDistance() { return this.distance; }
+
+  public void setSectionName(String sectionName) { this.sectionName = sectionName; }
+  public void setNode(Node node) { this.node = node; }
+  public void setDistance(double dist) { this.distance = dist; }
+
 }
