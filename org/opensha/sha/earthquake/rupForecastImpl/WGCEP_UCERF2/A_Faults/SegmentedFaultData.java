599287aa4c49c236eed61258c190e5eebe91a99c
Ned Field <field@usgs.gov>
2006-07-12 22:53:36 +0000

    *** empty log message ***

--- org/opensha/sha/earthquake/rupForecastImpl/WGCEP_UCERF2/A_Faults/SegmentedFaultData.java
+++ org/opensha/sha/earthquake/rupForecastImpl/WGCEP_UCERF2/A_Faults/SegmentedFaultData.java
@@ -10,7 +10,7 @@ import org.opensha.calc.FaultMomentCalc;
 import org.opensha.refFaultParamDb.vo.FaultSectionPrefData;
 
 /**
- * @author field
+ * @author Vipin Gupta and Ned Field
  *
  */
 public class SegmentedFaultData {
@@ -23,13 +23,21 @@ public class SegmentedFaultData {
 
 	
 	/**
-  	 * Description:
+  	 * Description: This class contains data for a number of contiguous fault segments, 
+  	 * where each segment is composed of one or more contiguous fault sections 
+  	 * (or FaultSectionPrefData objects).  This class also provides various derived data.
+  	 * Note that areas (or slip rates if aseisReducesArea is false)  will be reduced by any 
+  	 * non-zero fault-section aseimicity factors; the same is true for any values derived 
+  	 * from these.  Segment slip rates represent a weight-average over the sections, where
+  	 * the weights are section areas (and aseismicity factors are applied as specified).
+  	 * All data provided in the get methods are in SI units, which generally differs from the
+  	 * units in the input.
   	 * 
   	 * @param segmentData - an ArrayList containing N ArrayLists (one for each segment), 
   	 * where the arrayList for each segment contains some number of FaultSectionPrefData objects.
   	 * It is assumed that these are in proper order such that concatenating the FaultTraces will produce
   	 * a total FaultTrace with locations in the proper order.
-  	 * @param aseisReducesArea - if true apply asiesmicFactor as reduction of area, otherwise as reduction of slip rate
+  	 * @param aseisReducesArea - if true apply asiesmicFactor as reduction of area, otherwise it reduces slip rate
   	 * @
   	 */
 	public SegmentedFaultData(ArrayList segmentData, boolean aseisReducesArea) {
@@ -39,8 +47,9 @@ public class SegmentedFaultData {
 	}
 	
 	/**
-	 * Get the total area of all segments combined in sq km 
-	 * @return
+	 * Get the total area of all segments combined (note that this 
+	 * is reduced by any non-zero aseismicity factors if aseisReducesArea is true)
+	 * @return area in SI units (meters squared)
 	 */
 	public double getTotalArea() {
 		return totalArea;
@@ -55,9 +64,10 @@ public class SegmentedFaultData {
 	}
 	
 	/**
-	 * Get segment area by index
+	 * Get segment area by index - SI units (note that this is reduce by any 
+	 * non-zero aseismicity factors if aseisReducesArea is true)
 	 * @param index
-	 * @return
+	 * @return area in SI units (meters squared) 
 	 */
 	public double getSegmentArea(int index) {
 		return segArea[index];
@@ -66,33 +76,36 @@ public class SegmentedFaultData {
 	/**
 	 * Get segment length by index
 	 * @param index
-	 * @return
+	 * @return length in SI units (meters)
 	 */
 	public double getSegmentLength(int index) {
 		return this.segLength[index];
 	}
 	
 	/**
-	 * Get segment slip rate by index
+	 * Get segment slip rate by index (note that this is reduce by any non-zero 
+	 * aseismicity factors if aseisReducesArea is false)
 	 * @param index
-	 * @return
+	 * @return slip rate in SI units (m/sec)
 	 */
 	public double getSegmentSlipRate(int index) {
 		return segSlipRate[index];
 	}
 	
 	/**
-	 * Get segment moment rate by index
+	 * Get segment moment rate by index (note that this 
+	 * is reduce by any non-zero aseismicity factors)
 	 * @param index
-	 * @return
+	 * @return moment rate in SI units
 	 */
 	public double getSegmentMomentRate(int index) {
 		return segMoRate[index];
 	}
 	
 	/**
-	 * Get total Moment rate for all segments combined
-	 * @return
+	 * Get total Moment rate for all segments combined (note that this 
+	 * is reduce by any non-zero aseismicity factors)
+	 * @return total moment rate in SI units
 	 */
 	public double getTotalMomentRate() {
 		return totalMoRate;
@@ -132,22 +145,22 @@ public class SegmentedFaultData {
 				else segName[seg]+=sectData.getSectionName();
 				//set the area & moRate
 				double length = sectData.getLength(); // km
-				segLength[seg]+=length;
-				double ddw = sectData.getDownDipWidth(); //km
+				segLength[seg]+=length*1e3;  // converted to meters
+				double ddw = sectData.getDownDipWidth()*1e3; // converted to meters
+				double area = ddw*length; // converted to meters-squared
+				double slipRate = sectData.getAveLongTermSlipRate()*1e-3;  // converted to m/sec
+				double alpha = 1.0 - sectData.getAseismicSlipFactor();  // reduction factor
 				if(aseisReducesArea) {
-					double area = length*ddw*(1-sectData.getAseismicSlipFactor())*KM_TO_METERS_CONVERT; // meters-squared
-					segArea[seg] += area;
-					segMoRate[seg] += FaultMomentCalc.getMoment(area, 
-							sectData.getAveLongTermSlipRate()*1e-3); // SI units
+					segArea[seg] += area*alpha;
+					segMoRate[seg] += FaultMomentCalc.getMoment(area*alpha,slipRate); // SI units
 				}
 				else {
-					double area  = length*ddw*KM_TO_METERS_CONVERT;
 					segArea[seg] +=  area;// meters-squared
-					segMoRate[seg] += FaultMomentCalc.getMoment(area, 
-							sectData.getAveLongTermSlipRate()*1e-3*(1-sectData.getAseismicSlipFactor())); // SI units
+					segMoRate[seg] += FaultMomentCalc.getMoment(area,slipRate*alpha); // SI units
 				}
 				
 			}
+			// segment slip rate is an average weighted by the section areas
 			segSlipRate[seg] = FaultMomentCalc.getSlip(segArea[seg], segMoRate[seg]);
 			totalArea+=segArea[seg];
 			totalMoRate+=segMoRate[seg];
