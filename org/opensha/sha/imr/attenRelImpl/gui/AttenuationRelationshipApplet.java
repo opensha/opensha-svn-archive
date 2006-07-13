2959bec44ebe99805a7a8c73f3340aa7625726fa
niting <niting>
2006-07-13 21:30:45 +0000

    *** empty log message ***

--- org/opensha/sha/imr/attenRelImpl/gui/AttenuationRelationshipApplet.java
+++ org/opensha/sha/imr/attenRelImpl/gui/AttenuationRelationshipApplet.java
@@ -199,14 +199,14 @@ public class AttenuationRelationshipApplet extends JFrame
      *  loader to load these
      */
     static {
-      /*imNames.add(AS_2005_prelim_AttenRel.NAME);
+      imNames.add(AS_2005_prelim_AttenRel.NAME);
       attenRelClasses.add(AS_2005_CLASS_NAME);
       imNames.add(CY_2005_prelim_AttenRel.NAME);
       attenRelClasses.add(CY_2005_CLASS_NAME);
       imNames.add(CB_2005_prelim_AttenRel.NAME);
       attenRelClasses.add(CB_2005_CLASS_NAME);
       imNames.add(Boore_2005_prelim_AttenRel.NAME);
-      attenRelClasses.add(BOORE_2005_CLASS_NAME);*/
+      attenRelClasses.add(BOORE_2005_CLASS_NAME);
       imNames.add(CS_2005_AttenRel.NAME);
       attenRelClasses.add(CS_CLASS_NAME);
       imNames.add(BJF_1997_AttenRel.NAME);
