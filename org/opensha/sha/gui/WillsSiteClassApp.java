16fa9d24dd8ef78c71db8bb0d3529edfdd78272e
niting <niting>
2006-08-24 21:35:17 +0000

    *** empty log message ***

---
+++ org/opensha/sha/gui/WillsSiteClassApp.java
@@ -0,0 +1,198 @@
+package org.opensha.sha.gui;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+import org.opensha.data.Location;
+import org.opensha.data.LocationList;
+import org.opensha.sha.gui.infoTools.ConnectToCVM;
+
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+
+
+/**
+ * <p>Title: WillsSiteClassApp</p>
+ * <p>Description: This window allows the user to enter List of Lat-Lons and this application will
+ * retrieve Wills Site class for them and show it in a seperete window.</p>
+ * @author Nitin Gupta
+ * @version 1.0
+ */
+
+public class WillsSiteClassApp extends JFrame{
+ 
+
+  private JScrollPane locListScrollPane = new JScrollPane();
+  private JTextArea locsTextArea = new JTextArea();
+  private JLabel enterLocs = new JLabel("Enter Locations: ");
+  private JLabel exampleLocs = new JLabel();
+
+  private JButton getWillsSiteClassButton = new JButton();
+
+ 
+  private void init(){
+	  try{
+		 jbInit(); 
+	  }catch(Exception e){
+		  
+	  }
+  }
+  private void jbInit() throws Exception {
+ 
+    this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    this.setTitle("Wills Site Class Application");
+    this.setLayout(new BorderLayout());
+    this.getContentPane().setLayout(new GridBagLayout());
+    
+    getWillsSiteClassButton.setText("Get Wills Site Class");
+    getWillsSiteClassButton.addActionListener(new java.awt.event.ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+    	  getWillsSiteClassButton_actionPerformed(e);
+      }
+    });
+    this.getContentPane().add(enterLocs,  new GridBagConstraints(0,0 , 1, 1, 1.0, 0.0
+            ,GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+    exampleLocs.setText("Enter Location Per Line Example:");
+    exampleLocs.setToolTipText("Enter one Location per line \n"+
+    		"and one can enter as many location as one wants");
+    JLabel firstLocExample = new JLabel();
+    firstLocExample.setText("34.00   -118.20");
+    JLabel secLocExample = new  JLabel();
+    secLocExample.setText("34.40   -119.00");
+    
+    this.getContentPane().add(exampleLocs,  new GridBagConstraints(0,1 , 1, 1, 1.0, 0.0
+            ,GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+    this.getContentPane().add(firstLocExample,  new GridBagConstraints(0,2 , 1, 1, 1.0, 0.0
+            ,GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+    this.getContentPane().add(secLocExample,  new GridBagConstraints(0,3 , 1, 1, 1.0, 0.0
+            ,GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+    this.getContentPane().add(locListScrollPane,  new GridBagConstraints(0,4 , 1, 1, 1.0, 1.0
+            ,GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 214, 15));
+    
+    this.getContentPane().add(getWillsSiteClassButton,  new GridBagConstraints(0, 5, 1, 1, 1.0, 0.0
+            ,GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+    
+    locListScrollPane.getViewport().add(locsTextArea, null);
+    String info = new String("This uses both the CGS Preliminary Site "+
+            "Conditions Map of CA (Wills et al., 2000) ");
+    getWillsSiteClassButton.setToolTipText(info);
+    // set the size
+    this.setSize(new Dimension(250, 300));
+    Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+    this.setLocation((dim.width - this.getSize().width) / 2, 0);
+  }
+
+
+  /**
+   * Retrives the Wills Site class values for the list of locations entered by the user.
+   * @param e
+   */
+  void getWillsSiteClassButton_actionPerformed(ActionEvent e) {
+	  LocationList locs = getLocs();
+	  if(locs.size() >0){
+		  ArrayList willsSiteClassList = null;
+		  try{
+		      //getting the wills site class values from servlet
+		      willsSiteClassList = ConnectToCVM.getWillsSiteTypeFromCVM(locs);
+		    }catch(Exception ee){
+		     	JOptionPane.showMessageDialog(this,"Error connecting to the SCEC WebServer","Network Problem",
+		  				JOptionPane.INFORMATION_MESSAGE);
+		     	return;
+		    }
+		    showWillsSiteClassValsInWindow(willsSiteClassList);
+	  }
+  }
+
+  /**
+   * Shows the Wills Site Class values retrieved from the webservice.
+   *
+   */
+  private void showWillsSiteClassValsInWindow(ArrayList willsSiteClassList){
+	  JFrame willSiteClassFrame = new JFrame();
+	  willSiteClassFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	  willSiteClassFrame.setTitle("Wills Class Values");
+	  willSiteClassFrame.setLayout(new BorderLayout());
+	  willSiteClassFrame.getContentPane().setLayout(new GridBagLayout());
+	  JScrollPane willsSiteValsScroll =  new JScrollPane();
+	  JTextArea willsSiteValsText = new JTextArea();
+	  willsSiteValsText.setEditable(false);
+	  String willSiteClassVals="";
+	  int size = willsSiteClassList.size();
+	  String locs = locsTextArea.getText();
+	  StringTokenizer st = new StringTokenizer(locs,"\n");
+	  for(int i=0;i<size;++i){
+		  String willsVal = (String)willsSiteClassList.get(i);
+		  if(willsVal.equals("NA"))
+			  willsVal = "Not Available";
+  		  willSiteClassVals +=st.nextToken()+"  "+willsVal+"\n";
+	  }
+	  
+	  String infoText = "#Lat Lon WillsSiteClass-Value\n";
+	  willsSiteValsText.setText(infoText+willSiteClassVals);
+	  willSiteClassFrame.getContentPane().add(willsSiteValsScroll,  new GridBagConstraints(0,0 , 1, 1, 1.0, 1.0
+	            ,GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 214, 15));
+	    
+	  willsSiteValsScroll.getViewport().add(willsSiteValsText, null);
+	  willSiteClassFrame.setSize(200,300);
+	  // show the window at center of the parent component
+	  willSiteClassFrame.setLocation(this.getX()+this.getWidth()/2,
+                       this.getY()+this.getHeight()/2);
+	  willSiteClassFrame.setVisible(true);
+  }
+  
+/**
+ * Creates the LocationList from the location entered by the user in the TextArea.
+ * @return
+ */
+  private LocationList getLocs(){
+	  LocationList locs = new LocationList();
+	  String locsText = locsTextArea.getText();
+	  if(locsText ==null || locsText.trim().equals("")){
+		JOptionPane.showMessageDialog(this,"Please Enter valid Locations in text Area, see example","Input Error",
+				JOptionPane.ERROR_MESSAGE);
+		
+		return locs;
+	  }
+	  //getting the locations entered by the user in the textArea and adding it the locList object.
+	  	StringTokenizer st = new StringTokenizer(locsText,"\n");
+	    while(st.hasMoreTokens()){
+	
+	      StringTokenizer st1 = new StringTokenizer(st.nextToken());
+	      int numVals = st1.countTokens();
+	      if(numVals !=2){
+	    	  JOptionPane.showMessageDialog(this,"Each line should have just one Lat and "+
+	                  "one Lon value.\nPlease see exmaple above and enter just one location per line","Input Error",
+	  				JOptionPane.ERROR_MESSAGE);
+	        return locs;
+	      }
+	      double lat=0;
+	      double lon=0;
+	      try{
+	        lat = Double.parseDouble(st1.nextToken());
+	        lon = Double.parseDouble(st1.nextToken());
+	      }catch(NumberFormatException e){
+	    	  JOptionPane.showMessageDialog(this,"Lat and Lon Values entered must be valid numbers","Input Error",
+	  				JOptionPane.ERROR_MESSAGE);
+	        return locs;
+	      }
+	      locs.addLocation(new Location(lat,lon));
+	    }
+	    return locs;
+  }
+
+  /**
+   * 
+   * application main method
+   * @param args
+   */
+  public static void main(String[] args) {
+	  WillsSiteClassApp applet = new WillsSiteClassApp();
+      applet.setVisible(true);
+      applet.init();
+  }
+  
+  
+}
