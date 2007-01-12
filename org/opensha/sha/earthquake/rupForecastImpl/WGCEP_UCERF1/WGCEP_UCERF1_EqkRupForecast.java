c73c808611092fa18a5251cec0b025e2e180ef3d
Ned Field <field@usgs.gov>
2007-01-12 21:47:04 +0000

    *** empty log message ***

--- org/opensha/sha/earthquake/rupForecastImpl/WGCEP_UCERF1/WGCEP_UCERF1_EqkRupForecast.java
+++ org/opensha/sha/earthquake/rupForecastImpl/WGCEP_UCERF1/WGCEP_UCERF1_EqkRupForecast.java
@@ -1118,6 +1118,43 @@ public class WGCEP_UCERF1_EqkRupForecast extends EqkRupForecast{
      }
      return mo;
    }
+   
+   /**
+    * This assumes default values are used for the calculation
+    *
+    */
+   public void writeA_FaultMFDs() {
+	   int sjf_sources[] = {1,2,3,4,5,6};
+	   int elsinore_sources[] = {7,8,9,10,11};
+	   int ssaf_sources[] = {0,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38};
+	   int nsaf_sources[] = {39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,
+			   67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,390};
+	   int hrc_sources[] = {95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,
+			   120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,391};
+	   int calaveras_sources[] = {144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,
+			   164,165,166,167,168,392};
+	   
+	   int index, rup;
+	   double duration = timeSpan.getDuration();
+	   ProbEqkSource probSrc;
+	   ProbEqkRupture probRup;
+	   
+	   // SJF MFD
+	   String faultname = "SJF Fault MFD from UCERF 1";
+	   System.out.println(faultname);
+	   SummedMagFreqDist sjf_MFD = new SummedMagFreqDist(5.0,71,.05);
+	   for(index=0; index<sjf_sources.length; index++){
+		   probSrc = getSource(sjf_sources[index]);
+		   // for debugging to make sure correct sources are used
+		   System.out.println(probSrc.getName());
+		   for(rup=0; rup<probSrc.getNumRuptures(); rup++){
+			   probRup = probSrc.getRupture(rup);
+			   sjf_MFD.add(probRup.getMag(), probRup.getMeanAnnualRate(duration));
+		   }
+	   }
+	   // write name and MFD to file
+	   // System.out.println(sjf_MFD.toString());
+   }
 
 
    // this is temporary for testing purposes
@@ -1125,6 +1162,10 @@ public class WGCEP_UCERF1_EqkRupForecast extends EqkRupForecast{
 
      WGCEP_UCERF1_EqkRupForecast frankCast = new WGCEP_UCERF1_EqkRupForecast();
      frankCast.updateForecast();
+     
+     frankCast.timeDependentParam.setValue(new Boolean(true));
+     frankCast.writeA_FaultMFDs();
+/*
      try {
        frankCast.writeRuptureTraces();
      }
@@ -1139,6 +1180,8 @@ public class WGCEP_UCERF1_EqkRupForecast extends EqkRupForecast{
        ProbEqkSource src = (ProbEqkSource) frankCast.getSource(i);
        System.out.println(i+"\t"+src.getName());
      }
+*/
+     
 //     System.out.println("num sources="+frankCast.getNumSources());
 /*     ArrayList srcs = frankCast.getAllGR_FaultSources();
      for(int i=0; i<srcs.size(); i++) {
