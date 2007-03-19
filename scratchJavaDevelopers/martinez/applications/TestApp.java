dc24702f5d71e5eeebe1ad034b32effaf15df20c
martinez <martinez>
2007-03-19 18:15:32 +0000

    *** empty log message ***

--- scratchJavaDevelopers/martinez/applications/TestApp.java
+++ scratchJavaDevelopers/martinez/applications/TestApp.java
@@ -1,28 +1,28 @@
 package scratchJavaDevelopers.martinez.applications;
 
-import javax.swing.JFrame;
+import java.util.ArrayList;
+import java.util.TreeMap;
 
-import scratchJavaDevelopers.martinez.beans.ExceptionBean;
-import scratchJavaDevelopers.martinez.beans.GuiBeanAPI;
+import scratchJavaDevelopers.martinez.util.DBAccessor;
 
 public class TestApp {
 	public static void main(String[] args) {
-		try {
-			foo(null);
-		} catch (Exception ex) {
-			ExceptionBean eb = new ExceptionBean(ex.getMessage(), "Error", ex);
-			((JFrame) eb.getVisualization(GuiBeanAPI.SPLASH)).setVisible(true);
-		} finally {
-			System.out.println("This comes after the exception bean is shown!");
+		String[] gldintdb = {"oracle.jdbc.OracleDriver", "localhost", 
+				"1523", "TEAMT", "hc_owner", "ham23*ret"};
+		DBAccessor db = new DBAccessor(gldintdb, true);
+		if(db.hasValidConnection()) {
+			String sql = "SELECT * FROM HC_ANALYSIS_OPT ORDER BY DISPLAY_SEQ";
+			ArrayList<TreeMap<String, String>> result = db.doQuery(sql);
+			doPrint(result);
+		} else {
+			System.err.println("Failed to get connection!");
 		}
 	}
 	
-	private static void foo(int[] a) {
-		bar(a);
-	}
-	
-	private static void bar(int[] b) {
-		JFrame frame = null;
-		frame.setVisible(true);
+	private static void doPrint(ArrayList<TreeMap<String, String>> result) {
+		System.out.println("Analysis Options");
+		for(int i = 0; i < result.size(); ++i) {
+				System.out.println(result.get(i).get("ANALYSIS_OPT_DISPLAY"));
+		}
 	}
 }
