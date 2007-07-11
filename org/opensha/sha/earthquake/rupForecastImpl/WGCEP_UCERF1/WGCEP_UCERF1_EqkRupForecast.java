3870178c71bedefa79050a824a774e29b8e14572
Ned Field <field@usgs.gov>
2007-07-11 13:21:22 +0000

    *** empty log message ***

--- org/opensha/sha/earthquake/rupForecastImpl/WGCEP_UCERF1/WGCEP_UCERF1_EqkRupForecast.java
+++ org/opensha/sha/earthquake/rupForecastImpl/WGCEP_UCERF1/WGCEP_UCERF1_EqkRupForecast.java
@@ -1137,7 +1137,11 @@ public class WGCEP_UCERF1_EqkRupForecast extends EqkRupForecast{
 			   120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,142,390};
 	   int calaveras_sources[] = {143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,
 			   164,165,166,167,391,392};
-	   
+	   int con_greenValley_sources[] = {168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,
+			   188,189,190,191,192,193,194,195,196,197,198,199,200,201,202,203,204,205,393};
+	   int sanGregorio_sources[] = {206,207,208,209,210,211,212,213,214,215,216,217,218,219,220,221,222,223,224,225,394};
+	   int greenville_sources[] = {226,227,228,229,230,231,232,233,234,235,236,237,238,239,240,241,242,243,244,245,246,247,248,249,250,251,395};
+	   int mtDiablo_sources[] = {252,253,254,255,256,257,258,259,260};
 	   /*  Here are the others if and when I want them:
 	    
 Concord Green Valley
@@ -1259,8 +1263,14 @@ Mount Diablo (no floater here)
 	   srcList.add(ssaf_sources);
 	   srcList.add(hrc_sources);
 	   srcList.add(calaveras_sources);
+	   srcList.add(con_greenValley_sources);
+	   srcList.add(sanGregorio_sources);
+	   srcList.add(greenville_sources);
+	   srcList.add(mtDiablo_sources);
+
 	   
-	   String[] faultNames = {"SJF", "Elsinore", "N-SAF", "S-SAF", "HRC", "Calaveras" };
+	   String[] faultNames = {"SJF", "Elsinore", "N-SAF", "S-SAF", "HRC", "Calaveras", 
+			   "Concord-GreenValley", "San Gregorio", "Greenville","Mt Diablo" };
 	   int index, rup;
 	   double duration = timeSpan.getDuration();
 	   ProbEqkSource probSrc;
