d752621e0d389bf03e2fdb10ee9b21ec88b1496b
vgupta <vgupta>
2007-07-24 14:52:33 +0000

    *** empty log message ***

--- org/opensha/sha/earthquake/rupForecastImpl/WGCEP_UCERF_2_2/UCERF2.java
+++ org/opensha/sha/earthquake/rupForecastImpl/WGCEP_UCERF_2_2/UCERF2.java
@@ -535,6 +535,7 @@ public class UCERF2 extends EqkRupForecast {
 		magAreaRelationships.add(new Ellsworth_B_WG02_MagAreaRel());
 		magAreaRelationships.add(new HanksBakun2002_MagAreaRel());
 		magAreaRelationships.add(new Somerville_2006_MagAreaRel());
+		magAreaRelationships.add(new WGCEP_2007_PowLaw_MagAreaRel());
 		//magAreaRelationships.add(new WC1994_MagAreaRelationship());
 		// array List of Mag Area Rel names
 		ArrayList magAreaNamesList = new ArrayList();
