593ee10f46405e62bc503b03a49522a758fcfdf8
vgupta <vgupta>
2007-09-13 20:14:05 +0000

    *** empty log message ***

--- org/opensha/sha/earthquake/rupForecastImpl/WGCEP_UCERF_2_3/EmpiricalModel.java
+++ org/opensha/sha/earthquake/rupForecastImpl/WGCEP_UCERF_2_3/EmpiricalModel.java
@@ -1,13 +1,14 @@
 package org.opensha.sha.earthquake.rupForecastImpl.WGCEP_UCERF_2_3;
 
 import org.opensha.data.Location;
+import org.opensha.sha.earthquake.rupForecastImpl.WGCEP_UCERF_2_2.data.EmpiricalModelDataFetcher;
 
 /**
  * 
  * @author field
  */
 public class EmpiricalModel {
-
+	private EmpiricalModelDataFetcher empiricalModelDataFetcher = new EmpiricalModelDataFetcher();
 	/**
 	 * This returns the ratio of short term rate to long term rate
 	 * 
@@ -15,6 +16,14 @@ public class EmpiricalModel {
 	 * @return
 	 */
 	 public  double getCorrection(Location loc) {
+		 /*int numPolygons = empiricalModelDataFetcher.getNumRegions();
+		 for(int i=0; i<(numPolygons-1); ++i) { //loop over all empirical regions except rest of california
+			 if(empiricalModelDataFetcher.getRegion(i).isLocationInside(loc))
+				 return empiricalModelDataFetcher.getRate(i);
+		 }
+		 // return for rest of California
+		 return empiricalModelDataFetcher.getRate(numPolygons-1);*/
+		 
 		 return 1;
 	 }
 }
