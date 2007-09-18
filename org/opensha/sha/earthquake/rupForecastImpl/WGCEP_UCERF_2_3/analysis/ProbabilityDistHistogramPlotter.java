44c9330cf7c5448be6751e96836561e860439196
vgupta <vgupta>
2007-09-18 21:05:51 +0000

    *** empty log message ***

--- org/opensha/sha/earthquake/rupForecastImpl/WGCEP_UCERF_2_3/analysis/ProbabilityDistHistogramPlotter.java
+++ org/opensha/sha/earthquake/rupForecastImpl/WGCEP_UCERF_2_3/analysis/ProbabilityDistHistogramPlotter.java
@@ -11,6 +11,7 @@ import java.util.Iterator;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.opensha.data.function.EvenlyDiscretizedFunc;
 import org.opensha.param.ParameterAPI;
 import org.opensha.param.ParameterList;
@@ -35,45 +36,46 @@ public class ProbabilityDistHistogramPlotter implements GraphWindowAPI {
 	private final static String X_AXIS_LABEL = "Probability";
 	private final static String Y_AXIS_LABEL = "Contribution";
 	private final static String PLOT_LABEL = "Probability Contribution";
-	
+
 	private final PlotCurveCharacterstics PLOT_HISTOGRAM = new PlotCurveCharacterstics(PlotColorAndLineTypeSelectorControlPanel.HISTOGRAM,
-		      new Color(0,0,0), 2); // black
-	
+			new Color(0,0,0), 2); // black
+
 	private ArrayList funcs;
 	private ArrayList<PlotCurveCharacterstics> plottingCurveChars;
 	private HSSFWorkbook workbook;
-	
+
 	private double mags[] = { 5.0, 6.0, 6.5, 6.7, 7.0, 7.5, 8.0};
 	private String[] sources = { "A-Faults", "B-Faults", "Non-CA B-Faults", "C-Zones", "Background", "Total" };
+	UCERF2_TimeDependentEpistemicList ucerf2EpistemicList = new UCERF2_TimeDependentEpistemicList();
 	/**
 	 * Plot histograms of probability contributions from various branches
 	 * 
 	 * @param minMag
 	 */
-	public void generateTotalProbHistogramExcelSheet(double duration, String fileName) {
-		UCERF2_TimeDependentEpistemicList ucerf2EpistemicList = new UCERF2_TimeDependentEpistemicList();
-		
+	public void generateProbContributionsExcelSheet(double duration, String fileName) {
+
+
 		//	create a sheet that contains param settings for each logic tree branch
 		workbook  = new HSSFWorkbook();
-		createParamSettingsSheet(ucerf2EpistemicList);
-		
-		
+		createParamSettingsSheet();
+
+
 		int numSources = sources.length; // A-Faults, B-Faults, Non-CA B-Faults, Background, C-Zones, Total
-		
+
 		HSSFSheet allCA_RegionSheet = workbook.createSheet("All CA Region");
 		HSSFRow row1, row2;
-		
+
 		// add column for each magnitude and each source
-		 row1 = allCA_RegionSheet.createRow(0);
-		 row2 = allCA_RegionSheet.createRow(1);
-		 for(int magIndex=0; magIndex<mags.length; ++magIndex) {
-			 int colIndex = magIndex*numSources+1;
-			 row1.createCell((short)(colIndex)).setCellValue(" Mag "+mags[magIndex]);
-			 for(int srcIndex=0; srcIndex<numSources; ++srcIndex) // each source for this magnitude
-				 row2.createCell((short)(colIndex+srcIndex)).setCellValue(sources[srcIndex]);
-		 }
-		
-		
+		row1 = allCA_RegionSheet.createRow(0);
+		row2 = allCA_RegionSheet.createRow(1);
+		for(int magIndex=0; magIndex<mags.length; ++magIndex) {
+			int colIndex = magIndex*numSources+1;
+			row1.createCell((short)(colIndex)).setCellValue(" Mag "+mags[magIndex]);
+			for(int srcIndex=0; srcIndex<numSources; ++srcIndex) // each source for this magnitude
+				row2.createCell((short)(colIndex+srcIndex)).setCellValue(sources[srcIndex]);
+		}
+
+
 		int numERFs = ucerf2EpistemicList.getNumERFs(); // number of logic tree branches
 		//	now write the probability contribtuions of each source in each branch of logic tree
 		int startRowIndex = 2;
@@ -85,73 +87,102 @@ public class ProbabilityDistHistogramPlotter implements GraphWindowAPI {
 			ucerf2.getTimeSpan().setDuration(duration);
 			ucerf2.updateForecast();
 			for(int magIndex=0; magIndex<mags.length; ++magIndex) {
-				 int colIndex = magIndex*numSources+1;
-				 double mag = mags[magIndex];
-				 row1.createCell((short)(colIndex++)).setCellValue(ucerf2.getTotal_A_FaultsProb(mag));
-				 row1.createCell((short)(colIndex++)).setCellValue(ucerf2.getTotal_B_FaultsProb(mag));
-				 row1.createCell((short)(colIndex++)).setCellValue(ucerf2.getTotal_NonCA_B_FaultsProb(mag));
-				 row1.createCell((short)(colIndex++)).setCellValue(ucerf2.getTotal_C_ZoneProb(mag));
-				 row1.createCell((short)(colIndex++)).setCellValue(ucerf2.getTotal_BackgroundProb(mag));
-				 row1.createCell((short)(colIndex++)).setCellValue(ucerf2.getTotalProb(mag));
+				int colIndex = magIndex*numSources+1;
+				double mag = mags[magIndex];
+				row1.createCell((short)(colIndex++)).setCellValue(ucerf2.getTotal_A_FaultsProb(mag));
+				row1.createCell((short)(colIndex++)).setCellValue(ucerf2.getTotal_B_FaultsProb(mag));
+				row1.createCell((short)(colIndex++)).setCellValue(ucerf2.getTotal_NonCA_B_FaultsProb(mag));
+				row1.createCell((short)(colIndex++)).setCellValue(ucerf2.getTotal_C_ZoneProb(mag));
+				row1.createCell((short)(colIndex++)).setCellValue(ucerf2.getTotal_BackgroundProb(mag));
+				row1.createCell((short)(colIndex++)).setCellValue(ucerf2.getTotalProb(mag));
 			}	
 		}
-		 
-//		 write metadata excel sheet
-			try {
-				FileOutputStream fileOut = new FileOutputStream(fileName);
-				workbook.write(fileOut);
-				fileOut.close();
-			}catch(Exception e) {
-				e.printStackTrace();
-			}
-		
-		/*EvenlyDiscretizedFunc func = new EvenlyDiscretizedFunc(MIN_PROB, MAX_PROB, NUM_PROB);
-		func.setTolerance(DELTA_PROB);
-		
-		int numERFs = ucerf2EpistemicList.getNumERFs(); 
-		
-		for(int i=0; i<numERFs; ++i) {
-			System.out.println("Doing run "+(i+1)+" of "+numERFs);
-			UCERF2 ucerf2 = (UCERF2)ucerf2EpistemicList.getERF(i);
-			ucerf2.updateForecast();
-			double wt= ucerf2EpistemicList.getERF_RelativeWeight(i);
-			double prob = ucerf2.getTotalProb(minMag);
-			func.add(prob, wt);
+
+		//		 write  excel sheet
+		try {
+			FileOutputStream fileOut = new FileOutputStream(fileName);
+			workbook.write(fileOut);
+			fileOut.close();
+		}catch(Exception e) {
+			e.printStackTrace();
 		}
-		funcs = new ArrayList();
-		funcs.add(func);
-		plottingCurveChars = new ArrayList<PlotCurveCharacterstics>();
-		plottingCurveChars.add(PLOT_HISTOGRAM);
-		GraphWindow graphWindow= new GraphWindow(this);
-		graphWindow.setPlotLabel(PLOT_LABEL);
-		graphWindow.plotGraphUsingPlotPreferences();
-		graphWindow.setVisible(true);*/
 	}
-	
-	
+
+	/**
+	 * Plot Histogram of total Probs 
+	 * 
+	 * @param minMag
+	 * @param fileName
+	 */
+	public void plotTotalProbHistogramsAboveMag(double minMag, String fileName) {
+
+		boolean found = false;
+		int colIndex=-1;
+		// find the column based on the specfied magnitude
+		for(int magIndex=0; magIndex<this.mags.length && !found; ++magIndex) {
+			if(mags[magIndex]==minMag) {
+				colIndex = (magIndex+1)* sources.length;
+				found=true;
+			}
+		}
+		if(!found) throw new RuntimeException("Invalid minimum magnitude. Only 5.0, 6.0, 6.5, 6.7, 7.0, 7.5, 8.0 are allowed");
+
+		// Open the excel file
+		try {
+			POIFSFileSystem fs = new POIFSFileSystem(getClass().getClassLoader().getResourceAsStream(fileName));
+			HSSFWorkbook wb = new HSSFWorkbook(fs);
+			HSSFSheet sheet = wb.getSheetAt(1); // whole CA region
+
+			int startRowIndex = 2;
+			EvenlyDiscretizedFunc func = new EvenlyDiscretizedFunc(MIN_PROB, MAX_PROB, NUM_PROB);
+			func.setInfo("Total Probability histogram plot for "+fileName+" for Mag>="+minMag);
+			func.setTolerance(DELTA_PROB);
+
+			int numERFs = ucerf2EpistemicList.getNumERFs(); 
+			for(int i=0; i<numERFs; ++i) {
+				System.out.println("Doing run "+(i+1)+" of "+numERFs);
+				double wt= ucerf2EpistemicList.getERF_RelativeWeight(i);
+				double prob = sheet.getRow(startRowIndex+i).getCell((short)colIndex).getNumericCellValue();
+				func.add(prob, wt);
+			}
+
+			funcs = new ArrayList();
+			funcs.add(func);
+			plottingCurveChars = new ArrayList<PlotCurveCharacterstics>();
+			plottingCurveChars.add(PLOT_HISTOGRAM);
+			GraphWindow graphWindow= new GraphWindow(this);
+			graphWindow.setPlotLabel(PLOT_LABEL);
+			graphWindow.plotGraphUsingPlotPreferences();
+			graphWindow.setVisible(true);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
 	/**
 	 * create a sheet that lists parameter settings for each logic tree branch
 	 *
 	 */
-	private void createParamSettingsSheet(UCERF2_TimeDependentEpistemicList ucerf2EpistemicList) {
-		
+	private void createParamSettingsSheet() {
+
 		HSSFSheet paramSettingsSheet = workbook.createSheet("Parameter Settings");
 		HSSFRow row;
 		ParameterList adjustableParams = ucerf2EpistemicList.getParameterList(0);
 		Iterator it = adjustableParams.getParametersIterator();
 		ArrayList<String> adjustableParamNames = new ArrayList<String>();
 		while(it.hasNext()) {
-			 ParameterAPI param = (ParameterAPI)it.next();
-			 adjustableParamNames.add(param.getName());
-		 }
-		
+			ParameterAPI param = (ParameterAPI)it.next();
+			adjustableParamNames.add(param.getName());
+		}
+
 		// add column for each parameter name. 
-		 row = paramSettingsSheet.createRow(0); 
-		 for(int i=1; i<=adjustableParamNames.size(); ++i) {
+		row = paramSettingsSheet.createRow(0); 
+		for(int i=1; i<=adjustableParamNames.size(); ++i) {
 			row.createCell((short)i).setCellValue(adjustableParamNames.get(i-1));
 		}
-		
-		
+
+
 		int numERFs = ucerf2EpistemicList.getNumERFs(); // number of logic tree branches
 		//		now write all the parameter settings for each branch in the excel sheet
 		for(int i=0; i<numERFs; ++i) {
@@ -163,11 +194,11 @@ public class ProbabilityDistHistogramPlotter implements GraphWindowAPI {
 				if(adjustableParams.containsParameter(pName))
 					row.createCell((short)(paramIndex+1)).setCellValue(adjustableParams.getValue(pName).toString());
 			}
-				
+
 		}
 	}
-	
-	
+
+
 	/* (non-Javadoc)
 	 * @see org.opensha.sha.gui.infoTools.GraphWindowAPI#getCurveFunctionList()
 	 */
@@ -186,7 +217,7 @@ public class ProbabilityDistHistogramPlotter implements GraphWindowAPI {
 	 * @see org.opensha.sha.gui.infoTools.GraphWindowAPI#getYLog()
 	 */
 	public boolean getYLog() {
-		 return false;
+		return false;
 	}
 
 	/* (non-Javadoc)
@@ -202,15 +233,15 @@ public class ProbabilityDistHistogramPlotter implements GraphWindowAPI {
 	public String getYAxisLabel() {
 		return Y_AXIS_LABEL;
 	}
-	
+
 
 	/* (non-Javadoc)
 	 * @see org.opensha.sha.gui.infoTools.GraphWindowAPI#getPlottingFeatures()
 	 */
 	public ArrayList<PlotCurveCharacterstics> getPlottingFeatures() {
-		 return plottingCurveChars;
+		return plottingCurveChars;
 	}
-	
+
 
 	/* (non-Javadoc)
 	 * @see org.opensha.sha.gui.infoTools.GraphWindowAPI#isCustomAxis()
@@ -250,10 +281,11 @@ public class ProbabilityDistHistogramPlotter implements GraphWindowAPI {
 		//return 10;
 		throw new UnsupportedOperationException("Method not implemented yet");
 	}
-	
+
 	public static void main(String[] args) {
 		ProbabilityDistHistogramPlotter plotter = new ProbabilityDistHistogramPlotter();
-		plotter.generateTotalProbHistogramExcelSheet(30, "ProbabilityContributions_30yrs.xls");
+		//plotter.generateProbContributionsExcelSheet(5, "ProbabilityContributions_5yrs.xls");
+		//plotter.plotTotalProbHistogramsAboveMag(8.0, "ProbabilityContributions_30yrs.xls");
 	}
 
 }
