279b9f574f75826a2b937dda7ebb64db1d0beb34
vgupta <vgupta>
2007-09-22 02:49:00 +0000

    *** empty log message ***

--- org/opensha/sha/earthquake/rupForecastImpl/WGCEP_UCERF_2_3/analysis/ProbabilityDistHistogramPlotter.java
+++ org/opensha/sha/earthquake/rupForecastImpl/WGCEP_UCERF_2_3/analysis/ProbabilityDistHistogramPlotter.java
@@ -56,7 +56,7 @@ public class ProbabilityDistHistogramPlotter implements GraphWindowAPI {
 	private String []bFaultNames = { "San Gregorio Connected", "Greenville Connected", 
 			"Green Valley Connected", "Mount Diablo Thrust"};
 	private String []aFaultNames = { "Elsinore", "Garlock", "San Jacinto", "N. San Andreas", "S. San Andreas",
-			"Hayward-Rodgers Creek", "Calaveras"};
+			"Hayward-Rogers Creek", "Calaveras"};
 	
 	/**
 	 * Plot histograms of probability contributions from various branches
