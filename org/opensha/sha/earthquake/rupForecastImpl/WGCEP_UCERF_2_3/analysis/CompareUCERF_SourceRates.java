60c849aeee89a1a1f3100b41ad10e45269bc27b1
vgupta <vgupta>
2007-10-20 23:20:14 +0000

    *** empty log message ***

--- org/opensha/sha/earthquake/rupForecastImpl/WGCEP_UCERF_2_3/analysis/CompareUCERF_SourceRates.java
+++ org/opensha/sha/earthquake/rupForecastImpl/WGCEP_UCERF_2_3/analysis/CompareUCERF_SourceRates.java
@@ -60,6 +60,7 @@ public class CompareUCERF_SourceRates {
 		
 		// Mean UCERF 2
 		MeanUCERF2 meanUCERF2 = new MeanUCERF2();
+		meanUCERF2.setParameter(MeanUCERF2.PROB_MODEL_PARAM_NAME, MeanUCERF2.PROB_MODEL_POISSON);
 		meanUCERF2.setParameter(MeanUCERF2.BACK_SEIS_NAME, MeanUCERF2.BACK_SEIS_EXCLUDE);
 		meanUCERF2.getTimeSpan().setDuration(duration);
 		fw = new FileWriter("MeanUCERF2.txt");
