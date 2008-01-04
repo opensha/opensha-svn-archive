6ff44360e821bbe107c7ea8e5ea3d428d27bd9d3
vgupta <vgupta>
2008-01-04 19:55:30 +0000

    *** empty log message ***

--- org/opensha/sha/imr/attenRelImpl/test/BA_2008_test.java
+++ org/opensha/sha/imr/attenRelImpl/test/BA_2008_test.java
@@ -5,7 +5,7 @@ import java.util.*;
 import org.opensha.param.WarningDoubleParameter;
 import org.opensha.param.event.ParameterChangeWarningEvent;
 import org.opensha.param.event.ParameterChangeWarningListener;
-import org.opensha.sha.imr.attenRelImpl.CB_2008_AttenRel;
+import org.opensha.sha.imr.attenRelImpl.BA_2008_AttenRel;
 import org.opensha.util.FileUtils;
 import org.opensha.sha.param.*;
 
@@ -15,11 +15,11 @@ import java.io.IOException;
 import java.text.*;
 import junit.framework.TestCase;
 
-public class CB_2008_test extends TestCase implements ParameterChangeWarningListener{
+public class BA_2008_test extends TestCase implements ParameterChangeWarningListener{
 
-	private CB_2008_AttenRel cb_2008 = null;
+	private BA_2008_AttenRel ba_2008 = null;
 
-	private static final String RESULT_SET_PATH = "org/opensha/sha/imr/attenRelImpl/AttenRelResultSet/NGA_ModelsTestFiles/CB08/";
+	private static final String RESULT_SET_PATH = "org/opensha/sha/imr/attenRelImpl/AttenRelResultSet/NGA_ModelsTestFiles/BA08/";
 
 	private double[] period={0.010,0.020,0.030,0.050,0.075,0.10,0.15,0.20,0.25,0.30,0.40,0.50,0.75,1.0,1.5,2.0,3.0,4.0,5.0,7.5,10.0};
 
@@ -29,18 +29,18 @@ public class CB_2008_test extends TestCase implements ParameterChangeWarningList
 	private DecimalFormat format = new DecimalFormat("0.####");
 	private ArrayList testDataLines;
 	public static void main(String[] args) {
-		junit.swingui.TestRunner.run(CB_2008_test.class);
+		junit.swingui.TestRunner.run(BA_2008_test.class);
 	}
 
-	public CB_2008_test(String arg0) {
+	public BA_2008_test(String arg0) {
 		super(arg0);
 	}
 
 	protected void setUp() throws Exception {
 		super.setUp();
 		//create the instance of the CB_2006
-		cb_2008 = new CB_2008_AttenRel(this);
-		cb_2008.setParamDefaults();
+		ba_2008 = new BA_2008_AttenRel(this);
+		ba_2008.setParamDefaults();
 		//testDataLines = FileUtils.loadFile(CB_2006_RESULTS);
 	}
 
@@ -70,16 +70,15 @@ public class CB_2008_test extends TestCase implements ParameterChangeWarningList
 				testValString = "Mean";
 			} else { // test Standard Deviation
 				isMedian = false;
-				/* set whether we are testing Std dev of geomteric mean or 
-				 standard deviation of arbitrary horizontal component */
-				if(fileName.contains("SIGARB")) {
-					// Std Dev of arbitrary horizontal component
-					cb_2008.getParameter(cb_2008.COMPONENT_NAME).setValue(cb_2008.COMPONENT_RANDOM_HORZ);
-					testValString = "Std Dev of Arb Horz Comp";
+				
+				if(fileName.contains("SIGTM")) {
+					//Standard deviation of geometric mean for known faulting (evaluated for SS)
+					//ba_2008.getParameter(ba_2008.COMPONENT_NAME).setValue(ba_2008.COMPONENT_RANDOM_HORZ);
+					testValString = "Std Dev for known faulting";
 				} else {
-					//Std dev of geomteric mean 
-					cb_2008.getParameter(cb_2008.COMPONENT_NAME).setValue(cb_2008.COMPONENT_GMRotI50);
-					testValString = "Std dev of geomteric mean";
+					//Standard deviation of geometric mean for unspecified faulting (evaluated for SS)
+					//ba_2008.getParameter(ba_2008.COMPONENT_NAME).setValue(ba_2008.COMPONENT_GMRotI50);
+					testValString = "Std dev for unspecified faulting";
 				}
 			}
 
@@ -87,11 +86,13 @@ public class CB_2008_test extends TestCase implements ParameterChangeWarningList
 			String fltType = fileName.substring(index1-2, index1);
 			
 			if(fltType.equals("SS"))
-				cb_2008.getParameter(cb_2008.FLT_TYPE_NAME).setValue(cb_2008.FLT_TYPE_STRIKE_SLIP);
+				ba_2008.getParameter(ba_2008.FLT_TYPE_NAME).setValue(ba_2008.FLT_TYPE_STRIKE_SLIP);
 			else if(fltType.equals("RV"))
-				cb_2008.getParameter(cb_2008.FLT_TYPE_NAME).setValue(cb_2008.FLT_TYPE_REVERSE);
+				ba_2008.getParameter(ba_2008.FLT_TYPE_NAME).setValue(ba_2008.FLT_TYPE_REVERSE);
 			else if(fltType.equals("NM"))
-				cb_2008.getParameter(cb_2008.FLT_TYPE_NAME).setValue(cb_2008.FLT_TYPE_NORMAL);
+				ba_2008.getParameter(ba_2008.FLT_TYPE_NAME).setValue(ba_2008.FLT_TYPE_NORMAL);
+			else if(fltType.equals("UN"))
+				ba_2008.getParameter(ba_2008.FLT_TYPE_NAME).setValue(ba_2008.FLT_TYPE_UNKNOWN);
 			else 
 				throw new RuntimeException("Unknown Fault Type");
 			
@@ -103,51 +104,39 @@ public class CB_2008_test extends TestCase implements ParameterChangeWarningList
 					String fileLine = (String)testDataLines.get(j);
 					StringTokenizer st = new StringTokenizer(fileLine);
 					double mag = Double.parseDouble(st.nextToken().trim());
-					cb_2008.getParameter(cb_2008.MAG_NAME).setValue(new Double(mag));
+					((WarningDoubleParameter)ba_2008.getParameter(ba_2008.MAG_NAME)).setValueIgnoreWarning(new Double(mag));
 
-					double rrup = Double.parseDouble(st.nextToken().trim());
-					((WarningDoublePropagationEffectParameter)cb_2008.getParameter(DistanceRupParameter.NAME)).setValueIgnoreWarning(new Double(rrup));
+					st.nextToken().trim(); // ignore Rrup
 
 					double rjb = Double.parseDouble(st.nextToken().trim());
-					
-					double distRupMinusJB_OverRup;
-					if(rrup==0 && rjb==0) distRupMinusJB_OverRup=0;
-					else distRupMinusJB_OverRup = (rrup-rjb)/rrup;
-					
-					((WarningDoublePropagationEffectParameter)cb_2008.getParameter(DistRupMinusJB_OverRupParameter.NAME)).setValueIgnoreWarning(new Double(distRupMinusJB_OverRup));
+					((WarningDoublePropagationEffectParameter)ba_2008.getParameter(DistanceJBParameter.NAME)).setValueIgnoreWarning(new Double(rjb));
 					
 					st.nextToken().trim(); // ignore R(x) ( Horizontal distance from top of rupture perpendicular to fault strike)
-					
-					double dip = Double.parseDouble(st.nextToken().trim());
-					cb_2008.getParameter(cb_2008.DIP_NAME).setValue(new Double(dip));
-					
+					st.nextToken().trim(); // ignore Dip
 					st.nextToken().trim(); // ignore W, width of rup plane
+					st.nextToken().trim(); // ignore Depth Top
 					
-					double depthTop = Double.parseDouble(st.nextToken().trim());
-					cb_2008.getParameter(cb_2008.RUP_TOP_NAME).setValue(new Double(depthTop));
 
 					double vs30 = Double.parseDouble(st.nextToken().trim());
-					((WarningDoubleParameter)cb_2008.getParameter(cb_2008.VS30_NAME)).setValueIgnoreWarning(new Double(vs30));
+					((WarningDoubleParameter)ba_2008.getParameter(ba_2008.VS30_NAME)).setValueIgnoreWarning(new Double(vs30));
 
-					double depth25 = Double.parseDouble(st.nextToken().trim());
-					((WarningDoubleParameter)cb_2008.getParameter(cb_2008.DEPTH_2pt5_NAME)).setValueIgnoreWarning(new Double(depth25));
+					st.nextToken().trim(); // ignore basin depth
 					
-					cb_2008.setIntensityMeasure(cb_2008.SA_NAME);
+					ba_2008.setIntensityMeasure(ba_2008.SA_NAME);
 					int num= period.length;
 					double openSHA_Val, tested_Val;
 					boolean results;
 					for(int k=0;k<num;++k){
-						cb_2008.getParameter(cb_2008.PERIOD_NAME).setValue(new Double(period[k]));
-						if(isMedian) openSHA_Val = Math.exp(cb_2008.getMean());
-						else openSHA_Val = cb_2008.getStdDev();
+						ba_2008.getParameter(ba_2008.PERIOD_NAME).setValue(new Double(period[k]));
+						if(isMedian) openSHA_Val = Math.exp(ba_2008.getMean());
+						else openSHA_Val = ba_2008.getStdDev();
 						tested_Val = Double.parseDouble(st.nextToken().trim());
 						results = this.compareResults(openSHA_Val, tested_Val);
 						if(results == false){
 							String failedResultMetadata = "Results from file "+fileName+"failed for  calculation for " +
-							"CB-2008 attenuation with the following parameter settings:"+
-							"  SA at period = "+period[k]+"\nMag ="+(float)mag+" rRup = "+(float)rrup+
-							"  vs30 = "+vs30+"  rjb = "+(float)rjb+"\n"+ " depth2pt5 ="+depth25+" FaultType = "+fltType+
-							"   depthTop = "+depthTop+"\n   dip = "+dip+"\n"+
+							"BA-2008 attenuation with the following parameter settings:"+
+							"  SA at period = "+period[k]+"\nMag ="+(float)mag+
+							"  vs30 = "+vs30+"  rjb = "+(float)rjb+"\n"+
 							testValString+" from OpenSHA = "+openSHA_Val+"  should be = "+tested_Val;
 
 							//System.out.println("Test number= "+i+" failed for +"+failedResultMetadata);
@@ -157,51 +146,48 @@ public class CB_2008_test extends TestCase implements ParameterChangeWarningList
 					}
 
 					
-					cb_2008.setIntensityMeasure(cb_2008.PGA_NAME);
-					if(isMedian) openSHA_Val = Math.exp(cb_2008.getMean());
-					else openSHA_Val = cb_2008.getStdDev();
+					ba_2008.setIntensityMeasure(ba_2008.PGA_NAME);
+					if(isMedian) openSHA_Val = Math.exp(ba_2008.getMean());
+					else openSHA_Val = ba_2008.getStdDev();
 					tested_Val = Double.parseDouble(st.nextToken().trim());
 					results = this.compareResults(openSHA_Val, tested_Val);
 					if(results == false){
 						String failedResultMetadata = "Results from file "+fileName+"failed for  calculation for " +
-						"CB-2008 attenuation with the following parameter settings:"+
-						"  PGA "+"\nMag ="+(float)mag+" rRup = "+(float)rrup+
-						"  vs30 = "+vs30+"  rjb = "+(float)rjb+"\n"+ " depth2pt5 ="+depth25+" FaultType = "+fltType+
-						"   depthTop = "+depthTop+"\n   dip = "+dip+"\n"+
+						"BA-2008 attenuation with the following parameter settings:"+
+						"  PGA "+"\nMag ="+(float)mag+
+						"  vs30 = "+vs30+"  rjb = "+(float)rjb+"\n"+
 						testValString+" from OpenSHA = "+openSHA_Val+"  should be = "+tested_Val;
 
 						//System.out.println("Test number= "+i+" failed for +"+failedResultMetadata);
 						//System.out.println("OpenSHA Median = "+medianFromOpenSHA+"   Target Median = "+targetMedian);
 						this.assertNull(failedResultMetadata,failedResultMetadata);
 					}
-					cb_2008.setIntensityMeasure(cb_2008.PGV_NAME);
-					if(isMedian) openSHA_Val = Math.exp(cb_2008.getMean());
-					else openSHA_Val = cb_2008.getStdDev();
+					ba_2008.setIntensityMeasure(ba_2008.PGV_NAME);
+					if(isMedian) openSHA_Val = Math.exp(ba_2008.getMean());
+					else openSHA_Val = ba_2008.getStdDev();
 					tested_Val = Double.parseDouble(st.nextToken().trim());
 					results = this.compareResults(openSHA_Val, tested_Val);
 					if(results == false){
 						String failedResultMetadata = "Results from file "+fileName+"failed for calculation for " +
-						"CB-2008 attenuation with the following parameter settings:"+
-						"  PGV "+"\nMag ="+(float)mag+" rRup = "+(float)rrup+
-						"  vs30 = "+vs30+"  rjb = "+(float)rjb+"\n"+ " depth2pt5 ="+depth25+" FaultType = "+fltType+
-						"   depthTop = "+depthTop+"\n   dip = "+dip+"\n"+
+						"BA-2008 attenuation with the following parameter settings:"+
+						"  PGV "+"\nMag ="+(float)mag+
+						"  vs30 = "+vs30+"  rjb = "+(float)rjb+"\n"+
 						testValString+" from OpenSHA = "+openSHA_Val+"  should be = "+tested_Val;
 
 						//System.out.println("Test number= "+i+" failed for +"+failedResultMetadata);
 						//System.out.println("OpenSHA Median = "+medianFromOpenSHA+"   Target Median = "+targetMedian);
 						this.assertNull(failedResultMetadata,failedResultMetadata);
 					}
-					cb_2008.setIntensityMeasure(cb_2008.PGD_NAME);
-					if(isMedian) openSHA_Val = Math.exp(cb_2008.getMean());
-					else openSHA_Val = cb_2008.getStdDev();
+					ba_2008.setIntensityMeasure(ba_2008.PGD_NAME);
+					if(isMedian) openSHA_Val = Math.exp(ba_2008.getMean());
+					else openSHA_Val = ba_2008.getStdDev();
 					tested_Val = Double.parseDouble(st.nextToken().trim());
 					results = this.compareResults(openSHA_Val, tested_Val);
 					if(results == false){
 						String failedResultMetadata = "Results from file "+fileName+"failed for calculation for " +
-						"CB-2008 attenuation with the following parameter settings:"+
-						"  PGD "+"\nMag ="+(float)mag+" rRup = "+(float)rrup+
-						"  vs30 = "+vs30+"  rjb = "+(float)rjb+"\n"+ " depth2pt5 ="+depth25+" FaultType = "+fltType+
-						"   depthTop = "+depthTop+"\n   dip = "+dip+"\n"+
+						"BA-2008 attenuation with the following parameter settings:"+
+						"  PGD "+"\nMag ="+(float)mag+
+						"  vs30 = "+vs30+"  rjb = "+(float)rjb+"\n"+
 						testValString+" from OpenSHA = "+openSHA_Val+"  should be = "+tested_Val;
 
 						//System.out.println("Test number= "+i+" failed for +"+failedResultMetadata);
