1402fcb8c81fefa70e33ecaf1a022bdbac819f4b
Kevin Milner <kmilner@usc.edu>
2009-03-10 18:54:12 +0000

    arcsecond converter

---
+++ org/opensha/calc/ArcsecondConverter.java
@@ -0,0 +1,27 @@
+package org.opensha.calc;
+
+public class ArcsecondConverter {
+	
+	private static final double RADIANS_PER_ARC_SECOND = StrictMath.PI / 648000d;
+	
+	public static double getDegrees(double arcSeconds) {
+		double radians = RADIANS_PER_ARC_SECOND * arcSeconds;
+		return StrictMath.toDegrees(radians);
+	}
+	
+	public static double getArcseconds(double degrees) {
+		double radians = StrictMath.toRadians(degrees);
+		return radians / RADIANS_PER_ARC_SECOND;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		double degrees = getDegrees(30);
+		double secs = getArcseconds(degrees);
+		
+		System.out.println("30 -> " + degrees + " -> " + secs);
+	}
+
+}
