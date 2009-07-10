24accb2d1578728f2089791d006343d363f91e1b
Kevin Milner <kmilner@usc.edu>
2009-07-10 22:23:14 +0000

    updated path for opensha.usc.edu

--- org/opensha/sha/earthquake/rupForecastImpl/WG02/WG02_FortranWrappedERF_EpistemicList.java
+++ org/opensha/sha/earthquake/rupForecastImpl/WG02/WG02_FortranWrappedERF_EpistemicList.java
@@ -54,7 +54,9 @@ public class WG02_FortranWrappedERF_EpistemicList extends ERF_EpistemicList{
   /**
    * Static variable for input file name
    */
-  private final static String WG02_CODE_PATH ="/opt/install/apache-tomcat-5.5.20/webapps/OpenSHA/wg99/wg99_src_v27/";
+  private final static String WG02_CODE_PATH ="/usr/local/tomcat/default/webapps/OpenSHA/wg99/wg99_src_v27";
+  // this is the old path on gravity
+//  private final static String WG02_CODE_PATH ="/opt/install/apache-tomcat-5.5.20/webapps/OpenSHA/wg99/wg99_src_v27/";
   private final static String WG02_INPUT_FILE ="base_OpenSHA.inp";   // the templet WG02-code input file modified for OpenSHA purposes
   private final static String WG02_OPENSHA_INPUT_FILE = "OpenSHA.inp";     // the WG02-code input file that we create on the fly
   public final static String INPUT_FILE_NAME_1 = "WG02_WRAPPER_INPUT.DAT"; // the WG02-code output file that we read
