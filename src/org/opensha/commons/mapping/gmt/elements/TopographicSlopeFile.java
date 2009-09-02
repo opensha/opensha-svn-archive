96f3f16fb527c3f4582ab653fb08c61bca42020d
Kevin Milner <kmilner@usc.edu>
2009-09-02 20:08:13 +0000

    added SRTM_30_PLUS to enum

--- src/org/opensha/commons/mapping/gmt/elements/TopographicSlopeFile.java
+++ src/org/opensha/commons/mapping/gmt/elements/TopographicSlopeFile.java
@@ -8,7 +8,8 @@ public enum TopographicSlopeFile {
 	CA_THREE		(3, "calTopoInten03.grd", GMT_Map.ca_topo_region),
 	CA_SIX			(6, "calTopoInten06.grd", GMT_Map.ca_topo_region),
 	CA_EIGHTEEN		(18, "calTopoInten18.grd", GMT_Map.ca_topo_region),
-	CA_THIRTY 		(30, "calTopoInten30.grd", GMT_Map.ca_topo_region);
+	CA_THIRTY 		(30, "calTopoInten30.grd", GMT_Map.ca_topo_region),
+	SRTM_30_PLUS	(30, "srtm30_plus_v5.0_inten.grd", RectangularGeographicRegion.createEntireGlobeRegion());
 	
 	private final int resolution;
 	private final String fileName;
