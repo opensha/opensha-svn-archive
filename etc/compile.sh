29be928092d7430e82324a9a51d20a998ec7022f
Kevin Milner <kmilner@usc.edu>
2009-09-09 23:27:26 +0000

    added option to call resource tag

--- etc/compile.sh
+++ etc/compile.sh
@@ -3,6 +3,10 @@
 set -o errexit
 
 ant=${1-"ant"}
+resource=${2-0}
 
 $ant -f compile.xml -lib ../lib:../dev/scratch/ISTI/isti.util.jar
+if [[ $resource -eq 1 ]];then
+	$ant -f compile.xml resource
+fi
 exit $?
