ac0571abe2e6c4310f7848fe2c61333c30f4f163
Peter Powers <pmpowers@usgs.gov>
2009-09-24 00:16:23 +0000

    hazardcurvebuild work

--- src/org/opensha/sha/gui/HazardCurveLocalModeApplication.java
+++ src/org/opensha/sha/gui/HazardCurveLocalModeApplication.java
@@ -20,7 +20,7 @@ import org.opensha.sha.gui.infoTools.ExceptionWindow;
 import org.opensha.sha.gui.beans.EqkRupSelectorGuiBean;
 import org.opensha.sha.earthquake.EqkRupForecastBaseAPI;
 
-import scratch.bbradley.NewZealandERFv1;
+//import scratch.bbradley.NewZealandERFv1;
 
 /**
  * <p>Title: HazardCurveLocalModeApplication</p>
@@ -36,12 +36,18 @@ import scratch.bbradley.NewZealandERFv1;
 
 public class HazardCurveLocalModeApplication extends HazardCurveServerModeApplication {
 
-	public final static String NSHMP08_CEUS_ERF_CLASS_NAME = "org.opensha.sha.earthquake.rupForecastImpl.NSHMP_CEUS08.NSHMP08_CEUS_ERF";
+	//public final static String NSHMP08_CEUS_ERF_CLASS_NAME = "org.opensha.sha.earthquake.rupForecastImpl.NSHMP_CEUS08.NSHMP08_CEUS_ERF";
 	public final static String FRANKEL_ADJ_FORECAST_CLASS_NAME = "org.opensha.sha.earthquake.rupForecastImpl.Frankel96.Frankel96_AdjustableEqkRupForecast";
 	public final static String FRANKEL_FORECAST_CLASS_NAME = "org.opensha.sha.earthquake.rupForecastImpl.Frankel96.Frankel96_EqkRupForecast";
 	public final static String FRANKEL02_ADJ_FORECAST_CLASS_NAME = "org.opensha.sha.earthquake.rupForecastImpl.Frankel02.Frankel02_AdjustableEqkRupForecast";
+
 	public final static String WG02_ERF_LIST_CLASS_NAME = "org.opensha.sha.earthquake.rupForecastImpl.WG02.WG02_ERF_Epistemic_List";
+	public final static String WG02_ERF_CLASS_NAME = "org.opensha.sha.earthquake.rupForecastImpl.WG02.WG02_EqkRupForecast";
 	public final static String WGCEP_UCERF1_CLASS_NAME = "org.opensha.sha.earthquake.rupForecastImpl.WGCEP_UCERF1.WGCEP_UCERF1_EqkRupForecast";
+	public final static String WGCEP_UCERF_2_CLASS_NAME="org.opensha.sha.earthquake.rupForecastImpl.WGCEP_UCERF_2_Final.UCERF2";
+	public final static String WGCEP_UCERF_2_EPISTEMIC_LIST_CLASS_NAME="org.opensha.sha.earthquake.rupForecastImpl.WGCEP_UCERF_2_Final.UCERF2_TimeIndependentEpistemicList";
+	public final static String WGCEP_AVG_UCERF_2_CLASS_NAME="org.opensha.sha.earthquake.rupForecastImpl.WGCEP_UCERF_2_Final.MeanUCERF2.MeanUCERF2";
+
 	public final static String PEER_AREA_FORECAST_CLASS_NAME = "org.opensha.sha.earthquake.rupForecastImpl.PEER_TestCases.PEER_AreaForecast";
 	public final static String PEER_NON_PLANAR_FAULT_FORECAST_CLASS_NAME = "org.opensha.sha.earthquake.rupForecastImpl.PEER_TestCases.PEER_NonPlanarFaultForecast";
 	public final static String PEER_MULTI_SOURCE_FORECAST_CLASS_NAME = "org.opensha.sha.earthquake.rupForecastImpl.PEER_TestCases.PEER_MultiSourceForecast";
@@ -53,15 +59,11 @@ public class HazardCurveLocalModeApplication extends HazardCurveServerModeApplic
 	public final static String POINT_SRC_FORECAST_CLASS_NAME="org.opensha.sha.earthquake.rupForecastImpl.PointSourceERF";
 	public final static String POINT2MULT_VSS_FORECAST_CLASS_NAME="org.opensha.sha.earthquake.rupForecastImpl.Point2MultVertSS_Fault.Point2MultVertSS_FaultERF";
 	public final static String POINT2MULT_VSS_ERF_LIST_CLASS_NAME="org.opensha.sha.earthquake.rupForecastImpl.Point2MultVertSS_Fault.Point2MultVertSS_FaultERF_List";
-	public final static String WG02_ERF_CLASS_NAME = "org.opensha.sha.earthquake.rupForecastImpl.WG02.WG02_EqkRupForecast";
-	public final static String WGCEP_UCERF_2_CLASS_NAME="org.opensha.sha.earthquake.rupForecastImpl.WGCEP_UCERF_2_Final.UCERF2";
-	public final static String WGCEP_UCERF_2_EPISTEMIC_LIST_CLASS_NAME="org.opensha.sha.earthquake.rupForecastImpl.WGCEP_UCERF_2_Final.UCERF2_TimeIndependentEpistemicList";
-	public final static String WGCEP_AVG_UCERF_2_CLASS_NAME="org.opensha.sha.earthquake.rupForecastImpl.WGCEP_UCERF_2_Final.MeanUCERF2.MeanUCERF2";
 	public final static String YUCCA_MOUNTAIN_CLASS_NAME="org.opensha.sha.earthquake.rupForecastImpl.YuccaMountain.YuccaMountainERF";
 	public final static String YUCCA_MOUNTAIN_ERF_LIST_CLASS_NAME="org.opensha.sha.earthquake.rupForecastImpl.YuccaMountain.YuccaMountainERF_List";
-	public final static String CYBERSHAKE_ERF_LIST_CLASS_NAME="org.opensha.sha.cybershake.openshaAPIs.CyberShakeERF";
-	public final static String CYBERSHAKE_ERF_WRAPPER_LIST_CLASS_NAME="org.opensha.sha.cybershake.openshaAPIs.CyberShakeUCERFWrapper_ERF";
-	public final static String NZ_ERFv1_CLASS_NAME="scratch.bbradley.NewZealandERFv1";
+	//public final static String CYBERSHAKE_ERF_LIST_CLASS_NAME="org.opensha.sha.cybershake.openshaAPIs.CyberShakeERF";
+	//public final static String CYBERSHAKE_ERF_WRAPPER_LIST_CLASS_NAME="org.opensha.sha.cybershake.openshaAPIs.CyberShakeUCERFWrapper_ERF";
+	//public final static String NZ_ERFv1_CLASS_NAME="scratch.bbradley.NewZealandERFv1";
 	public final static String GEM_TEST_ERF_CLASS_NAME="org.opensha.sha.earthquake.rupForecastImpl.GEM.TestGEM_ERF";
 
 	protected final static String appURL = "http://www.opensha.org/applications/hazCurvApp/HazardCurveApp.jar";
@@ -115,16 +117,10 @@ public class HazardCurveLocalModeApplication extends HazardCurveServerModeApplic
 		ArrayList<String> erf_Classes = new ArrayList<String>();
 
 		//adding the client based ERF's to the application
-		erf_Classes.add(GEM_TEST_ERF_CLASS_NAME);
 		erf_Classes.add(FRANKEL_ADJ_FORECAST_CLASS_NAME);
 		erf_Classes.add(FRANKEL_FORECAST_CLASS_NAME);
 		erf_Classes.add(FRANKEL02_ADJ_FORECAST_CLASS_NAME);
 		//      erf_Classes.add(NSHMP08_CEUS_ERF_CLASS_NAME);
-		erf_Classes.add(YUCCA_MOUNTAIN_CLASS_NAME);
-		erf_Classes.add(YUCCA_MOUNTAIN_ERF_LIST_CLASS_NAME);
-		erf_Classes.add(WGCEP_UCERF_2_CLASS_NAME);
-		erf_Classes.add(WGCEP_UCERF_2_EPISTEMIC_LIST_CLASS_NAME);
-		erf_Classes.add(WGCEP_AVG_UCERF_2_CLASS_NAME);
 		erf_Classes.add(WG02_ERF_LIST_CLASS_NAME);
 		erf_Classes.add(WGCEP_UCERF1_CLASS_NAME);
 		erf_Classes.add(PEER_AREA_FORECAST_CLASS_NAME);
@@ -138,9 +134,18 @@ public class HazardCurveLocalModeApplication extends HazardCurveServerModeApplic
 		erf_Classes.add(POINT_SRC_FORECAST_CLASS_NAME);
 		erf_Classes.add(POINT2MULT_VSS_FORECAST_CLASS_NAME);
 		erf_Classes.add(POINT2MULT_VSS_ERF_LIST_CLASS_NAME);
+		
+		erf_Classes.add(WGCEP_UCERF_2_CLASS_NAME);
+		erf_Classes.add(WGCEP_UCERF_2_EPISTEMIC_LIST_CLASS_NAME);
+		erf_Classes.add(WGCEP_AVG_UCERF_2_CLASS_NAME);
+		
+		erf_Classes.add(YUCCA_MOUNTAIN_CLASS_NAME);
+		erf_Classes.add(YUCCA_MOUNTAIN_ERF_LIST_CLASS_NAME);
+		
+		erf_Classes.add(GEM_TEST_ERF_CLASS_NAME);
 		//      erf_Classes.add(CYBERSHAKE_ERF_LIST_CLASS_NAME);
 		//      erf_Classes.add(CYBERSHAKE_ERF_WRAPPER_LIST_CLASS_NAME);
-		erf_Classes.add(NZ_ERFv1_CLASS_NAME);
+		//erf_Classes.add(NZ_ERFv1_CLASS_NAME);
 		
 		return erf_Classes;
 	}
