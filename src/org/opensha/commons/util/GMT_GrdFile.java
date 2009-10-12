6096094cb2b76cb63e476fc280c83d86d47ca936
Kevin Milner <kmilner@usc.edu>
2009-10-12 21:09:25 +0000

    mostly javadoc's with a few minor api revisions

--- src/org/opensha/commons/util/GMT_GrdFile.java
+++ src/org/opensha/commons/util/GMT_GrdFile.java
@@ -1,5 +1,6 @@
 package org.opensha.commons.util;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
@@ -12,6 +13,16 @@ import ucar.nc2.Dimension;
 import ucar.nc2.NetcdfFile;
 import ucar.nc2.Variable;
 
+/**
+ * This class wraps the NetCDF-Java library for use specifically with GMT-style NetCDF files (.grd files).
+ * 
+ * X and Y values are pre-loaded, and Z values can either be cached (faster when there is ample memory
+ * for the file and either a large chunk of the file will be used, or lots of random access) or loaded
+ * on demand.
+ * 
+ * @author kevin
+ *
+ */
 public class GMT_GrdFile {
 	
 	private NetcdfFile file;
@@ -26,10 +37,23 @@ public class GMT_GrdFile {
 	
 	private static int singleShape[] = {1, 1};
 	
+	/**
+	 * Load the given GRD file...it will be cached by default
+	 * 
+	 * @param fileName
+	 * @throws IOException
+	 */
 	public GMT_GrdFile(String fileName) throws IOException {
 		this(fileName, true);
 	}
 	
+	/**
+	 * Load the given GRD file...it will be cached if cacheZ is true
+	 * 
+	 * @param fileName
+	 * @param cacheZ
+	 * @throws IOException
+	 */
 	public GMT_GrdFile(String fileName, boolean cacheZ) throws IOException {
 		file = NetcdfFile.openInMemory(fileName);
 		List<Dimension> dims = file.getDimensions();
@@ -45,44 +69,101 @@ public class GMT_GrdFile {
 		
 		zVar = vars.get(2);
 		if (cacheZ) {
-			zData = zVar.read();
-			zInd = zData.getIndex();
+			cacheZData();
 		}
 	}
 	
-	public int getXSize() {
+	/**
+	 * Get the number of X points in the GRD file
+	 * 
+	 * @return
+	 */
+	public int getNumX() {
 		return xDim;
 	}
 	
-	public int getYSize() {
+	/**
+	 * Get the number of Y points in the GRD file
+	 * 
+	 * @return
+	 */
+	public int getNumY() {
 		return yDim;
 	}
 	
+	/**
+	 * Get the longitude (X) pt at the given X-index
+	 * 
+	 * @param x
+	 * @return
+	 */
 	public double getX(int x) {
 		return xData.getDouble(x);
 	}
 	
+	/**
+	 * Get a NetCDF Array with all of the longitude (X) data
+	 * 
+	 * @return
+	 */
 	public Array getXData() {
 		return xData;
 	}
 	
+	/**
+	 * Get the latitude (Y) pt at the given X-index
+	 * 
+	 * @param x
+	 * @return
+	 */
 	public double getY(int y) {
 		return yData.getDouble(y);
 	}
 	
+	/**
+	 * Get a NetCDF Array with all of the latitude (Y) data
+	 * 
+	 * @return
+	 */
 	public Array getYData() {
 		return yData;
 	}
 	
+	/**
+	 * Get the Location at the following X,Y indexes
+	 * 
+	 * @param x
+	 * @param y
+	 * @return
+	 */
 	public Location getLoc(int x, int y) {
 		return new Location(getY(y), getX(x));
 	}
 	
+	/**
+	 * Get the Z value at the given X,Y indexes
+	 * 
+	 * @param x
+	 * @param y
+	 * @return
+	 * @throws IOException
+	 * @throws InvalidRangeException
+	 */
 	public double getZ(int x, int y) throws IOException, InvalidRangeException {
 		int pt[] = {y, x};
 		return getZ(pt);
 	}
 	
+	/**
+	 * Get the Z value at the given pt. The given point should be a 2-dimensional
+	 * integer array in the format (y,x)
+	 * 
+	 * @param x
+	 * @param y
+	 * @return
+	 * @throws IOException
+	 * @throws InvalidRangeException
+	 */
 	public double getZ(int pt[]) throws IOException, InvalidRangeException {
 		if (zData != null) {
 			zInd.set(pt);
@@ -93,11 +174,35 @@ public class GMT_GrdFile {
 		}
 	}
 	
-	public Array getZData() throws IOException {
+	/**
+	 * Load the entire Z data into memory if it hasn't already been loaded
+	 * 
+	 * @throws IOException
+	 */
+	public void cacheZData() throws IOException {
 		if (zData == null) {
 			zData = zVar.read();
 		}
+	}
+	
+	/**
+	 * Get a NetCDF Array with all of the Z data. It will be cached if needed.
+	 * 
+	 * @return
+	 * @throws IOException
+	 */
+	public Array getZData() throws IOException {
+		cacheZData();
 		return zData;
+	}
+	
+	/**
+	 * Return the name field embedded into this GRD file.
+	 * 
+	 * @return
+	 */
+	public String getZName() {
+		return zVar.getName();
 	}
 
 	/**
@@ -113,6 +218,17 @@ public class GMT_GrdFile {
 		int y = 146;
 		
 		System.out.println(grd.getLoc(x, y) + ": " + grd.getZ(x, y));
+		
+		FileWriter fw = new FileWriter("/tmp/slab.txt");
+		
+		for (x=0; x<grd.getNumX(); x++) {
+			for (y=0; y<grd.getNumY(); y++) {
+				Location loc = grd.getLoc(x, y);
+				double val = grd.getZ(x, y);
+				fw.write(loc.getLatitude() + "\t" + loc.getLongitude() + "\t" + val + "\n");
+			}
+		}
+		fw.close();
 	}
 
 }
