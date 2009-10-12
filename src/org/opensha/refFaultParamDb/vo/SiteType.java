14be4cff79b58283bd5e8adf63e4fc97b05b74b8
Peter Powers <pmpowers@usgs.gov>
2009-10-12 20:38:15 +0000

    added copyright and ASL2.0 header to every java and xml file

--- src/org/opensha/refFaultParamDb/vo/SiteType.java
+++ src/org/opensha/refFaultParamDb/vo/SiteType.java
@@ -1,9 +1,28 @@
+/*******************************************************************************
+ * Copyright 2009 OpenSHA.org in partnership with
+ * the Southern California Earthquake Center (SCEC, http://www.scec.org)
+ * at the University of Southern California and the UnitedStates Geological
+ * Survey (USGS; http://www.usgs.gov)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
 package org.opensha.refFaultParamDb.vo;
 
 /**
  * <p>Title: SiteType.java </p>
  * <p>Description: This saves the various site types associated with a
- * paloe site. Example of site types are : “trench”, “geologic”, “survey/cultural” </p>
+ * paloe site. Example of site types are : ï¿½trenchï¿½, ï¿½geologicï¿½, ï¿½survey/culturalï¿½ </p>
  * <p>Copyright: Copyright (c) 2002</p>
  * <p>Company: </p>
  * @author not attributable
