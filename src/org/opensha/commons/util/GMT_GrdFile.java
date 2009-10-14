7aa70da852294ca9af65a7d8940e3c7e4c2ae343
Kevin Milner <kmilner@usc.edu>
2009-10-14 01:42:40 +0000

    added getClosestZ and related methods

--- src/org/opensha/commons/util/GMT_GrdFile.java
+++ src/org/opensha/commons/util/GMT_GrdFile.java
@@ -1,6 +1,5 @@
 package org.opensha.commons.util;
 
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
@@ -29,6 +28,7 @@ public class GMT_GrdFile {
 	private NetcdfFile file;
 	
 	private int xDim, yDim;
+	private double xSpacing, ySpacing;
 	private Array xData;
 	private Array yData;
 	
@@ -68,12 +68,29 @@ public class GMT_GrdFile {
 		Variable yVar = vars.get(1);
 		
 		xData = xVar.read();
+		xSpacing = calcSpacing(xData);
 		yData = yVar.read();
+		ySpacing = calcSpacing(yData);
 		
 		zVar = vars.get(2);
 		if (cacheZ) {
 			cacheZData();
 		}
+	}
+	
+	private double calcSpacing(Array data) {
+		long max = 100;
+		if (max < data.getSize())
+			max = data.getSize();
+		double tot = 0;
+		int cnt = 0;
+		for (int i=0; i<max-1; i++) {
+			double val1 = data.getDouble(i);
+			double val2 = data.getDouble(i+1);
+			tot += Math.abs(val2 - val1);
+			cnt++;
+		}
+		return tot / (double)cnt;
 	}
 	
 	/**
@@ -92,6 +109,24 @@ public class GMT_GrdFile {
 	 */
 	public int getNumY() {
 		return yDim;
+	}
+	
+	/**
+	 * Get the grid spacing in the X dimension
+	 * 
+	 * @return
+	 */
+	public double getXSpacing() {
+		return xSpacing;
+	}
+	
+	/**
+	 * Get the grid spacing in the Y dimension
+	 * 
+	 * @return
+	 */
+	public double getYSpacing() {
+		return ySpacing;
 	}
 	
 	/**
@@ -141,6 +176,66 @@ public class GMT_GrdFile {
 	 */
 	public Location getLoc(int x, int y) {
 		return new Location(getY(y), getX(x));
+	}
+	
+	/**
+	 * This class calculates the index for the given point in terms of floats. When rounded,
+	 * this would be the closest point.
+	 * 
+	 * @param loc
+	 * @return
+	 */
+	private double[] getFloatIndexes(Location loc) {
+		double ret[] = { getFloatIndexY(loc), getFloatIndexX(loc) };
+		return ret;
+	}
+	
+	private double getFloatIndexX(Location loc) {
+		double lon = loc.getLongitude();
+		double minLon = getMinX();
+		return (lon - minLon) / xSpacing;
+	}
+	
+	private double getFloatIndexY(Location loc) {
+		double lat = loc.getLatitude();
+		double minLat = getMinY();
+		return (lat - minLat) / ySpacing;
+	}
+	
+	/**
+	 * Returns the closest point in the format (y,x)
+	 * 
+	 * @param loc
+	 * @return
+	 */
+	public int[] getClosestPt(Location loc) {
+		double[] fltPt = getFloatIndexes(loc);
+		int y = (int) (fltPt[0] + 0.5);
+		int x = (int) (fltPt[1] + 0.5);
+		
+		if (x < 0)
+			x = 0;
+		if (y < 0)
+			y = 0;
+		if (x >= xDim)
+			x = xDim - 1;
+		if (y >= yDim)
+			y = yDim - 1;
+		
+		int pt[] = {y, x};
+		return pt;
+	}
+	
+	/**
+	 * Get the Z value of the closest point in the file.
+	 * 
+	 * @param loc
+	 * @return
+	 * @throws IOException
+	 * @throws InvalidRangeException
+	 */
+	public double getClosestZ(Location loc) throws IOException, InvalidRangeException {
+		return getZ(getClosestPt(loc));
 	}
 	
 	/**
@@ -268,9 +363,11 @@ public class GMT_GrdFile {
 	public static void main(String[] args) throws IOException, InvalidRangeException {
 		boolean cacheZ = true;	// if 'true', all Z values will be loaded up front. otherwise they will
 								// be loaded on demand (slower if you need a lot of the file)
-		GMT_GrdFile grd = new GMT_GrdFile("/tmp/sum_slab1.0_clip.grd", cacheZ);
+		GMT_GrdFile grd = new GMT_GrdFile("G:\\Downloads\\sum_slab1.0_clip.grd", cacheZ);
+//		GMT_GrdFile grd = new GMT_GrdFile("/tmp/sum_slab1.0_clip.grd", cacheZ);
 		
 		System.out.println("NumX: " + grd.getNumX() + " NumY: " + grd.getNumY());
+		System.out.println("SpacingX: " + grd.getXSpacing() + " SpacingY: " + grd.getYSpacing());
 		System.out.println(grd.getRegion());
 		
 		int x = 1649;
