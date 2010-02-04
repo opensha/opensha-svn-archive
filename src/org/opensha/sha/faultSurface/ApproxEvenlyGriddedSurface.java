1b226089c7ec5ff397622609b645691ed8fcdaa8
Peter Powers <pmpowers@usgs.gov>
2010-02-04 04:37:59 +0000

    Updates to ApproxGriddedSurface internal main test

--- src/org/opensha/sha/faultSurface/ApproxEvenlyGriddedSurface.java
+++ src/org/opensha/sha/faultSurface/ApproxEvenlyGriddedSurface.java
@@ -134,29 +134,50 @@ public class ApproxEvenlyGriddedSurface extends EvenlyGriddedSurface {
 	/**
 	 * This explores an accuracy issue (final depth is right be lats and lons are more off)
 	 */
-	public void test() {
-		Location topLoc = new Location(-34.36338,-71.32979,50.0);
-		Location botLoc = new Location(-33.94507,-72.7105,10.0);
+	public static void test1(Location l1, Location l2) {
+		System.out.println("TEST-1");
+//		Location topLoc = new Location(-34.36338,-71.32979,50.0);
+//		Location botLoc = new Location(-33.94507,-72.7105,10.0);
 		double numSubdivisions=100;
-		Direction dir = RelativeLocation.getDirection(topLoc, botLoc);
+		Direction dir = RelativeLocation.getDirection(l1, l2);
+		System.out.println("Azimuth p1 to p2: " + dir.getAzimuth());
 		double horzIncr = dir.getHorzDistance()/numSubdivisions;
 		double vertIncr = -dir.getVertDistance()/numSubdivisions;  // minus sign because vertDist is pos up and depth is pos down
 		dir.setHorzDistance(horzIncr);
 		dir.setVertDistance(vertIncr);
-		Location prevLoc = topLoc;
+		Location prevLoc = l1;
 		for(int r=0;r<numSubdivisions;r++) {
 			Location nextLoc = RelativeLocation.getLocation(prevLoc, dir);
 			prevLoc = nextLoc;
 		}
-		double accuracyCheck = RelativeLocation.getTotalDistance(botLoc, prevLoc);
+		double accuracyCheck = RelativeLocation.getTotalDistance(l2, prevLoc);
 		System.out.println("Distance between actual and computed bottom point = "+(float)accuracyCheck);
-		System.out.println("DeltaLat = "+ (float)((botLoc.getLatitude()-prevLoc.getLatitude())*111));
-		System.out.println("DeltaLon = "+ (float)((botLoc.getLongitude()-prevLoc.getLongitude())*111));
-		System.out.println("DeltaDepth = "+ (float)(botLoc.getDepth()-prevLoc.getDepth()));
+		System.out.println("DeltaLat = "+ (float)((l2.getLatitude()-prevLoc.getLatitude())*111));
+		System.out.println("DeltaLon = "+ (float)((l2.getLongitude()-prevLoc.getLongitude())*111));
+		System.out.println("DeltaDepth = "+ (float)(l2.getDepth()-prevLoc.getDepth()));
+		System.out.println("");
+	}
 
-
-		
-		
+	public static void test2(Location l1, Location l2) {
+		System.out.println("TEST-2");
+		double numSubdivisions=100;
+		Direction dir = RelativeLocation.getDirection(l1, l2);
+		double horzIncr = dir.getHorzDistance()/numSubdivisions;
+		double vertIncr = -dir.getVertDistance()/numSubdivisions;  // minus sign because vertDist is pos up and depth is pos down
+		dir.setHorzDistance(horzIncr);
+		dir.setVertDistance(vertIncr);
+		Location prevLoc = l1;
+		for(int r=0;r<numSubdivisions;r++) {
+			Location nextLoc = RelativeLocation.getLocation(prevLoc, dir);
+			prevLoc = nextLoc;
+			dir.setAzimuth(RelativeLocation.getAzimuth(prevLoc, l2));
+		}
+		double accuracyCheck = RelativeLocation.getTotalDistance(l2, prevLoc);
+		System.out.println("Distance between actual and computed bottom point = "+(float)accuracyCheck);
+		System.out.println("DeltaLat = "+ (float)((l2.getLatitude()-prevLoc.getLatitude())*111));
+		System.out.println("DeltaLon = "+ (float)((l2.getLongitude()-prevLoc.getLongitude())*111));
+		System.out.println("DeltaDepth = "+ (float)(l2.getDepth()-prevLoc.getDepth()));
+		System.out.println("");
 	}
 
 
@@ -228,58 +249,72 @@ public class ApproxEvenlyGriddedSurface extends EvenlyGriddedSurface {
 	 */
 	public static void main(String[] args) {
 		
-		//these traces come from the file "sub-ch9-z4.in" from the NSHMP 2008 model (South America subduction zone)
-		FaultTrace topTrace = new FaultTrace(null);
-		
-		topTrace.addLocation(-33.9450824793, -72.710497654, 10.);
-		topTrace.addLocation(-33.7588818362, -72.6388800048, 10.);
-		topTrace.addLocation(-33.5399999996, -72.5818237309, 10.);
-		topTrace.addLocation(-32.9000000001, -72.4949902347, 10.);
-		topTrace.addLocation(-31.9400000003, -72.4908227539, 10.);
-		topTrace.addLocation(-30.2999999998, -72.3911303709, 10.);
-		topTrace.addLocation(-29.9600000004, -72.3579357913, 10.);
-		topTrace.addLocation(-29.2320361332, -72.2279632571, 10.);
-		topTrace.addLocation(-28.0045263675, -71.9045263674, 10.);
-		topTrace.addLocation(-26.8599999996, -71.6779882813, 10.);
-		topTrace.addLocation(-25.9555615232, -71.584437256, 10.);
-		topTrace.addLocation(-25.1200000003, -71.4298706053, 10.);
-		topTrace.addLocation(-24.7841723632, -71.3841735843, 10.);
-		topTrace.addLocation(-23.0199999996, -71.2632910159, 10.);
-		topTrace.addLocation(-22.22, -71.1589331058, 10.);
-		topTrace.addLocation(-21.8942095629, -71.1322380371, 10.);
+//		//these traces come from the file "sub-ch9-z4.in" from the NSHMP 2008 model (South America subduction zone)
+//		FaultTrace topTrace = new FaultTrace(null);
+//		
+//		topTrace.addLocation(-33.9450824793, -72.710497654, 10.);
+//		topTrace.addLocation(-33.7588818362, -72.6388800048, 10.);
+//		topTrace.addLocation(-33.5399999996, -72.5818237309, 10.);
+//		topTrace.addLocation(-32.9000000001, -72.4949902347, 10.);
+//		topTrace.addLocation(-31.9400000003, -72.4908227539, 10.);
+//		topTrace.addLocation(-30.2999999998, -72.3911303709, 10.);
+//		topTrace.addLocation(-29.9600000004, -72.3579357913, 10.);
+//		topTrace.addLocation(-29.2320361332, -72.2279632571, 10.);
+//		topTrace.addLocation(-28.0045263675, -71.9045263674, 10.);
+//		topTrace.addLocation(-26.8599999996, -71.6779882813, 10.);
+//		topTrace.addLocation(-25.9555615232, -71.584437256, 10.);
+//		topTrace.addLocation(-25.1200000003, -71.4298706053, 10.);
+//		topTrace.addLocation(-24.7841723632, -71.3841735843, 10.);
+//		topTrace.addLocation(-23.0199999996, -71.2632910159, 10.);
+//		topTrace.addLocation(-22.22, -71.1589331058, 10.);
+//		topTrace.addLocation(-21.8942095629, -71.1322380371, 10.);
+//
+//		FaultTrace botTrace = new FaultTrace(null);
+//
+//		botTrace.addLocation(-34.3633763178, -71.3297920084, 50.);
+//		botTrace.addLocation(-34.02, -71.1744982909, 50.);
+//		botTrace.addLocation(-33.7199999999, -71.0928930662, 50.);
+//		botTrace.addLocation(-33.4399999999, -71.0548120114, 50.);
+//		botTrace.addLocation(-32.6400000002, -70.993572998, 50.);
+//		botTrace.addLocation(-32.2813330081, -70.9813330075, 50.);
+//		botTrace.addLocation(-31.9799999997, -70.9924999998, 50.);
+//		botTrace.addLocation(-31.3460058597, -71.1060070798, 50.);
+//		botTrace.addLocation(-31.093046875, -71.1330468748, 50.);
+//		botTrace.addLocation(-30.8200000004, -71.1281042477, 50.);
+//		botTrace.addLocation(-30.5200000003, -71.0938342283, 50.);
+//		botTrace.addLocation(-29.6231591798, -70.9168408208, 50.);
+//		botTrace.addLocation(-28.1739575199, -70.5260412594, 50.);
+//		botTrace.addLocation(-26.872775879, -70.2672241207, 50.);
+//		botTrace.addLocation(-26.2200000001, -70.1064562992, 50.);
+//		botTrace.addLocation(-25.9844946293, -70.0644934082, 50.);
+//		botTrace.addLocation(-25.7599999999, -70.0563464355, 50.);
+//		botTrace.addLocation(-25.1999999999, -70.0945446773, 50.);
+//		botTrace.addLocation(-24.9400000001, -70.0942907718, 50.);
+//		botTrace.addLocation(-22.5600000003, -69.8766857908, 50.);
+//		botTrace.addLocation(-22.0599999998, -69.855518799, 50.);
+//		botTrace.addLocation(-21.5010148695, -69.8649200561, 50.);
+//		
+//		ApproxEvenlyGriddedSurface surf = new ApproxEvenlyGriddedSurface(topTrace,botTrace,5);
+////		surf.writeXYZ_toFile("dev/scratch/ned/hereItIs.txt");
+//		
+//		System.out.println ("AveGridSpacingAlongStrike="+surf.computeAveGridSpacingAlongStrike());
+//		System.out.println ("AveGridSpacingDownDip="+surf.computeAveGridSpacingDownDip());
+//		
+//		surf.test();
 
-		FaultTrace botTrace = new FaultTrace(null);
+		// lat aligned
+		Location top1 = new Location(20.0,0.0,10.0);
+		Location bot1 = new Location(40.0,0.0,50.0);
+		
+		// ln aligned
+		Location top2 = new Location(40.0,0.0,10.0);
+		Location bot2 = new Location(40.0,20.0,50.0);
 
-		botTrace.addLocation(-34.3633763178, -71.3297920084, 50.);
-		botTrace.addLocation(-34.02, -71.1744982909, 50.);
-		botTrace.addLocation(-33.7199999999, -71.0928930662, 50.);
-		botTrace.addLocation(-33.4399999999, -71.0548120114, 50.);
-		botTrace.addLocation(-32.6400000002, -70.993572998, 50.);
-		botTrace.addLocation(-32.2813330081, -70.9813330075, 50.);
-		botTrace.addLocation(-31.9799999997, -70.9924999998, 50.);
-		botTrace.addLocation(-31.3460058597, -71.1060070798, 50.);
-		botTrace.addLocation(-31.093046875, -71.1330468748, 50.);
-		botTrace.addLocation(-30.8200000004, -71.1281042477, 50.);
-		botTrace.addLocation(-30.5200000003, -71.0938342283, 50.);
-		botTrace.addLocation(-29.6231591798, -70.9168408208, 50.);
-		botTrace.addLocation(-28.1739575199, -70.5260412594, 50.);
-		botTrace.addLocation(-26.872775879, -70.2672241207, 50.);
-		botTrace.addLocation(-26.2200000001, -70.1064562992, 50.);
-		botTrace.addLocation(-25.9844946293, -70.0644934082, 50.);
-		botTrace.addLocation(-25.7599999999, -70.0563464355, 50.);
-		botTrace.addLocation(-25.1999999999, -70.0945446773, 50.);
-		botTrace.addLocation(-24.9400000001, -70.0942907718, 50.);
-		botTrace.addLocation(-22.5600000003, -69.8766857908, 50.);
-		botTrace.addLocation(-22.0599999998, -69.855518799, 50.);
-		botTrace.addLocation(-21.5010148695, -69.8649200561, 50.);
+		test1(top1, bot1);
+		test1(top2, bot2);
 		
-		ApproxEvenlyGriddedSurface surf = new ApproxEvenlyGriddedSurface(topTrace,botTrace,5);
-//		surf.writeXYZ_toFile("dev/scratch/ned/hereItIs.txt");
-		
-		System.out.println ("AveGridSpacingAlongStrike="+surf.computeAveGridSpacingAlongStrike());
-		System.out.println ("AveGridSpacingDownDip="+surf.computeAveGridSpacingDownDip());
-		
-		surf.test();
+		test2(top1, bot1);
+		test2(top2, bot2);
 		
 	
 	}
