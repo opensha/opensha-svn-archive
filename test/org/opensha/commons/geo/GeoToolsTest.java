c0180c3ebe78e1e2a88e75b2ce1d356192eb151c
Peter Powers <pmpowers@usgs.gov>
2010-04-01 21:52:37 +0000

    created geoToolsTest

---
+++ test/org/opensha/commons/geo/GeoToolsTest.java
@@ -0,0 +1,59 @@
+package org.opensha.commons.geo;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class GeoToolsTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void testValidateLats() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testValidateLat() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testValidateLons() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testValidateLon() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testValidateDepths() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testValidateDepth() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRadiusAtLocation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDegreesLatPerKm() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDegreesLonPerKm() {
+		fail("Not yet implemented");
+	}
+
+}
