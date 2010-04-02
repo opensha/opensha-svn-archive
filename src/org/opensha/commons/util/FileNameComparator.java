050092ab99b09ec84ebdabc8b067966e07ae5d4f
Peter Powers <pmpowers@usgs.gov>
2010-04-02 17:54:13 +0000

    cleaned up FileNameComparator

--- src/org/opensha/commons/util/FileNameComparator.java
+++ src/org/opensha/commons/util/FileNameComparator.java
@@ -23,28 +23,27 @@ import java.io.File;
 import java.text.Collator;
 import java.util.Comparator;
 
+/**
+ * <code>Comparator</code> for filenames that promotes directories and sorts
+ * by filename for the current <code>Locale</code>.
+ *
+ * @author Peter Powers
+ * @version $Id$
+ */
 public class FileNameComparator implements Comparator<File> {
-	// A Collator does string comparisons
+	
+	// A Collator for String comparisons
 	private Collator c = Collator.getInstance();
 	
-	/**
-	 * This is called when you do Arrays.sort on an array or Collections.sort on a collection (IE ArrayList).
-	 * 
-	 * It simply compares their names using a Collator. It doesn't know how to compare
-	 * a file with a directory, and returns -1 in this case.
-	 */
+	@Override
 	public int compare(File f1, File f2) {
-		if(f1 == f2)
-			return 0;
-
-		// check if it is comparing a file with a directory, and if so return -1
-		if(f1.isDirectory() && f2.isFile())
-			return -1;
-		if(f1.isFile() && f2.isDirectory())
-			return 1;
-
-
-		// let the Collator do the string comparison, and return the result
+		if (f1 == f2) return 0;
+		
+		// promote directories for file-directory pairs
+		if (f1.isDirectory() && f2.isFile()) return -1;
+		if (f1.isFile() && f2.isDirectory()) return 1;
+		
+		// use Collator for file-file and dir-dir pairs
 		return c.compare(f1.getName(), f2.getName());
 	}
 }
