5450c101a0895f0305b0a37edf61ff88a1d483f4
Kevin Milner <kmilner@usc.edu>
2010-07-26 19:30:42 +0000

    added scripts to SVN

---
+++ scripts/rmi/startRMIServer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mainDir="/usr/local/tomcat/default/webapps/OpenSHA/WEB-INF"
+java="/usr/java/default/bin/java"
+#java="/usr/java/jdk1.6.0_10/jre/bin/java"
+
+$java -Xmx500M -classpath ${mainDir}/dist/OpenSHA_complete.jar -Djava.rmi.server.codebase=file:${mainDir}/dist/OpenSHA_complete.jar -Djava.security.policy=file:$mainDir/SimpleRMI.policy -Djava.rmi.server.hostname=opensha.usc.edu org.opensha.sha.earthquake.rupForecastImpl.remote.RegisterRemoteERF_Factory &
