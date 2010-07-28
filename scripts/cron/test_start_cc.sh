e7dfa3fe4bf2a3c807d052907b65191f0145cd14
Kevin Milner <kmilner@usc.edu>
2010-07-28 20:04:04 +0000

    added cron scripts to SVN

---
+++ scripts/cron/test_start_cc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pids=`ps gx | grep java | grep cruisecontrol-launcher.jar | awk '{ print $1 }'`
+if [[ $pids ]];then
+	echo "cruise control is already running! proc(s): $pids"
+	exit
+fi
+cd /usr/local/cruise/main/
+/usr/local/cruise/main/bin/cruisecontrol.sh -configfile /usr/local/cruise/main/config.xml
