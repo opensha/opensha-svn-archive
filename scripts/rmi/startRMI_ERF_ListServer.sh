ac72d92bc42c6ea7511be7a6b9676d7b46e4da3e
Kevin Milner <kmilner@usc.edu>
2010-08-04 21:54:43 +0000

    updated scripts to use new rmi policy file

--- scripts/rmi/startRMI_ERF_ListServer.sh
+++ scripts/rmi/startRMI_ERF_ListServer.sh
@@ -5,7 +5,8 @@ if [[ `basename $mainDir` != "WEB-INF" ]];then
 	echo "must be run from WEB-INF directory!"
 	exit 2
 fi
+rmiPolicyFile=$mainDir/scripts/rmi/SimpleRMI.policy
 java="/usr/java/default/bin/java"
 #java="/usr/java/jdk1.6.0_10/jre/bin/java"
 
-$java -Xmx500M -classpath ${mainDir}/dist/OpenSHA_complete.jar -Djava.rmi.server.codebase=file:${mainDir}/dist/OpenSHA_complete.jar -Djava.security.policy=file:$mainDir/SimpleRMI.policy -Djava.rmi.server.hostname=opensha.usc.edu org.opensha.sha.earthquake.rupForecastImpl.remote.RegisterRemoteERF_ListFactory &
+$java -Xmx500M -classpath ${mainDir}/dist/OpenSHA_complete.jar -Djava.rmi.server.codebase=file:${mainDir}/dist/OpenSHA_complete.jar -Djava.security.policy=file:$rmiPolicyFile -Djava.rmi.server.hostname=opensha.usc.edu org.opensha.sha.earthquake.rupForecastImpl.remote.RegisterRemoteERF_ListFactory &
