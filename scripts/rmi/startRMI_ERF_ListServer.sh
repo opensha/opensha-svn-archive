ba4074da8e14954d9efa6faf2f2c38e9d5062328
Kevin Milner <kmilner@usc.edu>
2010-08-04 22:08:31 +0000

    fixed path to policy file

--- scripts/rmi/startRMI_ERF_ListServer.sh
+++ scripts/rmi/startRMI_ERF_ListServer.sh
@@ -5,7 +5,7 @@ if [[ `basename $mainDir` != "WEB-INF" ]];then
 	echo "must be run from WEB-INF directory!"
 	exit 2
 fi
-rmiPolicyFile=$mainDir/scripts/rmi/SimpleRMI.policy
+rmiPolicyFile=$mainDir/conf/SimpleRMI.policy
 java="/usr/java/default/bin/java"
 #java="/usr/java/jdk1.6.0_10/jre/bin/java"
 
