94b8521104cfd99466e0f58a6ecd3ea35226db9b
Kevin Milner <kmilner@usc.edu>
2010-08-04 22:49:02 +0000

    added cleanup for duplicate versions for dist dirs

--- scripts/cron/app_nightly_dist_clean.py
+++ scripts/cron/app_nightly_dist_clean.py
@@ -23,7 +23,8 @@ class JarFile:
 		split = fileName.split("-")
 		splitLen = len(split)
 		self.svn = int(split[splitLen-2])
-		self.version = split[splitLen-3]
+		verSplit = split[splitLen-3].split(".")
+		self.version = (int(verSplit[0]), int(verSplit[1]), int(verSplit[2]))
 		
 		dateStr = split[splitLen-1].split(".")[0]
 		dateSplit = dateStr.split("_")
@@ -41,9 +42,20 @@ def containsDate(jars, date):
 			return True
 	return False
 
+def containsVersion(jars, version):
+	for jar in jars:
+		if jar.version == version:
+			return True
+	return False
+
 def processDir(dir):
 	pathSplit = os.path.split(dir)
 	isNightly = pathSplit[1] == "nightly"
+	isDist = pathSplit[1] == "dist"
+	if isNightly:
+		print "handling nightly dir: " + dir
+	elif isDist:
+		print "handling dist dir: " + dir
 	jarFiles = []
 	for fileName in os.listdir(dir):
 		path = dir + os.sep + fileName
@@ -52,8 +64,8 @@ def processDir(dir):
 			continue
 		if not fileName.endswith(".jar"):
 			continue
-		if not isNightly:
-			# this isn't a nightly build folder
+		if not (isNightly or isDist):
+			# this isn't a build folder
 			continue
 		try:
 			jarFile = JarFile(fileName)
@@ -63,7 +75,10 @@ def processDir(dir):
 		jarFiles.append(jarFile)
 	if len(jarFiles) == 0:
 		return
-	jarFiles.sort(key=operator.attrgetter('date', 'svn'), reverse=True)
+	if isNightly:
+		jarFiles.sort(key=operator.attrgetter('date', 'svn'), reverse=True)
+	elif isDist:
+		jarFiles.sort(key=operator.attrgetter('version', 'date', 'svn'), reverse=True)
 	print "jars in dir '" + dir + "':"
 	for jar in jarFiles:
 		print jar.fileName
@@ -72,22 +87,28 @@ def processDir(dir):
 	jarsToKeep = []
 	jarsToDelete = []
 	for jar in jarFiles:
-		daysOld = (firstDate - jar.date).days
-		day = jar.date.day
-		if daysOld <= 7: # keep it if it's less than 7 days old
-			if not containsDate(jarsToKeep, jar.date):
-				jarsToKeep.append(jar)
+		if isNightly:
+			daysOld = (firstDate - jar.date).days
+			day = jar.date.day
+			if daysOld <= 7: # keep it if it's less than 7 days old
+				if not containsDate(jarsToKeep, jar.date):
+					jarsToKeep.append(jar)
+					continue
+			if daysOld <= 30: # keep if it it's less than 30 days and from sunday
+				if day in (1, 8, 15, 22, 29) and not containsDate(jarsToKeep, jar.date):
+					jarsToKeep.append(jar)
+					continue
+			elif daysOld <= 365: # keep it if it > 30 days old and it's from the 1st of the month
+				if day == 1 and not containsDate(jarsToKeep, jar.date):
+					jarsToKeep.append(jar)
+					continue
+			# if we made it here, then just delete it
+			jarsToDelete.append(jar)
+		elif isDist:
+			if containsVersion(jarsToKeep, jar.version):
+				jarsToDelete.append(jar)
 				continue
-		if daysOld <= 30: # keep if it it's less than 30 days and from sunday
-			if day in (1, 8, 15, 22, 29) and not containsDate(jarsToKeep, jar.date):
-				jarsToKeep.append(jar)
-				continue
-		elif daysOld <= 365: # keep it if it > 30 days old and it's from the 1st of the month
-			if day == 1 and not containsDate(jarsToKeep, jar.date):
-				jarsToKeep.append(jar)
-				continue
-		# if we made it here, then just delete it
-		jarsToDelete.append(jar)
+			jarsToKeep.append(jar)
 	print "keeping:"
 	for jar in jarsToKeep:
 		print jar.fileName
