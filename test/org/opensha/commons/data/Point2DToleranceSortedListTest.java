7190d135d1977617cfb4cfc093f73bc02769d230
Kevin Milner <kmilner@usc.edu>
2010-09-05 20:38:26 +0000

    updated test for new point2d list

--- test/org/opensha/commons/data/Point2DToleranceSortedListTest.java
+++ test/org/opensha/commons/data/Point2DToleranceSortedListTest.java
@@ -20,18 +20,36 @@ public class Point2DToleranceSortedListTest {
 		return val;
 	}
 	
+	private static void addRandom(Point2DToleranceSortedList list) {
+		double x = getRandVal();
+		double y = getRandVal();
+		Point2D pt = new Point2D.Double(x, y);
+		
+		list.add(pt);
+	}
+	
+	private static void addRandom(Point2DToleranceSortedList list, int num) {
+		for (int i=0; i<num; i++) {
+			addRandom(list);
+		}
+	}
+	
+	private static void removeRandom(Point2DToleranceSortedList list) {
+		int index = rand.nextInt(list.size());
+		list.remove(index);
+	}
+	
+	private static void removeRandom(Point2DToleranceSortedList list, int num) {
+		for (int i=0; i<num; i++) {
+			removeRandom(list);
+		}
+	}
+	
 	@Test
 	public void testMinMaxYSimple() {
 		Point2DToleranceSortedList list = new Point2DToleranceSortedList(new Point2DToleranceComparator());
 		
-		
-		for (int i=0; i<100; i++) {
-			double x = getRandVal();
-			double y = getRandVal();
-			Point2D pt = new Point2D.Double(x, y);
-			
-			list.add(pt);
-		}
+		addRandom(list, 100);
 		
 		validateMinMax(list);
 	}
@@ -55,30 +73,50 @@ public class Point2DToleranceSortedListTest {
 	public void testMinMaxYComplicated() {
 		Point2DToleranceSortedList list = new Point2DToleranceSortedList(new Point2DToleranceComparator());
 		
-		for (int i=0; i<100; i++) {
-			double x = getRandVal();
-			double y = getRandVal();
-			Point2D pt = new Point2D.Double(x, y);
-			
-			list.add(pt);
-		}
+		addRandom(list, 100);
 		
 		validateMinMax(list);
 		
 		for (int i=0; i<50; i++) {
-			int index = rand.nextInt(list.size());
-			list.remove(index);
+			removeRandom(list);
 			validateMinMax(list);
 		}
 		
 		for (int i=0; i<10; i++) {
-			double x = getRandVal();
-			double y = getRandVal();
-			Point2D pt = new Point2D.Double(x, y);
-			
-			list.add(pt);
+			addRandom(list);
 			validateMinMax(list);
 		}
 	}
+	
+	private static void validateListOrder(Point2DToleranceSortedList list) {
+		Point2D prev = null;
+		for (Point2D pt : list) {
+			if (prev != null) {
+				assertTrue("list order error: " + prev + ", " + pt, prev.getX() < pt.getX());
+			}
+			prev = pt;
+		}
+	}
+	
+	@Test
+	public void testListOrder() {
+		Point2DToleranceSortedList list = new Point2DToleranceSortedList(new Point2DToleranceComparator());
+		
+		for (int i=0; i<100; i++) {
+			addRandom(list);
+			validateListOrder(list);
+		}
+		
+		for (int i=0; i<50; i++) {
+			removeRandom(list);
+			validateListOrder(list);
+		}
+		
+		for (int i=0; i<10; i++) {
+			addRandom(list);
+			validateListOrder(list);
+		}
+	}
 
+	
 }
