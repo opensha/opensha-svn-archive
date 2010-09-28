85fb9f82e78535825068179dfe2920bd51d1b9ee
Kevin Milner <kmilner@usc.edu>
2010-09-28 19:01:40 +0000

    took pw out of cron, added operational test

--- scripts/cron/pref_data_update.sh
+++ scripts/cron/pref_data_update.sh
@@ -4,4 +4,4 @@ dir=`dirname $0`
 
 jar="${dir}/dist/OpenSHA_complete.jar"
 
-java -classpath $jar org.opensha.refFaultParamDb.dao.db.PrefFaultSectionDataDB_DAO opensha_cron cron_update
+java -classpath $jar org.opensha.refFaultParamDb.dao.db.PrefFaultSectionDataDB_DAO opensha_cron $1
