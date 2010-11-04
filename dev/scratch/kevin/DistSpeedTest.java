0ba1071f6ac4cc16b3e74228550491428a7b00f8
Kevin Milner <kmilner@usc.edu>
2010-11-04 19:50:31 +0000

    adding distance speed tests to svn

--- dev/scratch/kevin/DistSpeedTest.java
+++ dev/scratch/kevin/DistSpeedTest.java
@@ -1,44 +1,118 @@
 package scratch.kevin;
 
-import java.util.Iterator;
-
 import org.opensha.commons.geo.Location;
+import org.opensha.commons.geo.LocationList;
 import org.opensha.commons.geo.LocationUtils;
-import org.opensha.sha.earthquake.EqkRupForecast;
-import org.opensha.sha.earthquake.ProbEqkRupture;
-import org.opensha.sha.earthquake.ProbEqkSource;
-import org.opensha.sha.earthquake.rupForecastImpl.WGCEP_UCERF_2_Final.MeanUCERF2.MeanUCERF2;
-import org.opensha.sha.faultSurface.EvenlyGriddedSurfaceAPI;
+import org.opensha.commons.geo.LocationVector;
+import org.opensha.commons.util.DataUtils;
 
 public class DistSpeedTest {
 	
-	public static void main(String[] args) {
-		EqkRupForecast erf = new MeanUCERF2();
-		erf.updateForecast();
-		
-		Location testLoc = new Location(34, -118);
-		
-		long start = System.currentTimeMillis();
-		
-		for (int sourceID=0; sourceID<erf.getNumSources(); sourceID++) {
-			ProbEqkSource source = erf.getSource(sourceID);
-			for (int rupID=0; rupID<source.getNumRuptures(); rupID++) {
-				ProbEqkRupture rup = source.getRupture(rupID);
-				EvenlyGriddedSurfaceAPI surface = rup.getRuptureSurface();
-				
-				Iterator<Location> it = surface.iterator();
-				while (it.hasNext()) {
-					Location loc = it.next();
-//					LocationUtils.linearDistance(testLoc, loc);
-					LocationUtils.linearDistanceFast(testLoc, loc);
-				}
+	private Location startLoc;
+	private LocationVector vector;
+	private int iterations;
+	
+	private LocationList locs;
+	private double[] fastDists;
+	private double[] slowDists;
+	private long[] fastTimes;
+	private long[] slowTimes;
+	
+	private long fastMilis;
+	private long slowMilis;
+	
+	public DistSpeedTest(Location startLoc, LocationVector vector, int iterations) {
+		this.startLoc = startLoc;
+		this.vector = vector;
+		this.iterations = iterations;
+	}
+	
+	public void doCalc() {
+		if (locs == null) {
+			Location testLoc = startLoc;
+			locs = new LocationList();
+			
+			for (int i=0; i<iterations; i++) {
+				testLoc = LocationUtils.location(testLoc, vector);
+				locs.add(testLoc);
 			}
 		}
 		
-		long milis = System.currentTimeMillis() - start;
-		double secs = (double)milis / 1000d;
+		slowDists = new double[locs.size()];
+		slowTimes = new long[locs.size()];
+		slowMilis = doTest(false);
+		fastDists = new double[locs.size()];
+		fastTimes = new long[locs.size()];
+		fastMilis = doTest(true);
+	}
+	
+	public void printReport() {
+		double fastSecs = (double)fastMilis / 1000d;
+		double slowSecs = (double)slowMilis / 1000d;
+		double avgFast = (double)fastMilis / (double)locs.size();
+		double avgSlow = (double)slowMilis / (double)locs.size();
+		double speedup = slowSecs / fastSecs;
+		System.out.println("TOTAL TIMES");
+		System.out.println("Fast: " + fastMilis + " milis = " + fastSecs + " secs (" + avgFast + " milis/calc)");
+		System.out.println("Slow: " + slowMilis + " milis = " + slowSecs + " secs (" + avgSlow + " milis/calc)");
+		System.out.println("Speedup: " + speedup + "x");
 		
-		System.out.println("Took " + secs + " secs");
+		int targetNum = 100;
+		
+		int modulus = 1;
+		if (iterations > targetNum)
+			modulus = iterations / targetNum;
+		
+		System.out.println("Printing with modulus " + modulus);
+		
+		System.out.println();
+		System.out.println("slow dist,slow time,fast dist,fast time,fast-slow,percent diff,start lat,start lon,end lat, end lon");
+		for (int i=0; i<iterations; i++) {
+			if ((i+1) % modulus != 0)
+				continue;
+			double fastDist = fastDists[i];
+			double slowDist = slowDists[i];
+			Location loc = locs.get(i);
+			
+			double diff = fastDist - slowDist;
+			double pDiff = DataUtils.getPercentDiff(fastDist, slowDist);
+			
+			System.out.println(slowDist+","+slowTimes[i]+","+fastDist+","+fastTimes[i]+","+diff+","+pDiff+","
+					+startLoc.getLatitude()+","+startLoc.getLongitude()+","+loc.getLatitude()+","+loc.getLongitude());
+		}
+	}
+	
+	private long doTest(boolean fast) {
+		long start = System.currentTimeMillis();
+		for (int i=0; i<locs.size(); i++) {
+			if (fast) {
+				fastDists[i] = LocationUtils.linearDistanceFast(startLoc, locs.get(i));
+				fastTimes[i] = System.currentTimeMillis()-start;
+			} else {
+				slowDists[i] = LocationUtils.linearDistance(startLoc, locs.get(i));
+				slowTimes[i] = System.currentTimeMillis()-start;
+			}
+		}
+		long end = System.currentTimeMillis();
+		return end - start;
+	}
+	
+	public static void main(String[] args) {
+
+		Location testLoc = new Location(34, -118);
+		double azimuth = Math.toRadians(45);
+		double horizDist = 0.001d;
+		double vertDist = 0d;
+		LocationVector vector = new LocationVector(azimuth, horizDist, vertDist);
+		
+		int iterations = 5000000;
+		
+		DistSpeedTest test = new DistSpeedTest(testLoc, vector, iterations);
+		
+		test.doCalc();
+		test.doCalc();
+		test.doCalc();
+		test.printReport();
 	}
 
 }
