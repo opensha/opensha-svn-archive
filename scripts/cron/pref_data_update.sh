ae00f6cf0f2909ab3e65c7497c76eb0764436f7b
Kevin Milner <kmilner@usc.edu>
2010-11-12 22:03:41 +0000

    fix for ticket #76

--- scripts/cron/pref_data_update.sh
+++ scripts/cron/pref_data_update.sh
@@ -2,6 +2,6 @@
 
 dir=`dirname $0`
 
-jar="${dir}/dist/OpenSHA_complete.jar"
+jar="${dir}/../../dist/OpenSHA_complete.jar"
 
 java -classpath $jar org.opensha.refFaultParamDb.dao.db.PrefFaultSectionDataDB_DAO opensha_cron $1
