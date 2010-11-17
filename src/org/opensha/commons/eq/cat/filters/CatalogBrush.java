92a7fb32c99b2fc26e6e9fab3ec2a44dbe1a744f
Peter Powers <pmpowers@usgs.gov>
2010-11-17 22:51:36 +0000

    updating catalogbrush

--- src/org/opensha/commons/eq/cat/filters/CatalogBrush.java
+++ src/org/opensha/commons/eq/cat/filters/CatalogBrush.java
@@ -3,25 +3,25 @@ package org.opensha.commons.eq.cat.filters;
 import static org.opensha.commons.eq.cat.util.DataType.*;
 import static org.opensha.commons.eq.cat.CatTools.*;
 import static org.opensha.commons.geo.GeoTools.*;
+import static org.opensha.commons.eq.cat.filters.CatalogBrush.LimitKey.*;
+import static org.opensha.commons.eq.cat.filters.CatalogBrush.SelectionChange.*;
 
 import java.util.Arrays;
+import java.util.BitSet;
 import java.util.EnumSet;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 import org.opensha.commons.eq.cat.MutableCatalog;
-import org.opensha.commons.eq.cat.util.DataBound;
 import org.opensha.commons.eq.cat.util.DataType;
-import org.opensha.commons.geo.GeoTools;
 import org.opensha.commons.util.DataUtils;
 
 /**
- * Add comments here
- * 
- * memory overhead
- * creates sorted copies of data
- * All methods in this class fail silently
+ * This class may be used to efficiently brush catalog data. Specifically, the
+ * following <code>DataType</code>s can be filtered using min-max constraints:
+ * <code>[ TIME, LATITUDE,LONGITUDE, DEPTH, MAGNITUDE ]</code> For large
+ * catalogs it has a significant memory footprint once instantiated because it
+ * internally manages index arrays and sorted copies of catalog source data
+ * arrays.
  * 
  * @author Peter Powers
  * @version $Id:$
@@ -29,167 +29,178 @@ import org.opensha.commons.util.DataUtils;
 public class CatalogBrush {
 
 	private MutableCatalog catalog;
-	private EnumSet<DataType> brushables = EnumSet.of(
-		TIME, LATITUDE, LONGITUDE, DEPTH, MAGNITUDE);
-	
+	private EnumSet<DataType> brushables = EnumSet.of(TIME, LATITUDE,
+		LONGITUDE, DEPTH, MAGNITUDE);
+
 	private Map<DataType, double[]> sortedSources;
 	private Map<DataType, int[]> sourceIndices;
 	private Map<DataType, Integer> minCarets; // inclusive
-	private Map<DataType, Integer> maxCarets; // inclusive
-	private Map<DataType, Double> minValues;
-	private Map<DataType, Double> maxValues;
-	
+	private Map<DataType, Integer> maxCarets; // exclusive
+
+	private BitSet selectionChange;
+	private BitSet selection;
+
 	/**
 	 * Initializes a new data brush with the supplied catalog.
+	 * 
 	 * @param catalog
 	 */
 	public CatalogBrush(MutableCatalog catalog) {
 		this.catalog = catalog;
 		brushables.retainAll(catalog.getDataTypes());
+		selectionChange = new BitSet(catalog.size());
+		selection = new BitSet(catalog.size());
+		selection.set(0, selection.length());
 		initMaps();
 	}
-	
+
 	/**
-	 * Adjusts the catalog selection. Returns whether the requested change had
-	 * an effect. If the supplied brush <code>key</code> is not applicable
-	 * for the associated catalog (i.e. catalog does not contain data
-	 * corresponding to the <code>key</code>), or the supplied value is out of
-	 * range for the key or the key's compelement, no catalog selection change
-	 * occurs and this method returns <code>false</code>.
-	 * @param type of data to adjust
-	 * @param key to adjust
-	 * @param value for key
-	 * @return <code>true</code> if catalog selection has changed, 
-	 * <code>false</code> otherwise
+	 * Returns the current selection as a <code>BitSet</code>.
+	 * 
+	 * @return the current selection
 	 */
-	public boolean adjust(DataType type, Key key, double value) {
-		if (!brushables.contains(type)) return false;
-		if (!validateValue(type, value)) return false;
-		if (key == Key.MIN) {
-			return adjustMin(type, value);
-		} else {
-			return adjustMax(type, value);
-		}
-		
-		// determine index
-//		int idx = Arrays.binarySearch(sortedSources.get(type), value);
-//		
-//		// need to prevent brush overlap
-//		if (key == Key.MIN) {
-//			
-//		}
-//		
-//		
-//		return false;
+	public BitSet selection() {
+		return selection;
 	}
+
+	/**
+	 * Returns the current selection change as a <code>BitSet</code>.
+	 *
+	 * @return the current selection change
+	 */
+	public BitSet selectionChange() {
+		return selectionChange;
+	}
+
 	
-	// binary search is inclusive (min) and exclusive (max)
-	// when carat is at min or max, we want to avoid specifying 0 length of array to search which always returns -1
-	
-	// caratMin can never == caratMax and vice versa
-	
-	private boolean adjustMin(DataType type, double value) {
-		double currentMin = minValues.get(type);
-		double catalogMin = catalog.minForType(type);
-		
+	public SelectionChange adjust(LimitKey key, long value) {
+		return NONE;
+	}
+
+	/**
+	 * Adjusts the catalog selection and returns a flag describing the change.
+	 * If the <code>DataType</code> supplied is not applicable for the
+	 * associated catalog, the supplied value is out of range for the the
+	 * <code>DataType</code>, or the selection hasn't changed,
+	 * <code>SelectionChange.NONE</code> is returned. Otherwise method returns
+	 * <code>SelectionChange.ADD</code> or <code>SelectionChange.REMOVE</code>.
+	 * Once method returns, the <code>BitSet</code> returned by
+	 * {@link #selectionChange()} represents the change.
+	 * 
+	 * @param type of data to adjust
+	 * @param key to adjust - min or max limit
+	 * @param newValue for limit
+	 * @return <code>true</code> if catalog selection has changed,
+	 *         <code>false</code> otherwise
+	 */
+	public SelectionChange adjust(DataType type, LimitKey key, double newValue) {
+		// reset change tracker
+		selectionChange.clear();
+		// check inputs
+		if (!brushables.contains(type)) return NONE;
+		if (!validateValue(type, newValue)) return NONE;
+		// update adjustor
+		SelectionChange change;
+		if (key == MIN) {
+			change = adjustMin(type, newValue);
+		} else {
+			change = adjustMax(type, newValue);
+		}
+		// update selection
+		if (change == ADD) {
+			selection.or(selectionChange);
+		} else {
+			selection.andNot(selectionChange);
+		}
+		return change;
+	}
+
+	/*
+	 * Performs an update to a min limit change.
+	 */
+	private SelectionChange adjustMin(DataType type, double newValue) {
 		double[] source = sortedSources.get(type);
 		int minCaret = minCarets.get(type);
 		int maxCaret = maxCarets.get(type);
-		
-		int idx = adjustMin(source, minCaret, maxCaret, value);
-		if (idx == -1) return false;
-		
-//		int idx = Arrays.binarySearch(source, value);
-//		if (idx == minCaret) return false;
-//		// adjust values outside source array limits
-//		idx = (idx == -1) ? 0 : (idx < -1) ? -idx - 1 : idx;
-//		// prevent min caret from equalling or passing max caret 
-//		idx = (idx >= maxCaret) ? maxCaret - 1 : idx;
-//		// adjust values down so that all equal to value are included
-//		while (source[idx]-1 == value) {
-//			idx--;
-//		}
-		
-//		minCarets
-//			
-//		} else if
-//		int newCarat = 
-//		
-//		
-//		if (value == currentMin) {
-//			return false;
-//		} else if (value <= catalogMin) {
-//			minCarats.put(type, 0);
-//			// TODO update selection;
-//			return true;
-//		} else {
-//			idx = Arrays.binarySearch(sortedSources.get(type), 0, minCarats.get(type)+1, value);
-//			
-//		} else {
-//			idx = Arrays.binarySearch(sortedSources.get(type), minCarats.get(type), maxCarats.get(type), value);
-//		}
-//		//if 
-//		//int idx = Arrays.binarySearch(sortedSources.get(type), value);
-//		
-		return false;
+		double oldValue = source[minCaret];
+		int idx = calcMinCaret(source, minCaret, maxCaret, oldValue, newValue);
+		if (idx == -1) return NONE;
+		int minSelect = (idx < minCaret) ? idx : minCaret;
+		int maxSelect = (idx < minCaret) ? minCaret : idx;
+		minCarets.put(type, idx);
+		updateSelection(type, minSelect, maxSelect);
+		return (idx < minCaret || idx > maxCaret) ? ADD : REMOVE;
 	}
-	
-	private static int adjustMin(double[] data, int minCaret, int maxCaret, double value) {
-		int idx = Arrays.binarySearch(data, value);
-		if (idx == minCaret) return -1;
-		// adjust values outside source array limits
+
+	/*
+	 * Performs an update to a max limit change.
+	 */
+	private SelectionChange adjustMax(DataType type, double newValue) {
+		double[] source = sortedSources.get(type);
+		int minCaret = minCarets.get(type);
+		int maxCaret = maxCarets.get(type);
+		double oldValue = source[maxCaret];
+		int idx = calcMaxCaret(source, minCaret, maxCaret, oldValue, newValue);
+		if (idx == -1) return NONE;
+		int minSelect = (idx < maxCaret) ? idx : maxCaret;
+		int maxSelect = (idx < maxCaret) ? maxCaret : idx;
+		minCarets.put(type, idx);
+		updateSelection(type, minSelect, maxSelect);
+		return (idx < minCaret || idx > maxCaret) ? ADD : REMOVE;
+	}
+
+	/*
+	 * Determines new minimum caret. Package private for testing.
+	 */
+	static int calcMinCaret(double[] data, int minCaret, int maxCaret,
+			double oldValue, double newValue) {
+		if (oldValue == newValue) return -1;
+		int idx = (newValue > oldValue) ? Arrays.binarySearch(data, minCaret,
+			maxCaret, newValue) : Arrays.binarySearch(data, 0, minCaret,
+			newValue);
+		// adjust caret values outside source array limits
 		idx = (idx == -1) ? 0 : (idx < -1) ? -idx - 1 : idx;
-		// prevent min caret from equalling or passing max caret 
+		// prevent min caret from equalling or passing max caret
 		idx = (idx >= maxCaret) ? maxCaret - 1 : idx;
-		// adjust values down so that all equal to value are included
-		while (data[idx-1] == value) {
+		// adjust caret down so that all equal to value are included
+		while (idx != 0 && data[idx - 1] == data[idx]) {
 			idx--;
 		}
-		return idx;
+		return (idx == minCaret) ? -1 : idx;
 	}
 
-	private static int adjustMax(double[] data, int minCaret, int maxCaret, double value) {
-		int idx = Arrays.binarySearch(data, value);
-		if (idx == maxCaret) return -1;
-		// adjust values outside source array limits
+	/*
+	 * Determines new maximum caret. Package private for independent testing.
+	 */
+	static int calcMaxCaret(double[] data, int minCaret, int maxCaret,
+			double oldValue, double newValue) {
+		if (oldValue == newValue) return -1;
+		int idx = (newValue > oldValue) ? Arrays.binarySearch(data, maxCaret,
+			data.length, newValue) : Arrays.binarySearch(data, minCaret,
+			maxCaret, newValue);
+		// set idx so that it can be incremented by one at end of method
+		// adjust caret values outside source array limits
 		idx = (idx == -1) ? 0 : (idx < -1) ? -idx - 2 : idx;
-		// prevent max caret from equalling or passing min caret 
-		idx = (idx <= minCaret) ? minCaret + 1 : idx;
-		// adjust values up so that all equal to value are included
-		while (data[idx+1] == value) {
+		// prevent max caret from equalling or passing min caret
+		idx = (idx <= minCaret) ? minCaret : idx;
+		// adjust caret up so that all equal to value are included
+		while (idx < data.length - 1 && data[idx + 1] == data[idx]) {
 			idx++;
 		}
-		return idx;
+		return (idx + 1 == maxCaret) ? -1 : ++idx;
 	}
 
-	private boolean adjustMax(DataType type, double value) {
-		return false;
-	}
-	
-	
-	public boolean adjust(DataBound key, long value) {
-		return false;
-	}
-	
-	public boolean[] selectionMask() {
-		return null;
-	}
-	
-	public static void main(String[] args) {
-		double[] M = {0.3,0.7,1.1,1.1,1.1,1.1,1.8,2.2,2.7,2.7,2.7,2.7,2.8,3.1,3.6,4.5,4.5,4.5,4.5,4.5,4.5,4.5,4.5,5.1,6.7,7.2};
-		int cMin = 0;
-		int cMax = 25;
-		System.out.println(adjustMax(M,cMin,cMax, 2.71));
-	}
-	
-	/**
-	 * Returns the indices of currently selected events.
-	 * @return the selected catalog event indices
+	/*
+	 * Flips the required bits. 'adjustor' has always been cleared prior to
+	 * calliong this methd
 	 */
-	public int[] selection() {
-		return null;
+	private void updateSelection(DataType type, int min, int max) {
+		int[] indices = sourceIndices.get(type);
+		for (int i = min; i < max; i++) {
+			selectionChange.flip(indices[i]);
+		}
 	}
-	
+
 	/* initialize all data tracking maps */
 	private void initMaps() {
 		for (DataType type : brushables) {
@@ -199,20 +210,22 @@ public class CatalogBrush {
 			sourceIndices.put(type, DataUtils.indexAndSort(source));
 			sortedSources.put(type, source);
 			minCarets.put(type, 0);
-			maxCarets.put(type, catalog.size()-1);
-			minValues.put(type, catalog.minForType(type));
-			maxValues.put(type, catalog.maxForType(type));
+			maxCarets.put(type, catalog.size());
 		}
 	}
-	
+
 	/* determine whether a supplied value is allowed */
 	private boolean validateValue(DataType type, double value) {
 		try {
 			switch (type) {
-				case LATITUDE: validateLat(value);
-				case LONGITUDE: validateLon(value);
-				case DEPTH: validateDepth(value);
-				case MAGNITUDE: validateMag(value);
+				case LATITUDE:
+					validateLat(value);
+				case LONGITUDE:
+					validateLon(value);
+				case DEPTH:
+					validateDepth(value);
+				case MAGNITUDE:
+					validateMag(value);
 			}
 		} catch (IllegalArgumentException iae) {
 			return false;
@@ -224,12 +237,23 @@ public class CatalogBrush {
 	 * Key used to identify whether DataBrush should adjust a minimum or a
 	 * maximum value.
 	 */
-	public enum Key {
-
+	public enum LimitKey {
 		/** Minimum value identifier. */
 		MIN,
 		/** Maximum value identifier. */
 		MAX;
+	}
+
+	/**
+	 * Identifies a type of selection change.
+	 */
+	public enum SelectionChange {
+		/** Selection was added to. */
+		ADD,
+		/** Selection was reduced. */
+		REMOVE,
+		/** No change occurred. */
+		NONE;
 	}
 
 }
