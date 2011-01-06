6ccc58d431519ce12b6cad8c55d27a534ba9777f
Kevin Milner <kmilner@usc.edu>
2011-01-06 22:12:33 +0000

    changed to be time independent

--- src/org/opensha/sha/calc/hazus/parallel/HardCodedTest.java
+++ src/org/opensha/sha/calc/hazus/parallel/HardCodedTest.java
@@ -137,8 +137,8 @@ public class HardCodedTest {
 
 	public static void main(String args[]) throws IOException, InvocationTargetException {
 		int years = 50;
-		int startYear = 2010;
-//		int startYear = -1;
+//		int startYear = 2010;
+		int startYear = -1;
 		boolean includeBackSeis = true;
 		EqkRupForecast erf = getERF(years, startYear, includeBackSeis);
 		
