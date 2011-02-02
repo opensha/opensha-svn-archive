37e2822c682944ee815441726aefa1528dad0e5c
Peter Powers <pmpowers@usgs.gov>
2011-02-02 17:29:18 +0000

    added app Icon fethcer method

--- src/org/opensha/ui/components/Resources.java
+++ src/org/opensha/ui/components/Resources.java
@@ -2,6 +2,11 @@ package org.opensha.ui.components;
 
 import java.net.URL;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 /**
  * Wrapper class for easy access to shared resources used by OpenSHA
  * applications such as icons and license files etc.
@@ -11,17 +16,37 @@ import java.net.URL;
  */
 public class Resources {
 
+	private static String resPath = "/resources/";
+	private static String imgPath = resPath + "images/";
 	private static URL license;
-	
+
 	static {
-		license = Resources.class.getResource("/resources/LICENSE.html");
+		license = Resources.class.getResource(resPath + "LICENSE.html");
 	}
-	
+
 	/**
 	 * Returns the <code>URL</code> of the OpenSHA license/disclaimer file.
 	 * @return the license/disclaier <code>URL</code>
 	 */
 	public static URL getLicense() {
 		return license;
+	}
+
+	/**
+	 * Retruns the 64x64 pixel OpenSHA logo.
+	 * @return the logo
+	 */
+	public static Icon getLogo64() {
+		return new ImageIcon(Resources.class.getResource(imgPath +
+			"logos/opensha_64.png"));
+	}
+	
+	// TODO clean
+	public static void main(String[] args) {
+		JFrame f = new JFrame();
+		JLabel l = new JLabel(getLogo64());
+		f.add(l);
+		f.pack();
+		f.setVisible(true);
 	}
 }
