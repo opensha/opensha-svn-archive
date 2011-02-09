e7090e1fab9ed4ec65386220d6bff1262deb511a
Peter Powers <pmpowers@usgs.gov>
2011-02-09 18:06:44 +0000

    added devstatus enum

---
+++ src/org/opensha/commons/util/DevStatus.java
@@ -0,0 +1,23 @@
+package org.opensha.commons.util;
+
+/**
+ * Class that may be used to represent different states of development. For
+ * example, a particular interface or abstract class may have some
+ * implementations that have been vetted and tested and are ready for use in
+ * production environments while others are under development, experimental, or
+ * deprecated.
+ * 
+ * @author Peter Powers
+ * @version $Id:$
+ */
+public enum DevStatus {
+
+	/** Status indicating something is production ready. */
+	PRODUCTION,
+
+	/** Status indicating something is under development. */
+	DEVELOPMENT,
+
+	/** Status indicating something is deprecated. */
+	DEPRECATED;
+}
