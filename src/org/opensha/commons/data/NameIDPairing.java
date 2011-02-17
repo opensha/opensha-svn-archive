832783e9246f9122cf1c05bec70bdb015ffc381d
Kevin Milner <kmilner@usc.edu>
2011-02-17 00:38:50 +0000

    added equals/hashcode

--- src/org/opensha/commons/data/NameIDPairing.java
+++ src/org/opensha/commons/data/NameIDPairing.java
@@ -1,7 +1,7 @@
 package org.opensha.commons.data;
 
 public class NameIDPairing implements NamedObjectAPI {
-	
+
 	private int id;
 	private String name;
 	
@@ -17,6 +17,34 @@ public class NameIDPairing implements NamedObjectAPI {
 	
 	public int getID() {
 		return id;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		NameIDPairing other = (NameIDPairing) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
 	}
 
 }
