a6c6d860a2f59402933e3aadbdc707dd967abe18
Kevin Milner <kmilner@usc.edu>
2011-09-22 01:26:39 +0000

    added caching for fault system solutions

---
+++ dev/scratch/UCERF3/CachedFaultSystemSolution.java
@@ -0,0 +1,120 @@
+package scratch.UCERF3;
+
+import java.util.HashMap;
+
+import org.opensha.sha.gui.infoTools.CalcProgressBar;
+
+
+public class CachedFaultSystemSolution extends SimpleFaultSystemSolution {
+	
+	private boolean showProgress;
+
+	public CachedFaultSystemSolution(FaultSystemSolution solution) {
+		this(solution, false);
+	}
+	
+	public CachedFaultSystemSolution(FaultSystemSolution solution, boolean showProgress) {
+		super(solution);
+		this.showProgress = showProgress;
+	}
+	
+	/* PARTICIPATION RATES */
+	
+	HashMap<String, double[]> particRatesCache = new HashMap<String, double[]>();
+
+	@Override
+	public synchronized double calcParticRateForSect(int sectIndex, double magLow,
+			double magHigh) {
+		return calcParticRateForAllSects(magLow, magHigh)[sectIndex];
+	}
+	
+	@Override
+	public synchronized double[] calcParticRateForAllSects(double magLow, double magHigh) {
+		String key = (float)magLow+"_"+(float)magHigh;
+		if (!particRatesCache.containsKey(key)) {
+			double[] particRates = new double[getNumSections()];
+			CalcProgressBar p = null;
+			if (showProgress) p = new CalcProgressBar("Fault System Solution", "Calculating Participation Rates");
+			for (int i=0; i<particRates.length; i++) {
+				if (showProgress) p.updateProgress(i, particRates.length);
+				particRates[i] = super.calcParticRateForSect(i, magLow, magHigh);
+			}
+			if (showProgress) p.dispose();
+			particRatesCache.put(key, particRates);
+		}
+		return particRatesCache.get(key);
+	}
+	
+	/* TOTAL PARTICIPATION RATES */
+	
+	private double[] totParticRatesCache;
+
+	@Override
+	public synchronized double calcTotParticRateForSect(int sectIndex) {
+		return calcTotParticRateForAllSects()[sectIndex];
+	}
+
+	@Override
+	public synchronized double[] calcTotParticRateForAllSects() {
+		if (totParticRatesCache == null) {
+			totParticRatesCache = new double[getNumSections()];
+			CalcProgressBar p = null;
+			if (showProgress) p = new CalcProgressBar("Fault System Solution", "Calculating Total Participation Rates");
+			for (int i=0; i<totParticRatesCache.length; i++) {
+				if (showProgress) p.updateProgress(i, totParticRatesCache.length);
+				totParticRatesCache[i] = super.calcTotParticRateForSect(i);
+			}
+			if (showProgress) p.dispose();
+		}
+		return totParticRatesCache;
+	}
+	
+	/* PALEO-VISIBLE RATES */
+	
+	private double[] paleoVisibleRatesCache;
+
+	@Override
+	public synchronized double calcTotPaleoVisibleRateForSect(int sectIndex) {
+		return calcTotPaleoVisibleRateForAllSects()[sectIndex];
+	}
+
+	@Override
+	public synchronized double[] calcTotPaleoVisibleRateForAllSects() {
+		if (paleoVisibleRatesCache == null) {
+			paleoVisibleRatesCache = new double[getNumSections()];
+			CalcProgressBar p = null;
+			if (showProgress) p = new CalcProgressBar("Fault System Solution", "Calculating Paleo Visible Rates");
+			for (int i=0; i<paleoVisibleRatesCache.length; i++) {
+				if (showProgress) p.updateProgress(i, paleoVisibleRatesCache.length);
+				paleoVisibleRatesCache[i] = super.calcTotPaleoVisibleRateForSect(i);
+			}
+			if (showProgress) p.dispose();
+		}
+		return paleoVisibleRatesCache;
+	}
+	
+	/* SLIP RATES */
+	
+	private double[] slipRatesCache;
+
+	@Override
+	public synchronized double calcSlipRateForSect(int sectIndex) {
+		return calcSlipRateForAllSects()[sectIndex];
+	}
+
+	@Override
+	public synchronized double[] calcSlipRateForAllSects() {
+		if (slipRatesCache == null) {
+			slipRatesCache = new double[getNumSections()];
+			CalcProgressBar p = null;
+			if (showProgress) p = new CalcProgressBar("Fault System Solution", "Calculating Slip Rates");
+			for (int i=0; i<slipRatesCache.length; i++) {
+				if (showProgress) p.updateProgress(i, slipRatesCache.length);
+				slipRatesCache[i] = super.calcSlipRateForSect(i);
+			}
+			if (showProgress) p.dispose();
+		}
+		return slipRatesCache;
+	}
+
+}
