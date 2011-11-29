5bf76d05e0df533621b3061b3cc700c4a8699da7
Kevin Milner <kmilner@usc.edu>
2011-11-29 19:44:39 +0000

    added timeout and fallback to icon fetching

--- src/org/opensha/sha/gui/util/IconFetcher.java
+++ src/org/opensha/sha/gui/util/IconFetcher.java
@@ -11,6 +11,8 @@ import javax.swing.JFrame;
 import org.opensha.commons.util.ServerPrefUtils;
 import org.opensha.commons.util.ServerPrefs;
 
+import com.google.common.base.Stopwatch;
+
 public class IconFetcher {
 	
 	private static final boolean D = false;
@@ -37,21 +39,75 @@ public class IconFetcher {
 					url = new URL(addy);
 				} catch (Throwable t) {}
 			}
+			
 			if (url == null)
-				// couldn't load from local or internet
+				// couldn't load from local and url construction failed
 				continue;
+			
+			if (D) System.out.println("loading icon from: " + url);
+			BufferedImage img = loadWithTimeout(url, 2000);
+			if (img == null)
+				break;
+			images.add(img);
+		}
+		if (images.size() == 0) {
 			try {
-				if (D) System.out.println("loading icon from: " + url);
-				images.add(ImageIO.read(url));
+				BufferedImage fallback = ImageIO.read(IconFetcher.class.getResourceAsStream(
+						"/resources/images/logos/opensha_64.png"));
+				images.add(fallback);
 			} catch (IOException e) {
-				//e.printStackTrace();
-				//System.err.println("Error reading icon from: "+url);
+				// print and return null;
+				e.printStackTrace();
 				return null;
 			}
 		}
-		if (images.size() == 0)
-			return null;
 		return images;
+	}
+	
+	private static class ImageReadThread extends Thread {
+		BufferedImage img;
+		URL url;
+		
+		public ImageReadThread(URL url) {
+			this.url = url;
+		}
+		
+		@Override
+		public void run() {
+			try {
+				img = ImageIO.read(url);
+			} catch (IOException e) {}
+		}
+	}
+	
+	private static BufferedImage loadWithTimeout(URL url, long timeout) {
+		ImageReadThread t = new ImageReadThread(url);
+		
+		Stopwatch watch = new Stopwatch();
+		watch.start();
+		t.start();
+		
+		while (t.isAlive() && watch.elapsedMillis() < timeout) {
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				// shouldn't happen, but in case just print it and continue waiting
+				e.printStackTrace();
+			}
+		}
+		
+		if (t.isAlive()) {
+			// exceeded timeout
+			try {
+				t.interrupt();
+			} catch (Exception e) {
+				// just print it and continue with failure
+				e.printStackTrace();
+			}
+			return null;
+		}
+		
+		return t.img;
 	}
 	
 	public static void main(String[] args) throws IOException {
