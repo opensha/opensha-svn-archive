3409541edc4ed9362ad4ac685004ea32092d4fec
Kevin Milner <kmilner@usc.edu>
2012-02-24 20:17:14 +0000

    added ID field to fault model branch enum

--- dev/scratch/UCERF3/enumTreeBranches/FaultModelBranches.java
+++ dev/scratch/UCERF3/enumTreeBranches/FaultModelBranches.java
@@ -2,17 +2,28 @@ package scratch.UCERF3.enumTreeBranches;
 
 public enum FaultModelBranches {
 
-	FM3_1("Fault Model 3.1"),
-	FM3_2("Fault Model 3.2");
+	FM3_1("Fault Model 3.1", 101),
+	FM3_2("Fault Model 3.2", 102);
 	
 	private String modelName;
+	private int id;
 	
-	private FaultModelBranches(String modelName) {
+	private FaultModelBranches(String modelName, int id) {
 		this.modelName = modelName;
+		this.id = id;
 	}
 	
 	public String getName() {
 		return modelName;
+	}
+	
+	public int getID() {
+		return id;
+	}
+	
+	@Override
+	public String toString() {
+		return getName();
 	}
 	
 }
