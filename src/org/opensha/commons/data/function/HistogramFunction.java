b3c2a0cf7bd695bdd145c9b350782e7de3c0995e
Ned Field <field@usgs.gov>
2012-02-29 16:21:33 +0000

    Added a HistogramFunction class because I got tired of setting the tolerance of an EvenlyDiscretizedFunc, and wanted some related utility methods.  Sorry, no time to enter a trac ticket.

---
+++ src/org/opensha/commons/data/function/HistogramFunction.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package org.opensha.commons.data.function;
+
+/**
+ * This class sets the tolerance high so that it can be used to construct histograms.
+ * 
+ * It also provides some methods relevant to a histogram.
+ * 
+ * @author field
+ *
+ */
+public class HistogramFunction extends EvenlyDiscretizedFunc {
+
+	/**
+	 * @param min
+	 * @param num
+	 * @param delta
+	 */
+	public HistogramFunction(double min, int num, double delta) {
+		super(min, num, delta);
+		this.setTolerance(getDelta());
+	}
+
+	/**
+	 * @param min
+	 * @param max
+	 * @param num
+	 */
+	public HistogramFunction(double min, double max, int num) {
+		super(min, max, num);
+		this.setTolerance(getDelta());
+	}
+
+	/**
+	 * This normalized the function so that the sum of Y valaues is 1.0
+	 */
+	public void normalizeBySumOfY_Vals() {
+		scale(1.0/calcSumOfY_Vals());
+	}
+	
+	/**
+	 * This returns the cumulative distribution function (total number less than and equal to each x-axis value)
+	 * @return
+	 */
+	public HistogramFunction getCumulativeDistFunction() {
+		HistogramFunction cumHist = new HistogramFunction(getMinX(), getNum(), getDelta());
+		double sum=0;
+		for(int i=0;i<getNum();i++) {
+			sum+=getY(i);
+			cumHist.set(i,sum);
+		}
+		return cumHist;
+	}
+	
+}
