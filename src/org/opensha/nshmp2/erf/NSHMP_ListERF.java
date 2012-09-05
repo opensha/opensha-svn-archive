1e88a5168239e144af0761b8f5546e983462b5d3
Peter Powers <pmpowers@usgs.gov>
2012-09-05 22:54:09 +0000

    refactoring NSHMP erf parent classes

---
+++ src/org/opensha/nshmp2/erf/NSHMP_ListERF.java
@@ -0,0 +1,182 @@
+package org.opensha.nshmp2.erf;
+
+import static com.google.common.base.Preconditions.*;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.opensha.commons.data.TimeSpan;
+import org.opensha.commons.param.Parameter;
+import org.opensha.commons.param.event.ParameterChangeEvent;
+import org.opensha.nshmp2.erf.source.NSHMP_ERF;
+import org.opensha.sha.earthquake.AbstractERF;
+import org.opensha.sha.earthquake.AbstractEpistemicListERF;
+import org.opensha.sha.earthquake.ERF;
+import org.opensha.sha.earthquake.ProbEqkSource;
+import org.opensha.sha.nshmp.SourceRegion;
+import org.opensha.sha.nshmp.SourceType;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multimaps;
+
+/**
+ * Custom {@code EpistemicListERF} implementation for NSHAMP. Implementation
+ * allows subsets of nested {@code ERF}s to be retreived by {@code SourceType}
+ * or {@code SourceRegion} and provides {@code add()} and {@code get()} methods
+ * that take and return a {@code NSHMP_ERF} type, respectively.
+ * 
+ * @author Peter Powers
+ * @version $Id:$
+ */
+public abstract class NSHMP_ListERF extends AbstractEpistemicListERF implements
+		Iterable<NSHMP_ERF> {
+
+	private String name;
+	private Multimap<SourceType, NSHMP_ERF> typeMap;
+	private Multimap<SourceRegion, NSHMP_ERF> regionMap;
+	
+	/**
+	 * Instantiates a new ERF backed by the supplied Map
+	 * @param name for this ERF
+	 */
+	public NSHMP_ListERF(String name) {
+		checkArgument(StringUtils.isNotBlank(name),
+			"Must supply a name for this ERF");
+		this.name = name;
+		typeMap = ArrayListMultimap.create();
+		regionMap = ArrayListMultimap.create();
+		
+		timeSpan = new TimeSpan(TimeSpan.NONE, TimeSpan.YEARS);
+		timeSpan.setDuration(1);
+		timeSpan.addParameterChangeListener(this);
+	}
+
+	/**
+	 * Add the supplied {@code NSHMP_ERF} to the internal list of {@code ERF}s.
+	 * @param type of {@code ERF} to add
+	 * @param erf to add
+	 */
+	protected void addERF(NSHMP_ERF erf) {
+		checkNotNull(erf, "Supplied ERF is null");
+		typeMap.put(erf.getSourceType(), erf);
+		regionMap.put(erf.getSourceRegion(), erf);
+		super.addERF(erf, erf.getSourceWeight());
+	}
+
+	/**
+	 * Add the supplied {@code NSHMP_ERF}s to the internal list of {@code ERF}s.
+	 * @param type of {@code ERF} to add
+	 * @param erfs to add
+	 */
+	protected void addERFs(List<? extends NSHMP_ERF> erfs) {
+		checkNotNull(erfs, "Supplied ERF list is null");
+		for (NSHMP_ERF erf : erfs) {
+			addERF(erf);
+		}
+	}
+	
+	@Override
+	public NSHMP_ERF getERF(int idx) {
+		return (NSHMP_ERF) super.getERF(idx);
+	}
+
+//	@Override
+//	public void updateForecast() {
+//		for (List<ERF> erfList : erf_List.values()) {
+//			for (ERF erf : erfList) {
+//				erf.setTimeSpan(timeSpan);
+//				erf.updateForecast();
+//			}
+//		}
+//	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	public int getRuptureCount() {
+		int count = 0;
+		for (int i=0; i<getNumERFs(); i++) {
+			count += getERF(i).getRuptureCount();
+		}
+		return count;
+	}
+	
+	/**
+	 * Returns the number or sources contained in the ERF at idx.
+	 * @param idx
+	 * @return
+	 */
+	public int getSourceCount(int idx) {
+		return getERF(idx).getNumSources();
+	}
+	
+	/**
+	 * Returns the total number of sources that this {@code ERF} represents.
+	 * @return the total number of {@code ERF}s
+	 */
+	public int getSourceCount() {
+		int count = 0;
+		for (ERF erf : erf_List) {
+			count += erf.getNumSources();
+		}
+		return count;
+	}
+
+	/**
+	 * Returns the total number of sources of the specified type.
+	 * @param type requested
+	 * @return the total number of the specified type
+	 */
+	public int getNumSources(SourceType type) {
+		Collection<NSHMP_ERF> erfList = typeMap.get(type);
+		if (erfList == null) return 0;
+		int count = 0;
+		for (ERF erf : erfList) {
+			count += erf.getNumSources();
+		}
+		return count;
+	}
+	
+	@Override
+	public Iterator<NSHMP_ERF> iterator() {
+		return new Iterator<NSHMP_ERF>() {
+			Iterator<ERF> it = erf_List.iterator();
+			// @formatter:off
+			@Override public boolean hasNext() { return it.hasNext(); }
+			@Override public NSHMP_ERF next() { return (NSHMP_ERF) it.next(); }
+			@Override public void remove() { it.remove(); }
+			// @formatter:off
+		};
+	}
+
+	// rebuild a param list
+	private void initParams() {
+	}
+
+	/**
+	 * Returns the
+	 * @param type
+	 * @return
+	 */
+	protected void addSourceParam(SourceType type, Parameter<?> param) {
+//		paramMgr.addSourceParam(type, param);
+	}
+
+	@Override
+	public void parameterChange(ParameterChangeEvent e) {
+
+		// need to propogate time span changes among others
+		// for any group disable -- disable children
+	}
+	
+	
+	
+}
---
+++ src/org/opensha/nshmp2/erf/WUS_ERF.java
@@ -0,0 +1,142 @@
+package org.opensha.nshmp2.erf;
+
+import static org.opensha.sha.nshmp.SourceRegion.*;
+
+import java.util.EnumSet;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.opensha.commons.param.impl.EnumParameter;
+import org.opensha.nshmp2.erf.source.NSHMP_ERF;
+import org.opensha.nshmp2.erf.source.Sources;
+import org.opensha.nshmp2.util.FaultType;
+import org.opensha.nshmp2.util.FocalMech;
+
+/**
+ * Wrapper for NSHMP western US earthquake sources.
+ * 
+ * @author Peter Powers
+ * @version $Id:$
+ */
+public class WUS_ERF extends NSHMP_ListERF {
+
+	public WUS_ERF() {
+		super("NSHMP West");
+		init();
+	}
+
+	// want to get param group/list for each source type and add
+
+	private void initParams() {
+
+	}
+
+	private static final String NSHMP_FAULT_TYPE_FILTER_PARAM_NAME = "Fault Type Filter";
+	private static final String NSHMP_FOCAL_MECH_FILTER_PARAM_NAME = "Focal Mech Filter";
+
+	private void initFaultParams() {
+
+		EnumParameter<FaultType> faultTypeFilter = new EnumParameter<FaultType>(
+			"Fault Type Filter", EnumSet.allOf(FaultType.class), null, "All");
+		faultTypeFilter.addParameterChangeListener(this);
+
+		EnumParameter<FocalMech> focalMechFilter = new EnumParameter<FocalMech>(
+			"Focal Mech Filter", EnumSet.allOf(FocalMech.class), null, "All");
+		focalMechFilter.addParameterChangeListener(this);
+
+		
+	}
+
+	private void init() {
+		
+		addERFs(Sources.getGridList(CEUS));
+		System.out.println("CEUS_GRD " + getSourceCount());
+		addERFs(Sources.getFaultList(CEUS));
+		System.out.println("CEUS_FLT " + getSourceCount());
+		addERFs(Sources.getClusterList(CEUS));
+		System.out.println("CEUS_CLU " + getSourceCount());
+		
+		addERFs(Sources.getGridList(WUS));
+		System.out.println("WUS_GRD  " + getSourceCount());
+		addERFs(Sources.getFaultList(WUS));
+		System.out.println("WUS_FLT  " + getSourceCount());
+		addERFs(Sources.getGridList(CA));
+		System.out.println("CA_GRD   " + getSourceCount());
+		addERFs(Sources.getFaultList(CA));
+		System.out.println("CA_FLT   " + getSourceCount());
+		addERFs(Sources.getSubductionList(CASC));
+		System.out.println("WUS_SUB  " + getSourceCount());
+		
+//		addERFs(Sources.getFaultList(CA));
+//		addERF(Sources.getFault("aFault_aPriori_D2.1.in"));
+//		addERF(Sources.getFault("aFault_MoBal.in"));
+//		addERF(Sources.getFault("aFault_unseg.in"));
+//		addERF(Sources.getFault("bFault.ch.in"));
+//		addERF(Sources.getFault("bFault.gr.in"));
+
+//		for (NSHMP_ERF erf : this) {
+//			System.out.println(erf.getName());
+//		}
+
+//		aFault_aPriori_D2.1.in
+//		aFault_MoBal.in
+//		aFault_unseg.in
+//		bFault.ch.in
+//		bFault.gr.in
+
+		// north of Memphis on New Mdrid fault
+//		addERF(GridERF.getTestGrid(new Location(35.6,-90.4)));
+		
+//		addERF(Sources.get("CEUS.2007all8.J.in"));
+
+//		addERF(Sources.get("bFault.gr.in"));
+		
+//		addERF(Sources.get("bFault.ch.in"));
+//		addERF(Sources.get("CAmap.21.gr.in"));
+//		addERF(Sources.get("impext.ch.in"));
+//		addERF(Sources.get("CAmap.24.gr.in"));
+
+//		addERF(Sources.get("pnwdeep.in"));
+//		addERF(Sources.get("CEUSchar.71.in"));
+		
+//		addERF(Sources.get("sangorg.in")); // fixed strike source results not great
+			
+//		addERF(Sources.get("newmad.1500.cluster.in"));
+//		addERF(Sources.get("NMSZnocl.1000yr.5branch.in"));
+	}
+	
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		for (NSHMP_ERF src : this) {
+			sb.append(StringUtils.rightPad(src.getName(), 30));
+			sb.append(StringUtils.rightPad(src.getSourceRegion().name(), 6));
+			sb.append(	StringUtils.rightPad(src.getSourceType().name(), 14));
+			sb.append(	StringUtils.rightPad(src.getSourceIMR().name(), 12));
+			sb.append(	StringUtils.rightPad(String.valueOf(src.getSourceWeight()), 12));
+			sb.append(	StringUtils.rightPad(String.valueOf(src.getRuptureCount()), 10));
+			sb.append(	StringUtils.leftPad(String.valueOf(src.getMaxDistance()), 10));
+			sb.append(IOUtils.LINE_SEPARATOR);
+		}
+		return sb.toString();
+	}
+	
+	public static void main(String[] args) {
+		
+//
+//		WUS_ERF wus = new WUS_ERF();
+//		wus.updateForecast();
+//		System.out.println(wus);
+		
+		
+
+//		while (true) {
+//			try {
+//				Thread.sleep(5000);
+//			} catch (InterruptedException e) {
+//					e.printStackTrace();
+//			}
+//				
+//		}
+	}
+
+}
