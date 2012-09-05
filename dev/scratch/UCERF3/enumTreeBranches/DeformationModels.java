bdaa61b1448c4e1c17c46a8cfee617fc76f66a1f
Kevin Milner <kmilner@usc.edu>
2012-09-05 18:58:44 +0000

    updated neokinema and ABM

--- dev/scratch/UCERF3/enumTreeBranches/DeformationModels.java
+++ dev/scratch/UCERF3/enumTreeBranches/DeformationModels.java
@@ -21,13 +21,13 @@ public enum DeformationModels implements LogicTreeBranchNode<DeformationModels> 
 	
 	// UCERF3
 	// AVERAGE BLOCK MODEL
-	ABM(					"Average Block Model",	"ABM",		0.20d,	FaultModels.FM3_1,	"ABM_slip_rake_fm_3_1_2012_06_22.csv",
-																		FaultModels.FM3_2,	"ABM_slip_rake_fm_3_2_2012_06_22.csv"),
+	ABM(					"Average Block Model",	"ABM",		0.20d,	FaultModels.FM3_1,	"ABM_slip_rake_fm_3_1_2012_08_31.csv",
+																		FaultModels.FM3_2,	"ABM_slip_rake_fm_3_2_2012_08_31.csv"),
 	// GEOBOUNDED INVERSION
 	GEOBOUND(				"Geobounded",			"GEOB",		0d,		FaultModels.FM3_1,	"geobound_slip_rake__MAPPED_2012_06_05.csv"),
 	// NEOKINEMA
-	NEOKINEMA(				"Neokinema",			"NEOK",		0.35d,	FaultModels.FM3_1,	"neokinema_slip_rake_fm_3_1_2012_06_08.csv",
-																		FaultModels.FM3_2,	"neokinema_slip_rake_fm_3_2_2012_06_08.csv"),
+	NEOKINEMA(				"Neokinema",			"NEOK",		0.35d,	FaultModels.FM3_1,	"neokinema_slip_rake_fm_3_1_2012_08_18.csv",
+																		FaultModels.FM3_2,	"neokinema_slip_rake_fm_3_2_2012_08_18.csv"),
 	// ZENG
 	ZENG(					"Zeng",					"ZENG",		0.30d,	FaultModels.FM3_1,	"zeng_slip_rake_fm_3_1_2012_06_08.csv",
 																		FaultModels.FM3_2,	"zeng_slip_rake_fm_3_2_2012_06_08.csv"),
