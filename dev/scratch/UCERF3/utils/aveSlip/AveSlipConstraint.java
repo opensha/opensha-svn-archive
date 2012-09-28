3aee243d1e0556aeb4987aec08a84f5609d6ab7b
Kevin Milner <kmilner@usc.edu>
2012-09-28 16:40:52 +0000

    added ave slip data from table R6 (paleoseismology) and added paleo sites/mapping viewer in SCEC-VDO

--- dev/scratch/UCERF3/utils/aveSlip/AveSlipConstraint.java
+++ dev/scratch/UCERF3/utils/aveSlip/AveSlipConstraint.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
@@ -16,6 +17,7 @@ import org.opensha.commons.util.FaultUtils;
 import org.opensha.refFaultParamDb.vo.FaultSectionPrefData;
 import org.opensha.sha.faultSurface.FaultTrace;
 
+import com.google.common.base.Joiner;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
@@ -28,7 +30,8 @@ import scratch.UCERF3.utils.UCERF3_DataUtils;
 public class AveSlipConstraint {
 	
 	public static final String DIR_NAME = "aveSlip";
-	public static final String FILE_NAME = "Table R5v4.xls";
+	public static final String TABLE_5_FILE_NAME = "Table R5v4.xls";
+	public static final String TABLE_6_FILE_NAME = "Table R6v4.xls";
 	
 	private static ArbitrarilyDiscretizedFunc probObsSlipModel;
 	static {
@@ -44,14 +47,17 @@ public class AveSlipConstraint {
 	private double weightedMean;
 	private double upperUncertaintyBound;
 	private double lowerUncertaintyBound;
+	private Location loc;
 	
 	public AveSlipConstraint(int subSectionIndex, double weightedMean,
-			double upperUncertaintyBound, double lowerUncertaintyBound) {
+			double upperUncertaintyBound, double lowerUncertaintyBound,
+			Location loc) {
 		super();
 		this.subSectionIndex = subSectionIndex;
 		this.weightedMean = weightedMean;
 		this.upperUncertaintyBound = upperUncertaintyBound;
 		this.lowerUncertaintyBound = lowerUncertaintyBound;
+		this.loc = loc;
 	}
 
 	public int getSubSectionIndex() {
@@ -87,6 +93,10 @@ public class AveSlipConstraint {
 		return lowerUncertaintyBound;
 	}
 	
+	public Location getSiteLocation() {
+		return loc;
+	}
+	
 	public static double getProbabilityOfObservedSlip(double meters) {
 		if (meters > probObsSlipModel.getMaxX())
 			return probObsSlipModel.getY(probObsSlipModel.getNum()-1);
@@ -102,7 +112,11 @@ public class AveSlipConstraint {
 	}
 
 	public static List<AveSlipConstraint> load(List<FaultSectionPrefData> subSectData) throws IOException {
-		return load(UCERF3_DataUtils.locateResourceAsStream(DIR_NAME, FILE_NAME), subSectData);
+		List<AveSlipConstraint> aveSlipData =
+			load(UCERF3_DataUtils.locateResourceAsStream(DIR_NAME, TABLE_5_FILE_NAME), subSectData);
+		aveSlipData.addAll(
+				load(UCERF3_DataUtils.locateResourceAsStream(DIR_NAME, TABLE_6_FILE_NAME), subSectData));
+		return aveSlipData;
 	}
 	
 	public static List<AveSlipConstraint> load(
@@ -142,42 +156,63 @@ public class AveSlipConstraint {
 			String faultName = row.getCell(0).getStringCellValue().trim();
 			if (faultName.isEmpty())
 				continue;
-			if (faultName.equals("EXPLANATION"))
+			if (faultName.equals("EXPLANATION") || faultName.isEmpty())
 				// we're done
 				break;
 //			System.out.println(rowIndex+": "+row.getCell(0));
-			Integer parentID = (int)row.getCell(1).getNumericCellValue();
+			List<Integer> parentIDs = Lists.newArrayList();
+			HSSFCell parentCell = row.getCell(1);
+			if (parentCell.getCellType() == HSSFCell.CELL_TYPE_STRING) {
+				String[] parentSplits = parentCell.getStringCellValue().trim().split(",");
+				for (String parentStr : parentSplits)
+					parentIDs.add(Integer.parseInt(parentStr.trim()));
+			} else {
+				parentIDs.add((int)row.getCell(1).getNumericCellValue());
+			}
+			
 			double lat = row.getCell(2).getNumericCellValue();
 			double lon = row.getCell(3).getNumericCellValue();
 			Location loc = new Location(lat, lon);
 			
+			boolean blindThrustHack = faultName.startsWith("Compton") || faultName.startsWith("Puente Hills");
+			
 			FaultSectionPrefData matchSect = null;
 			double minDist = Double.POSITIVE_INFINITY;
-			for (FaultSectionPrefData subSect : parentSectsMap.get(parentID)) {
-				FaultTrace trace = FaultUtils.resampleTrace(subSect.getFaultTrace(), 11);
-				for (Location traceLoc : trace) {
-					double dist = LocationUtils.horzDistanceFast(loc, traceLoc);
-					if (dist < minDist) {
-						minDist = dist;
-						matchSect = subSect;
+			for (Integer parentID : parentIDs) {
+				List<FaultSectionPrefData> subSects = parentSectsMap.get(parentID);
+				if (subSects == null)
+					continue;
+				for (FaultSectionPrefData subSect : subSects) {
+					FaultTrace trace = FaultUtils.resampleTrace(subSect.getFaultTrace(), 11);
+					for (Location traceLoc : trace) {
+						double dist = LocationUtils.horzDistanceFast(loc, traceLoc);
+						if (dist < minDist) {
+							minDist = dist;
+							matchSect = subSect;
+						}
 					}
 				}
 			}
-			Preconditions.checkNotNull(matchSect, "no sub sects for parent?");
-			Preconditions.checkState(minDist < 5d,
-					"no sub sect found within 5km for site on "+faultName+" at: "+loc);
+			if (matchSect == null) {
+				System.out.println("Skipping ave slip site '"+faultName+"' as parent(s) don't" +
+						" exist in given fault model: "+Joiner.on(",").join(parentIDs));
+				continue;
+			}
+			Preconditions.checkState(blindThrustHack || minDist < 5d,
+					"no sub sect found within 5km for site on "+faultName+" at: "+loc+" (mindist="+minDist+")");
 			double mean = row.getCell(22).getNumericCellValue();
 			double uncertaintyPlus = row.getCell(23).getNumericCellValue();
 			double uncertaintyMinus = row.getCell(24).getNumericCellValue();
 			
-			constraints.add(new AveSlipConstraint(matchSect.getSectionId(), mean, mean+uncertaintyPlus, mean-uncertaintyMinus));
+			constraints.add(new AveSlipConstraint(matchSect.getSectionId(), mean,
+					mean+uncertaintyPlus, mean-uncertaintyMinus, loc));
 		}
 		
 		return constraints;
 	}
 	
 	public static void main(String[] args) throws IOException {
-		FaultModels fm = FaultModels.FM2_1;
+		FaultModels fm = FaultModels.FM3_1;
 		DeformationModels dm = DeformationModels.forFaultModel(fm).get(0);
 		List<FaultSectionPrefData> subSects = new DeformationModelFetcher(
 						fm, dm, UCERF3_DataUtils.DEFAULT_SCRATCH_DATA_DIR, 0.1d).getSubSectionList();
