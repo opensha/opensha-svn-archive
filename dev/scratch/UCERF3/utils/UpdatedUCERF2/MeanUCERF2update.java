344daff717d559f69f366a5e8a2960a1c8422bf4
Peter Powers <pmpowers@usgs.gov>
2012-09-28 22:51:07 +0000

    added MeanUCERF2update which used 2008 NSHMP background source implementation

---
+++ dev/scratch/UCERF3/utils/UpdatedUCERF2/MeanUCERF2update.java
@@ -0,0 +1,58 @@
+package scratch.UCERF3.utils.UpdatedUCERF2;
+
+import java.util.ArrayList;
+
+import org.opensha.sha.earthquake.ProbEqkSource;
+import org.opensha.sha.earthquake.rupForecastImpl.WGCEP_UCERF_2_Final.UCERF2;
+import org.opensha.sha.earthquake.rupForecastImpl.WGCEP_UCERF_2_Final.MeanUCERF2.MeanUCERF2;
+
+import com.google.common.collect.Lists;
+
+/**
+ * Due to discrepanices between the current NSHMP background seismsicity model
+ * and that implemented in UCERF2, this updated version of {@code MeanUCERF2}
+ * supplies the current NSHMP model to facilitate comparisons between the NSHMP,
+ * UCERF2, and UCERF3.
+ * 
+ * This implementation also ignores the non-California b-Fault sources.
+ * 
+ * the current NSHMP background
+ *
+ * 
+ * @author Peter Powers
+ * @version $Id:$
+ */
+public class MeanUCERF2update extends MeanUCERF2 {
+
+	private NSHMP08_GridSourceGenerator gridSrcGen;
+
+	/**
+	 * Returns the  ith earthquake source
+	 *
+	 * @param iSource : index of the source needed
+	 */
+	public ProbEqkSource getSource(int idx) {
+		return (idx < allSources.size()) ? 
+			allSources.get(idx) : 
+			gridSrcGen.getSource(idx - allSources.size());
+	}
+
+	@Override
+	public int getNumSources() {
+		if(backSeisParam.getValue().equals(UCERF2.BACK_SEIS_INCLUDE) ||
+				backSeisParam.getValue().equals(UCERF2.BACK_SEIS_ONLY))
+			return allSources.size() + gridSrcGen.getNumSources();
+		else return allSources.size();
+	}
+	
+	@Override
+	protected void mkNonCA_B_FaultSources() {
+		nonCA_bFaultSources = Lists.newArrayList();
+	}
+	
+	@Override
+	public ArrayList<ProbEqkSource>  getSourceList(){
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ dev/scratch/UCERF3/utils/UpdatedUCERF2/NSHMP08_GridSourceGenerator.java
@@ -0,0 +1,53 @@
+package scratch.UCERF3.utils.UpdatedUCERF2;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.opensha.nshmp2.erf.NSHMP2008;
+import org.opensha.sha.earthquake.ERF;
+import org.opensha.sha.earthquake.ProbEqkSource;
+import org.opensha.sha.earthquake.rupForecastImpl.WGCEP_UCERF_2_Final.griddedSeis.Point2Vert_FaultPoisSource;
+import org.opensha.sha.magdist.SummedMagFreqDist;
+
+import com.google.common.collect.Lists;
+import com.google.common.primitives.Ints;
+
+/**
+ * Add comments here
+ *
+ * 
+ * @author Peter Powers
+ * @version $Id:$
+ */
+public class NSHMP08_GridSourceGenerator {
+	
+	private NSHMP2008 gridListERF;
+	
+	// array of summed erf source counts to facilitate
+	// indexing of nested erfs
+	private int[] erfIndices;
+	
+	public NSHMP08_GridSourceGenerator() {
+		gridListERF = NSHMP2008.createCaliforniaGridded();
+		int prevValue = 0;
+		List<Integer> indexList = Lists.newArrayList();
+		for (ERF erf : gridListERF) {
+			int currentTotal = erf.getNumSources() + prevValue;
+			indexList.add(currentTotal);
+			prevValue = currentTotal;
+		}
+		erfIndices = Ints.toArray(indexList);
+	}
+	
+	public ProbEqkSource getSource(int i) {
+		int erfIdx = Arrays.binarySearch(erfIndices, i);
+		if (erfIdx < 0) erfIdx = -(i+2);
+		int srcIdx = i - erfIndices[erfIdx];
+		return gridListERF.getERF(erfIdx).getSource(srcIdx);
+	}
+	
+	public int getNumSources() {
+		return gridListERF.getSourceCount();
+	}
+
+}
