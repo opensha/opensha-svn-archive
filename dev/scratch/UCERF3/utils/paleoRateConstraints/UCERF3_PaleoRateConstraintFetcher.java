2c7d14ac46cad29289055d5c55c25b6a9ba0e773
Kevin Milner <kmilner@usc.edu>
2012-10-02 17:04:52 +0000

    added mapping to paleo data file

--- dev/scratch/UCERF3/utils/paleoRateConstraints/UCERF3_PaleoRateConstraintFetcher.java
+++ dev/scratch/UCERF3/utils/paleoRateConstraints/UCERF3_PaleoRateConstraintFetcher.java
@@ -2,6 +2,7 @@ package scratch.UCERF3.utils.paleoRateConstraints;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -23,6 +24,7 @@ import scratch.UCERF3.FaultSystemRupSet;
 import scratch.UCERF3.FaultSystemSolution;
 import scratch.UCERF3.SimpleFaultSystemSolution;
 import scratch.UCERF3.enumTreeBranches.DeformationModels;
+import scratch.UCERF3.enumTreeBranches.FaultModels;
 import scratch.UCERF3.inversion.InversionFaultSystemRupSetFactory;
 import scratch.UCERF3.inversion.InversionInputGenerator;
 import scratch.UCERF3.utils.UCERF3_DataUtils;
@@ -31,18 +33,40 @@ import scratch.UCERF3.utils.aveSlip.AveSlipConstraint;
 public class UCERF3_PaleoRateConstraintFetcher {
 	
 	private final static String PALEO_DATA_SUB_DIR = "paleoRateData";
-	private final static String PALEO_DATA_FILE_NAME = "UCERF3_PaleoRateData_v05.xls";
+	private final static String PALEO_DATA_FILE_NAME = "UCERF3_PaleoRateData_v05_withMappings.xls";
 	
 	protected final static boolean D = true;  // for debugging
 	
 	public static ArrayList<PaleoRateConstraint> getConstraints(
 			List<FaultSectionPrefData> faultSectionData) throws IOException {
+		return getConstraints(faultSectionData, -1);
+	}
+	
+	/**
+	 * this additional constructor is for writing the subsection mapping to the given column in the file
+	 * @param faultSectionData
+	 * @param mappingCol
+	 * @return
+	 * @throws IOException
+	 */
+	private static ArrayList<PaleoRateConstraint> getConstraints(
+			List<FaultSectionPrefData> faultSectionData, int mappingCol) throws IOException {
 		
 		ArrayList<PaleoRateConstraint> paleoRateConstraints   = new ArrayList<PaleoRateConstraint>();
 		if(D) System.out.println("Reading Paleo Seg Rate Data from "+PALEO_DATA_FILE_NAME);
-		InputStream is =
-			UCERF3_DataUtils.locateResourceAsStream(PALEO_DATA_SUB_DIR, PALEO_DATA_FILE_NAME);
-		POIFSFileSystem fs = new POIFSFileSystem(is);
+		POIFSFileSystem fs;
+		File outputFile = null;
+		if (mappingCol > 0) {
+			File dir = new File(UCERF3_DataUtils.DEFAULT_SCRATCH_DATA_DIR.getParentFile(), PALEO_DATA_SUB_DIR);
+			outputFile = new File(dir, PALEO_DATA_FILE_NAME);
+			InputStream is =
+				new FileInputStream(outputFile);
+			fs = new POIFSFileSystem(is);
+		} else {
+			InputStream is =
+				UCERF3_DataUtils.locateResourceAsStream(PALEO_DATA_SUB_DIR, PALEO_DATA_FILE_NAME);
+			fs = new POIFSFileSystem(is);
+		}
 		HSSFWorkbook wb = new HSSFWorkbook(fs);
 		HSSFSheet sheet = wb.getSheetAt(0);
 		int lastRowIndex = sheet.getLastRowNum();
@@ -111,14 +135,28 @@ public class UCERF3_PaleoRateConstraintFetcher {
 					" (section index = "+closestFaultSectionIndex+")\tdist="+(float)minDist+"\tmeanRate="+(float)meanRate+
 					"\tlower68="+(float)lower68Conf+"\tupper68="+(float)upper68Conf);
 			paleoRateConstraints.add(paleoRateConstraint);
+			
+			if (mappingCol > 0) {
+				HSSFCell mappingCell = row.getCell(mappingCol);
+				mappingCell.setCellValue(name);
+			}
 		}
+		
+		if (mappingCol > 0)
+			wb.write(new FileOutputStream(outputFile));
 		return paleoRateConstraints;
 	}
 	
 	public static void main(String args[]) throws IOException, DocumentException {
-		
-   		FaultSystemRupSet faultSysRupSet = InversionFaultSystemRupSetFactory.cachedForBranch(DeformationModels.GEOLOGIC);
-   		UCERF3_PaleoRateConstraintFetcher.getConstraints(faultSysRupSet.getFaultSectionDataList());
+		int mappingCol = 9;
+		for (FaultModels fm : FaultModels.values()) {
+			FaultSystemRupSet faultSysRupSet =
+				InversionFaultSystemRupSetFactory.cachedForBranch(fm, DeformationModels.forFaultModel(fm).get(0));
+			UCERF3_PaleoRateConstraintFetcher.getConstraints(faultSysRupSet.getFaultSectionDataList(), mappingCol++);
+		}
+		System.exit(0);
+//   		FaultSystemRupSet faultSysRupSet = InversionFaultSystemRupSetFactory.cachedForBranch(DeformationModels.GEOLOGIC);
+//   		UCERF3_PaleoRateConstraintFetcher.getConstraints(faultSysRupSet.getFaultSectionDataList());
 
 //		File rupSetsDir = new File(precomp, "FaultSystemRupSets");
 //		ArrayList<FaultSystemSolution> sols = new ArrayList<FaultSystemSolution>();
