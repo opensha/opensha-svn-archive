aed840d8108b26fd58db0f35b5ace33ee879d48c
Kevin Milner <kmilner@usc.edu>
2012-12-19 21:14:08 +0000

    rolled back to using UCERF3.1 creep and ave slip data for apples to apples comparisons until we are ready for 3.2 runs

--- dev/scratch/UCERF3/utils/DeformationModelFileParser.java
+++ dev/scratch/UCERF3/utils/DeformationModelFileParser.java
@@ -448,8 +448,11 @@ public class DeformationModelFileParser {
 			
 			// now load the creep file
 			
+			// TODO switch back to new version when ready for 3.2 runs
+			String creepFileName = "creep-by-minisection-2012_06_03.xls";
+//			String creepFileName = "creep-by-minisection-2012_11_28.xls";
 			InputStream is = UCERF3_DataUtils.locateResourceAsStream("creep",
-					"creep-by-minisection-2012_11_28.xls");
+					creepFileName);
 			POIFSFileSystem fs = new POIFSFileSystem(is);
 			HSSFWorkbook wb = new HSSFWorkbook(fs);
 			HSSFSheet sheet = wb.getSheetAt(0);
