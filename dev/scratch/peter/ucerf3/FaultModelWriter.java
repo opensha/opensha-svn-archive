c1d18eb23db4ab12c436a18016c97083844deb3b
Peter Powers <pmpowers@usgs.gov>
2013-03-07 19:56:09 +0000

    added utility to write fault model traces to kml

--- dev/scratch/peter/ucerf3/FaultModelWriter.java
+++ dev/scratch/peter/ucerf3/FaultModelWriter.java
@@ -1,16 +1,21 @@
 package scratch.peter.ucerf3;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.dom4j.Document;
+import org.opensha.commons.geo.LocationList;
 import org.opensha.commons.util.XMLUtils;
+import org.opensha.nshmp2.util.GridUtils;
 import org.opensha.refFaultParamDb.vo.FaultSectionPrefData;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 import scratch.UCERF3.SimpleFaultSystemRupSet;
 import scratch.UCERF3.enumTreeBranches.FaultModels;
@@ -23,7 +28,7 @@ class FaultModelWriter {
 	public static void writeFaultModel() {
 		try {
 			File dir = new File("tmp");
-			FaultModels fm = FaultModels.FM3_2;
+			FaultModels fm = FaultModels.FM3_1;
 			ArrayList<FaultSectionPrefData> datas = fm.fetchFaultSections();
 			Document doc = XMLUtils.createDocumentWithRoot();
 			SimpleFaultSystemRupSet.fsDataToXML(doc.getRootElement(), datas,
@@ -37,8 +42,8 @@ class FaultModelWriter {
 	}
 	
 	public static void main(String[] args) {
-		writeFaultModel();
-		
+//		writeFaultModel();
+		writeFaultModelToKML();
 //		double[] dd = { 1,2,3,4,5,6,7,8};
 //		List<Double> dList = Lists.newArrayList();
 //		for (double d : dd) {
@@ -52,5 +57,18 @@ class FaultModelWriter {
 //		}
 //		System.out.println(dList);
 	}
+	
+	public static void writeFaultModelToKML() {
+			FaultModels fm = FaultModels.FM3_2;
+			ArrayList<FaultSectionPrefData> datas = fm.fetchFaultSections();
+			Map<String, LocationList> traceMap = Maps.newHashMap();
+			for (FaultSectionPrefData fspd : datas) {
+				traceMap.put(fspd.getSectionName(), fspd.getFaultTrace());
+			}
+			GridUtils.tracesToKML(traceMap, "UC3_FM32", 
+				Color.RED);
+			System.exit(0);
+	}
+
 
 }
