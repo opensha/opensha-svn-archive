66dbbf70aa872634d8e41bf9249a1f63bc544130
Peter Powers <pmpowers@usgs.gov>
2013-04-21 03:03:43 +0000

    fixing bad import listed in build fail but that does not seem to be root cause of build failure

--- dev/scratch/peter/tmp/scratch.java
+++ dev/scratch/peter/tmp/scratch.java
@@ -6,14 +6,20 @@ import static org.opensha.nshmp2.util.SourceType.GRIDDED;
 import java.io.File;
 import java.io.IOException;
 
+import org.opensha.commons.geo.GriddedRegion;
 import org.opensha.nshmp.NEHRP_TestCity;
+import org.opensha.nshmp2.tmp.TestGrid;
+import org.opensha.sha.earthquake.param.IncludeBackgroundOption;
 
 import scratch.UCERF3.AverageFaultSystemSolution;
+import scratch.UCERF3.CompoundFaultSystemSolution;
+import scratch.UCERF3.FaultSystemSolution;
 import scratch.UCERF3.SimpleFaultSystemSolution;
+import scratch.UCERF3.erf.UCERF3_FaultSysSol_ERF;
+import scratch.UCERF3.logicTree.LogicTreeBranch;
 import scratch.peter.ucerf3.calc.UC3_CalcUtils;
 
 import com.google.common.io.Files;
-import com.sun.org.apache.bcel.internal.classfile.SourceFile;
 
 /**
  * Add comments here
@@ -27,13 +33,44 @@ public class scratch {
 	
 	public static void main(String[] args) throws IOException {
 
-		tmp();
+//		GriddedRegion gr = TestGrid.LITTLE_SALMON.grid(0.02);
+//		System.out.println(gr.getNodeCount());
 		
-//		String dir = "/Users/pmpowers/projects/OpenSHA/tmp/UC3maps/mapsUC32localRatio";
-//		consolidateMaps(dir);
+
+//		tmp();
+		
+		String dir = "/Users/pmpowers/projects/OpenSHA/tmp/UC3maps/mapsUC32b/UC32littleSalmon/multi-UC32/PE1IN100-sol9";
+		consolidateMaps(dir);
 		
 //		for (NEHRP_TestCity city : NEHRP_TestCity.values()) {
 //			System.out.println(city);
+//		}
+		
+//		UCERF3_FaultSysSol_ERF erf = UC3_CalcUtils.getUC3_ERF(
+//			"/Users/pmpowers/projects/OpenSHA/tmp/invSols/conv/2013_02_01-ucerf3p2-convergence_bundle5_sol14.zip",
+//			IncludeBackgroundOption.INCLUDE, false, true, 1.0);
+//		erf.updateForecast();
+		
+		//		try {
+//			String srcDir = "/Users/pmpowers/projects/OpenSHA/tmp/invSols/conv/";
+//			String solPath = srcDir + "2013_02_01-ucerf3p2-convergence_bundle5_COMPOUND_SOL.zip";
+//			CompoundFaultSystemSolution cfss = UC3_CalcUtils.getCompoundSolution(solPath);
+//			for (LogicTreeBranch br : cfss.getBranches()) {
+//				System.out.println(br.buildFileName());
+//				
+//			}
+//			
+//			int idx = 0;
+//			for (FaultSystemSolution fss : cfss) {
+//				System.out.println(fss.getClass());
+//				String solName = "2013_02_01-ucerf3p2-convergence_bundle5_sol" + (idx++) + ".zip";
+//				SimpleFaultSystemSolution sfss = new SimpleFaultSystemSolution(fss);
+//				File solZip = new File(srcDir + solName);
+//				sfss.toZipFile(solZip);
+//			}
+//			
+//		} catch (Exception e) {
+//			e.printStackTrace();
 //		}
 	}
 	
