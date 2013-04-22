843791826126d5980c65a48a13fa71d5b0d4d9eb
Peter Powers <pmpowers@usgs.gov>
2013-04-22 19:36:42 +0000

    new spatial seis

--- dev/scratch/UCERF3/utils/SmoothSeismicitySpatialPDF_Fetcher.java
+++ dev/scratch/UCERF3/utils/SmoothSeismicitySpatialPDF_Fetcher.java
@@ -6,6 +6,8 @@ import java.util.StringTokenizer;
 
 import org.apache.commons.lang3.StringUtils;
 import org.opensha.commons.data.region.CaliforniaRegions;
+import org.opensha.commons.data.xyz.GeoDataSet;
+import org.opensha.commons.data.xyz.GeoDataSetMath;
 import org.opensha.commons.data.xyz.GriddedGeoDataSet;
 import org.opensha.commons.geo.Location;
 import org.opensha.commons.util.ExceptionUtils;
@@ -14,6 +16,7 @@ import scratch.UCERF3.analysis.DeformationModelsCalc;
 import scratch.UCERF3.analysis.GMT_CA_Maps;
 import scratch.UCERF3.enumTreeBranches.DeformationModels;
 import scratch.UCERF3.enumTreeBranches.FaultModels;
+import scratch.UCERF3.griddedSeismicity.GridReader;
 
 /**
  * This reads and provides the smoothed seismicity spatial PDFs 
@@ -25,6 +28,10 @@ public class SmoothSeismicitySpatialPDF_Fetcher {
 	public static final String SUBDIR = "SeismicityGrids";
 	public static final String FILENAME_UCERF2 = "SmoothSeis_UCERF2.txt";
 	public static final String FILENAME_UCERF3 = "SmoothSeis_KF_5-5-2012.txt";
+	public static final String FILENAME_UCERF3pt3 = "SmoothSeis_KF_4-9-2013.txt";
+	public static final String FILENAME_UCERF3pt3_SHALLOW = "SmoothSeis_KF_4-9-2013_Shallow.txt";
+	public static final String FILENAME_UCERF3pt3_DEEP = "SmoothSeis_KF_4-9-2013_Deep.txt";
+
 	
 	final static CaliforniaRegions.RELM_TESTING_GRIDDED griddedRegion  = new CaliforniaRegions.RELM_TESTING_GRIDDED();
 	
@@ -40,29 +47,49 @@ public class SmoothSeismicitySpatialPDF_Fetcher {
 		return readPDF_Data(FILENAME_UCERF3);
 	}
 	
+	public static GriddedGeoDataSet getUCERF3pt3_PDF() {
+		return readPDF_Data(FILENAME_UCERF3pt3);
+	}
+	public static GriddedGeoDataSet getUCERF3pt3shallow_PDF() {
+		return readPDF_Data(FILENAME_UCERF3pt3_SHALLOW);
+	}
+	public static GriddedGeoDataSet getUCERF3pt3deep_PDF() {
+		return readPDF_Data(FILENAME_UCERF3pt3_DEEP);
+	}
+	
+	
+	
 
 	private static GriddedGeoDataSet readPDF_Data(String filename) {
 		GriddedGeoDataSet pdfData = new GriddedGeoDataSet(griddedRegion, true);	// true makes X latitude
-		try {
-			BufferedReader reader = new BufferedReader(UCERF3_DataUtils.getReader(SUBDIR, filename));
-			int l=-1;
-			String line;
-			while ((line = reader.readLine()) != null) {
-				StringTokenizer tokenizer = new StringTokenizer(line);
-				Location loc = new Location(Double.valueOf(tokenizer.nextElement().toString()),Double.valueOf(tokenizer.nextElement().toString()));
-				int index = griddedRegion.indexForLocation(loc);
-				if(index >=0)
-					pdfData.set(index, Double.valueOf(tokenizer.nextElement().toString()));
-			}
-		} catch (Exception e) {
-			ExceptionUtils.throwAsRuntimeException(e);
+		GridReader reader = new GridReader(filename);
+		for (Location loc : griddedRegion) {
+			pdfData.set(loc, reader.getValue(loc));
 		}
-		
-//		System.out.println("min="+pdfData.getMinZ());
-//		System.out.println("max="+pdfData.getMaxZ());
-//		System.out.println("sum="+getSumOfData(pdfData));
 		return pdfData;
 	}
+
+//	private static GriddedGeoDataSet readPDF_Data(String filename) {
+//		GriddedGeoDataSet pdfData = new GriddedGeoDataSet(griddedRegion, true);	// true makes X latitude
+//		try {
+//			BufferedReader reader = new BufferedReader(UCERF3_DataUtils.getReader(SUBDIR, filename));
+//			String line;
+//			while ((line = reader.readLine()) != null) {
+//				StringTokenizer tokenizer = new StringTokenizer(line);
+//				Location loc = new Location(Double.valueOf(tokenizer.nextElement().toString()),Double.valueOf(tokenizer.nextElement().toString()));
+//				int index = griddedRegion.indexForLocation(loc);
+//				if(index >=0)
+//					pdfData.set(index, Double.valueOf(tokenizer.nextElement().toString()));
+//			}
+//		} catch (Exception e) {
+//			ExceptionUtils.throwAsRuntimeException(e);
+//		}
+//		
+////		System.out.println("min="+pdfData.getMinZ());
+////		System.out.println("max="+pdfData.getMaxZ());
+////		System.out.println("sum="+getSumOfData(pdfData));
+//		return pdfData;
+//	}
 	
 	
 	
@@ -84,17 +111,52 @@ public class SmoothSeismicitySpatialPDF_Fetcher {
 		try {
 			GriddedGeoDataSet u2pdf = getUCERF2_PDF();
 			GriddedGeoDataSet u3pdf = getUCERF3_PDF();
+			GriddedGeoDataSet u33pdf = getUCERF3pt3_PDF();
+			GriddedGeoDataSet u33pdfShallow = getUCERF3pt3shallow_PDF();
+			GriddedGeoDataSet u33pdfDeep = getUCERF3pt3deep_PDF();
 			
-			GriddedGeoDataSet avePDF = new GriddedGeoDataSet(griddedRegion, true);	// true makes X latitude
-			for(int i=0; i<u2pdf.size(); i++) {
-				avePDF.set(i, 0.5*(u2pdf.get(i)+u3pdf.get(i)));
-//				if( i>500 && i < 510)
-//					System.out.println(u2pdf.get(i)+"\t"+u3pdf.get(i)+"\t"+avePDF.get(i));
-			}
+//			GriddedGeoDataSet avePDF = new GriddedGeoDataSet(griddedRegion, true);	// true makes X latitude
+//			for(int i=0; i<u2pdf.size(); i++) {
+//				avePDF.set(i, 0.5*(u2pdf.get(i)+u3pdf.get(i)));
+////				if( i>500 && i < 510)
+////					System.out.println(u2pdf.get(i)+"\t"+u3pdf.get(i)+"\t"+avePDF.get(i));
+//			}
 
-			GMT_CA_Maps.plotSpatialPDF_Map(u2pdf.copy(), "UCERF2_SmoothSeisPDF", "test meta data", "UCERF2_SmoothSeisPDF_Map");
-			GMT_CA_Maps.plotSpatialPDF_Map(u3pdf, "UCERF3_SmoothSeisPDF", "test meta data", "UCERF3_SmoothSeisPFD_Map");
-			GMT_CA_Maps.plotRatioOfRateMaps(avePDF, u2pdf, "aveUCERF3vsUCERF2_SmoothSeisPDF_Ratio", "test meta data", "aveUCERF3vsUCERF2_SmoothSeisPDF_Ratio");
+//			GMT_CA_Maps.plotSpatialPDF_Map(u2pdf.copy(), "UCERF2_SmoothSeisPDF", "test meta data", "UCERF2_SmoothSeisPDF_Map");
+//			GMT_CA_Maps.plotSpatialPDF_Map(u3pdf, "UCERF3_SmoothSeisPDF", "test meta data", "UCERF3_SmoothSeisPFD_Map");
+//			GMT_CA_Maps.plotRatioOfRateMaps(avePDF, u2pdf, "aveUCERF3vsUCERF2_SmoothSeisPDF_Ratio", "test meta data", "aveUCERF3vsUCERF2_SmoothSeisPDF_Ratio");
+
+			
+			// UC3 map
+			GMT_CA_Maps.plotSpatialPDF_Map(u3pdf.copy(), 
+				"UCERF32_SmoothSeisPDF", "test meta data", 
+				"UCERF32_SmoothSeisPFD_Map");
+
+			// UC33 map
+			GMT_CA_Maps.plotSpatialPDF_Map(u33pdf.copy(), 
+				"UCERF33_SmoothSeisPDF", "test meta data", 
+				"UCERF33_SmoothSeisPFD_Map");
+			
+			// UC33 map shallow
+			GMT_CA_Maps.plotSpatialPDF_Map(u33pdfShallow.copy(), 
+				"UCERF33shallow_SmoothSeisPDF", "test meta data", 
+				"UCERF33shallow_SmoothSeisPFD_Map");
+
+			// UC33 map deep
+			GMT_CA_Maps.plotSpatialPDF_Map(u33pdfDeep.copy(), 
+				"UCERF33deep_SmoothSeisPDF", "test meta data", 
+				"UCERF33deep_SmoothSeisPFD_Map");
+			
+			// ratio of UC33 to UC3
+			GMT_CA_Maps.plotRatioOfRateMaps(u33pdf, u3pdf, 
+				"UCERF33_UCERF32_SeisPDF_Ratio", "test meta data", 
+				"UCERF33_UCERF32_SeisPDF_Ratio");
+			
+			// ratio of UC33shallow to UC33
+			GMT_CA_Maps.plotRatioOfRateMaps(u33pdfShallow, u33pdf, 
+				"UCERF33shallow_UCERF33_SeisPDF_Ratio", "test meta data", 
+				"UCERF33shallow_UCERF33_SeisPDF_Ratio");
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
