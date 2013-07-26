f71f65caa3704a90682cd40a3cefbbaaf6dd3709
Peter Powers <pmpowers@usgs.gov>
2013-07-26 11:19:11 +0000

    other classes required by NSHMP GMPEs

---
+++ dev/scratch/peter/nga/DefaultGroundMotion.java
@@ -0,0 +1,30 @@
+package scratch.peter.nga;
+
+import scratch.peter.newcalc.ScalarGroundMotion;
+
+/**
+ * Default wrapper for ground motion prediction equation (GMPE) results.
+ * 
+ * @author Peter Powers
+ * @version $Id:$
+ */
+public class DefaultGroundMotion implements ScalarGroundMotion {
+
+	private double mean;
+	private double sigma;
+	
+	/**
+	 * Create a new ground motion container.
+	 * 
+	 * @param mean ground motion (in natural log units)
+	 * @param sigma aleatory uncertainty
+	 */
+	public DefaultGroundMotion(double mean, double sigma) {
+		this.mean = mean;
+		this.sigma = sigma;
+	}
+
+	@Override public double mean() { return mean; }
+	@Override public double stdDev() { return sigma; }
+
+}
